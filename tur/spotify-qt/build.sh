2bb23dc7ba1b08383a8a6784840cc8561f87d1c2
Ansh Dadwal <68729523+T-Dynamos@users.noreply.github.com>
2022-07-04 15:22:44 +0530

    Fix deps and other changes

--- tur/spotify-qt/build.sh
+++ tur/spotify-qt/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE="https://github.com/kraxarn/spotify-qt"
 TERMUX_PKG_DESCRIPTION="Lightweight Spotify client using Qt"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="@Tur"
-TERMUX_PKG_VERSION="3.8"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION=$TERMUX_PKG_VERSION
 TERMUX_PKG_SRCURL="https://github.com/kraxarn/spotify-qt/archive/refs/tags/v3.8.tar.gz"
 TERMUX_PKG_SHA256="deb12800b9fb4f501f8dfc97a2fdc4989849fe8bbf6f38d79503214e03afe5ad"
-TERMUX_PKG_DEPENDS="qt5-qtbase, zlib"
+TERMUX_PKG_DEPENDS="qt5-qtbase,zlib,qt5-qtsvg"
