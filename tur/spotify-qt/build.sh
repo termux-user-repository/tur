7f4a7dcf91e93e41a8e0cd4b1362bf70218b1f79
Ansh Dadwal <68729523+T-Dynamos@users.noreply.github.com>
2022-07-04 12:00:05 +0530

    Fix lisence

--- tur/spotify-qt/build.sh
+++ tur/spotify-qt/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE="https://github.com/kraxarn/spotify-qt"
 TERMUX_PKG_DESCRIPTION="Lightweight Spotify client using Qt"
-TERMUX_PKG_LICENSE="GNU General Public License v3.0"
+TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.8"
 TERMUX_PKG_SRCURL="https://github.com/kraxarn/spotify-qt/archive/refs/tags/v3.8.tar.gz"
