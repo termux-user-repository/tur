7b76f3fac20cd9ed2c48925e3c28a3376c2985b5
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-07-09 02:03:37 +0800

    new package: libllvm-11, lfortran-llvm

---
+++ tur/libllvm-11/build.sh
@@ -0,0 +1,80 @@
+TERMUX_PKG_HOMEPAGE=https://clang.llvm.org/
+TERMUX_PKG_DESCRIPTION="Modular compiler and toolchain technologies library (Version 11)"
+TERMUX_PKG_LICENSE="NCSA"
+TERMUX_PKG_MAINTAINER="@buttaface, @termux-user-repository"
+TERMUX_PKG_VERSION=11.1.0
+TERMUX_PKG_SHA256=74d2529159fd118c3eac6f90107b5611bccc6f647fdea104024183e8d5e25831
+TERMUX_PKG_SRCURL=https://github.com/llvm/llvm-project/releases/download/llvmorg-$TERMUX_PKG_VERSION/llvm-project-$TERMUX_PKG_VERSION.src.tar.xz
+TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_DEPENDS="binutils, libc++, ncurses, ndk-sysroot, libffi, zlib"
+# Replace gcc since gcc is deprecated by google on android and is not maintained upstream.
+# Conflict with clang versions earlier than 3.9.1-3 since they bundled llvm.
+TERMUX_PKG_CONFLICTS="gcc, clang (<< 3.9.1-3)"
+TERMUX_PKG_BREAKS="libclang, libclang-dev, libllvm-dev"
+TERMUX_PKG_REPLACES="gcc, libclang, libclang-dev, libllvm-dev"
+_PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
+# See http://llvm.org/docs/CMake.html:
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DANDROID_PLATFORM_LEVEL=$TERMUX_PKG_API_LEVEL
+-DPYTHON_EXECUTABLE=$(command -v python3)
+-DLLVM_ENABLE_PIC=ON
+-DLLVM_ENABLE_LIBEDIT=OFF
+-DLLVM_INCLUDE_TESTS=OFF
+-DDEFAULT_SYSROOT=$(dirname $TERMUX_PREFIX)
+-DLLVM_LINK_LLVM_DYLIB=ON
+-DLLVM_TABLEGEN=$TERMUX_PKG_HOSTBUILD_DIR/bin/llvm-tblgen
+-DLIBOMP_ENABLE_SHARED=FALSE
+-DLLVM_ENABLE_SPHINX=ON
+-DSPHINX_OUTPUT_MAN=ON
+-DSPHINX_WARNINGS_AS_ERRORS=OFF
+-DLLVM_TARGETS_TO_BUILD=all
+-DPERL_EXECUTABLE=$(command -v perl)
+-DLLVM_ENABLE_FFI=ON
+-DLLVM_INSTALL_UTILS=ON
+-DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX/opt/libllvm-11
+"
+
+if [ $TERMUX_ARCH_BITS = 32 ]; then
+	# Do not set _FILE_OFFSET_BITS=64
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DLLVM_FORCE_SMALLFILE_FOR_ANDROID=on"
+fi
+
+TERMUX_PKG_FORCE_CMAKE=true
+TERMUX_PKG_HAS_DEBUG=false
+
+termux_step_host_build() {
+	termux_setup_cmake
+	termux_setup_ninja
+
+	cmake -G Ninja $TERMUX_PKG_SRCDIR/llvm
+	ninja -j $TERMUX_MAKE_PROCESSES llvm-tblgen
+}
+
+termux_step_pre_configure() {
+	# Add unknown vendor, otherwise it screws with the default LLVM triple
+	# detection.
+	export LLVM_DEFAULT_TARGET_TRIPLE=${CCTERMUX_HOST_PLATFORM/-/-unknown-}
+	export LLVM_TARGET_ARCH
+	if [ $TERMUX_ARCH = "arm" ]; then
+		LLVM_TARGET_ARCH=ARM
+	elif [ $TERMUX_ARCH = "aarch64" ]; then
+		LLVM_TARGET_ARCH=AArch64
+	elif [ $TERMUX_ARCH = "i686" ] || [ $TERMUX_ARCH = "x86_64" ]; then
+		LLVM_TARGET_ARCH=X86
+	else
+		termux_error_exit "Invalid arch: $TERMUX_ARCH"
+	fi
+	# see CMakeLists.txt and tools/clang/CMakeLists.txt
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DLLVM_TARGET_ARCH=$LLVM_TARGET_ARCH"
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DLLVM_HOST_TRIPLE=$LLVM_DEFAULT_TARGET_TRIPLE"
+	export TERMUX_SRCDIR_SAVE=$TERMUX_PKG_SRCDIR
+	TERMUX_PKG_SRCDIR=$TERMUX_PKG_SRCDIR/llvm
+}
+
+termux_step_post_configure() {
+	TERMUX_PKG_SRCDIR=$TERMUX_SRCDIR_SAVE
+}
+
+termux_step_post_make_install() {
+	ln -sfr $TERMUX_PREFIX/opt/libllvm-11/lib/libLLVM-11.so $TERMUX_PREFIX/lib/
+}
