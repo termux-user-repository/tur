ff387b22d646ed42fe36acef53f127da2208fcaa
Uchiha Kakashi <45286352+licy183@users.noreply.github.com>
2022-07-09 17:32:03 +0800

    libllvm-11: fix LICENSE path

--- tur/libllvm-11/build.sh
+++ tur/libllvm-11/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://clang.llvm.org/
 TERMUX_PKG_DESCRIPTION="Modular compiler and toolchain technologies library (Version 11)"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_LICENSE_FILE="llvm/LICENSE.txt"
+TERMUX_PKG_LICENSE_FILE="llvm/LICENSE.TXT"
 TERMUX_PKG_MAINTAINER="@buttaface, @termux-user-repository"
 TERMUX_PKG_VERSION=11.1.0
 TERMUX_PKG_REVISION=1
