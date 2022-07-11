4723147d70b7c63d647f57cc50aad5bb66d15ae0
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-07-11 14:59:48 +0800

    libllvm-15: add to disabled-tur

---
+++ disabled-tur/libllvm-15/lld-15.subpackage.sh
@@ -0,0 +1,11 @@
+TERMUX_SUBPKG_INCLUDE="
+$_INSTALL_PREFIX/bin/ld.lld
+$_INSTALL_PREFIX/bin/ld64.lld
+$_INSTALL_PREFIX/bin/lld
+$_INSTALL_PREFIX/bin/lld-link
+$_INSTALL_PREFIX/bin/wasm-ld
+$_INSTALL_PREFIX/include/lld/
+$_INSTALL_PREFIX/lib/cmake/lld/
+$_INSTALL_PREFIX/lib/liblld*.a
+"
+TERMUX_SUBPKG_DESCRIPTION="LLVM-based linker"
