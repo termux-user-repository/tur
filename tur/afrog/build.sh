352a5b71acb8a2dce0306a449eb0b7d43991bae9
2096779623 <admin@utermux.dev>
2022-07-14 01:23:28 +0800

    new package: afrog (#32)

---
+++ tur/afrog/build.sh
@@ -0,0 +1,31 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/zan8in/afrog
+TERMUX_PKG_DESCRIPTION="A tool for finding vulnerabilities"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@UtermuxBlog"
+TERMUX_PKG_VERSION="1.3.5"
+TERMUX_PKG_SRCURL=https://github.com/zan8in/afrog/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=eed92c2d646b58ba7d51d14673f90f1d4856670dd51e7183a24e76c795f185d2
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy
+}
+
+termux_step_make() {
+	CGO_ENABLED=0 go build -v -a -o afrog cmd/afrog/main.go
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin "$TERMUX_PKG_SRCDIR"/afrog
+}
+
+termux_step_create_debscripts() {
+	cat <<- EOF > ./prerm
+	#!$TERMUX_PREFIX/bin/sh
+	rm -rf $HOME/.config/afrog
+	EOF
+}
