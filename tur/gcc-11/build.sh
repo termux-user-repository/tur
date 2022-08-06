35adcddef28553d1162c57c35aa34830a1f3e9c0
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-08-06 20:09:52 +0800

    new package: gcc-11

---
+++ tur/gcc-11/build.sh
@@ -0,0 +1,190 @@
+TERMUX_PKG_HOMEPAGE=http://gcc.gnu.org/
+TERMUX_PKG_DESCRIPTION="GNU C compiler"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_DEPENDS="binutils, libc++, libgmp, libmpfr, libmpc, libisl, zlib"
+TERMUX_PKG_VERSION=11.3.0
+TERMUX_PKG_MAINTAINER="@licy183"
+TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/gcc/gcc-${TERMUX_PKG_VERSION}/gcc-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=98438e6cc7294298b474cf0da7655d9a8c8b796421bb0210531c294a950374ed
+TERMUX_PKG_BREAKS="binutils-is-llvm"
+TERMUX_PKG_NO_STATICSPLIT=true
+TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="\
+--enable-initfini-array
+--enable-default-pie
+--enable-languages=c,c++,fortran
+--enable-lto
+--enable-host-shared
+--enable-host-libquadmath
+--enable-libatomic
+--enable-libatomic-ifuncs=no
+--enable-libbacktrace
+--enable-libquadmath
+--enable-libgomp
+--enable-gold
+--enable-version-specific-runtime-libs
+--enable-eh-frame-hdr-for-static
+--disable-shared
+--disable-libmpx
+--disable-libssp
+--disable-libstdcxx
+--disable-multilib
+--disable-tls
+--with-libatomic
+--with-system-zlib
+--with-gmp=$TERMUX_PREFIX
+--with-mpfr=$TERMUX_PREFIX
+--with-mpc=$TERMUX_PREFIX
+--with-isl=$TERMUX_PREFIX
+--with-gxx-include-dir=$TERMUX_PREFIX/include/c++/v1
+--program-suffix=-11
+"
+
+_EXTRA_HOST_BUILD=""
+_ARCH_SPECS=""
+
+if [ "$TERMUX_ARCH" = "arm" ]; then
+	_EXTRA_HOST_BUILD="--with-arch=armv7-a --with-float=soft --with-fpu=vfp"
+elif [ "$TERMUX_ARCH" = "aarch64" ]; then
+	_ARCH_SPECS="\n*cc1:\n+ -ffixed-x18\n\n*cc1plus:\n+ -ffixed-x18\n"
+	_EXTRA_HOST_BUILD="--enable-fix-cortex-a53-835769 --enable-fix-cortex-a53-843419"
+elif [ "$TERMUX_ARCH" = "i686" ]; then
+	_ARCH_SPECS="\n*link_emulation:\nelf_i386\n\n*dynamic_linker:\n/system/bin/linker\n"
+	_EXTRA_HOST_BUILD="--with-arch=i686 --with-fpmath=sse "
+elif [ "$TERMUX_ARCH" = "x86_64" ]; then
+	_EXTRA_HOST_BUILD="--with-arch=x86-64 --with-fpmath=sse"
+fi
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" $_EXTRA_HOST_BUILD"
+
+if $TERMUX_ON_DEVICE_BUILD; then
+	termux_error_exit "Package '$TERMUX_PKG_NAME' is not available for on-device builds."
+fi
+
+source $TERMUX_PKG_BUILDER_DIR/setup_toolchain_ndk_r17c.sh
+
+termux_step_host_build() {
+	if [ -f $TERMUX_PKG_CACHEDIR/.placeholder-newer-toolchain-$TERMUX_ARCH ]; then
+		ln -sr $TERMUX_PKG_CACHEDIR/newer-toolchain-$TERMUX_ARCH $TERMUX_PKG_HOSTBUILD_DIR/newer-toolchain
+		return
+	fi
+
+	# XXX: Install some build dependencies
+	# XXX: So should TUR use a custom builder image?
+	env -i PATH="$PATH" sudo apt update
+	env -i PATH="$PATH" sudo apt install -y libgmp-dev libmpfr-dev libmpc-dev zlib1g-dev libisl-dev libtinfo5 libncurses5
+
+	# Setup a standalone toolchain
+	_setup_standalone_toolchain_ndk_r17c $TERMUX_PKG_HOSTBUILD_DIR/standalone-toolchain
+	cp -R ./standalone-toolchain/sysroot/usr/include/$TERMUX_HOST_PLATFORM/* ./standalone-toolchain/sysroot/usr/include/
+
+	_OLD_PATH="$PATH"
+	PATH="$TERMUX_PKG_HOSTBUILD_DIR/standalone-toolchain/bin:$PATH"
+
+	# Build a custom toolchain
+	mkdir -p newer-toolchain
+	cp -R ./standalone-toolchain/sysroot ./newer-toolchain/
+
+	mkdir -p newer-toolchain-build
+	pushd newer-toolchain-build
+
+	export CFLAGS="-D__ANDROID_API__=$TERMUX_PKG_API_LEVEL"
+	export CPPFLAGS="-D__ANDROID_API__=$TERMUX_PKG_API_LEVEL"
+	export CXXFLAGS="-D__ANDROID_API__=$TERMUX_PKG_API_LEVEL"
+
+	$TERMUX_PKG_SRCDIR/configure \
+				--host=x86_64-linux-gnu  \
+				--build=x86_64-linux-gnu \
+				--target=$TERMUX_HOST_PLATFORM \
+				--disable-shared \
+				--disable-nls \
+				--enable-default-pie \
+				--with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' \
+				--with-gnu-as --with-gnu-ld \
+				--disable-libstdc__-v3 \
+				--disable-tls \
+				--disable-bootstrap \
+				--enable-initfini-array \
+				--enable-libatomic-ifuncs=no \
+				--prefix=$TERMUX_PKG_HOSTBUILD_DIR/newer-toolchain \
+				--with-gmp --with-mpfr --with-mpc --with-system-zlib \
+				--enable-languages=c,c++,fortran \
+				--enable-plugins --enable-libgomp \
+				--enable-gnu-indirect-function \
+				--disable-libcilkrts --disable-libsanitizer \
+				--enable-gold --enable-threads \
+				--enable-eh-frame-hdr-for-static \
+				--enable-graphite=yes --with-isl \
+				--disable-multilib \
+				$_EXTRA_HOST_BUILD \
+				--with-sysroot=$TERMUX_PKG_HOSTBUILD_DIR/newer-toolchain/sysroot \
+				--with-gxx-include-dir=$TERMUX_PKG_HOSTBUILD_DIR/newer-toolchain/include/c++/$TERMUX_PKG_VERSION
+	
+	make -j $TERMUX_MAKE_PROCESSES
+	make -j $TERMUX_MAKE_PROCESSES install
+	popd
+
+	PATH="$_OLD_PATH"
+
+	# Move the pre-built toolchain to $TERMUX_PKG_CACHEDIR and touch a placeholder
+	rm -rf $TERMUX_PKG_CACHEDIR/newer-toolchain-$TERMUX_ARCH
+	mv $TERMUX_PKG_HOSTBUILD_DIR/newer-toolchain $TERMUX_PKG_CACHEDIR/newer-toolchain-$TERMUX_ARCH
+	ln -sr $TERMUX_PKG_CACHEDIR/newer-toolchain-$TERMUX_ARCH $TERMUX_PKG_HOSTBUILD_DIR/newer-toolchain
+	touch $TERMUX_PKG_CACHEDIR/.placeholder-newer-toolchain-$TERMUX_ARCH
+}
+
+termux_step_pre_configure() {
+	# XXX: Remove this all the time, as toolchian is architecture-specific.
+	rm -rf $TERMUX_HOSTBUILD_MARKER
+
+	export ac_cv_func_aligned_alloc=no
+	export ac_cv_func__aligned_malloc=no
+	export ac_cv_func_memalign=no
+	export ac_cv_c_bigendian=no
+
+	_setup_toolchain_ndk_r17c
+
+	# Merge toolchain
+	cp -R $TERMUX_PKG_CACHEDIR/newer-toolchain-$TERMUX_ARCH/* $OLD_NDK_TOOLCHAIN/
+	mv $OLD_NDK_TOOLCHAIN/include/c++/4.9.x $OLD_NDK_TOOLCHAIN/include/c++/$TERMUX_PKG_VERSION
+	export FC=$TERMUX_HOST_PLATFORM-gfortran
+
+	# Explicitly define __BIONIC__ and __ANDROID__API__
+	CFLAGS+=" -D__BIONIC__ -D__ANDROID_API__=$TERMUX_PKG_API_LEVEL"
+	CPPFLAGS+=" -D__BIONIC__ -D__ANDROID_API__=$TERMUX_PKG_API_LEVEL"
+	CXXFLAGS+=" -D__BIONIC__ -D__ANDROID_API__=$TERMUX_PKG_API_LEVEL"
+	FCFLAGS=" -D__ANDROID_API__=$TERMUX_PKG_API_LEVEL"
+
+	# Add the specs file
+	sed "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" $TERMUX_PKG_BUILDER_DIR/specs.in |
+		sed "s|@TERMUX_HOST_PLATFORM@|$TERMUX_HOST_PLATFORM|g" |
+		sed "s|@ARCH_PLACEHOLDER@|$_ARCH_SPECS|g" > $TERMUX_PKG_TMPDIR/specs
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-stage1-ldflags=\"-specs=$TERMUX_PKG_TMPDIR/specs\""
+
+	# Add host and target flag
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --host=$TERMUX_HOST_PLATFORM --target=$TERMUX_HOST_PLATFORM"
+
+	# Dummpy an INPUT script for libstdc++.so when building for arm.
+	if [ "$TERMUX_ARCH" = "arm" ]; then
+		echo "INPUT(-lc++_shared)" > $TERMUX_PREFIX/lib/libstdc++.so
+	fi
+}
+
+termux_step_post_make_install() {
+	# Delete the dummy INPUT script
+	if [ "$TERMUX_ARCH" = "arm" ]; then
+		rm -f $TERMUX_PREFIX/lib/libstdc++.so
+	fi
+	# GCC searches $PREFIX/$TERMUX_HOST_PLATFORM/include, so just make a symlink
+	mkdir -p $TERMUX_PREFIX/$TERMUX_HOST_PLATFORM/include
+	ln -sfr $TERMUX_PREFIX/include/$TERMUX_HOST_PLATFORM/asm $TERMUX_PREFIX/$TERMUX_HOST_PLATFORM/include/
+	# Copy the build spec file
+	cp $TERMUX_PKG_TMPDIR/specs $TERMUX_PREFIX/lib/gcc/$TERMUX_HOST_PLATFORM/$TERMUX_PKG_VERSION/
+	# XXX: This is pretty hacking anyway, but I cannot find a better solution. 
+	# XXX: `ndk-sysroot` has a different version of Android headers (like 23c)
+	# XXX: but our custom toolchain should use the Android headers versioning 17c.
+	# XXX: Another way to solve this issue is adding `ndk-sysroot-gcc-compact` to
+	# XXX: `TERMUX_PKG_RECOMMENDS`, but that would break `gcc` if someone installs
+	# XXX: `ndk-sysroot-gcc-compact` and `gcc`, and then install `ndk-sysroot` manually.
+	TERMUX_PKG_DEPENDS+=", ndk-sysroot-gcc-compact"
+}
---
+++ tur/gcc-11/setup_toolchain_ndk_r17c.sh
@@ -0,0 +1,161 @@
+_download_ndk_r17c() {
+	if [ ! -f $TERMUX_COMMON_CACHEDIR/older-ndk/.placeholder-android-ndk-r17c ]; then
+		echo "Start downloading Android NDK toolchain (version r17c)..."
+		mkdir -p $TERMUX_COMMON_CACHEDIR/older-ndk/
+		local _NDK_ARCHIVE_FILE=$TERMUX_COMMON_CACHEDIR/older-ndk/android-ndk-r17c-linux-x86_64.zip
+		local _NDK_URL=https://dl.google.com/android/repository/android-ndk-r17c-linux-x86_64.zip
+		local _NDK_SHA256=3f541adbd0330a9205ba12697f6d04ec90752c53d6b622101a2a8a856e816589
+		termux_download $_NDK_URL $_NDK_ARCHIVE_FILE $_NDK_SHA256
+		unzip -d $TERMUX_COMMON_CACHEDIR/older-ndk/ $_NDK_ARCHIVE_FILE > /dev/null 2>&1
+		touch $TERMUX_COMMON_CACHEDIR/older-ndk/.placeholder-android-ndk-r17c
+		echo "Downloading completed."
+	fi
+}
+
+_setup_standalone_toolchain_ndk_r17c() {
+	_download_ndk_r17c
+
+	local TOOLCHAIN_DIR="$1"
+	rm -rf $TOOLCHAIN_DIR
+
+	local _NDK_ARCH
+	if [ "$TERMUX_ARCH" == "aarch64" ]; then
+		_NDK_ARCH="arm64"
+	elif [ "$TERMUX_ARCH" == "arm" ]; then
+		_NDK_ARCH="arm"
+	elif [ "$TERMUX_ARCH" == "x86_64" ]; then
+		_NDK_ARCH="x86_64"
+	elif [ "$TERMUX_ARCH" == "i686" ]; then
+		_NDK_ARCH="x86"
+	fi
+
+	# Setup a standalone toolchain
+	python $TERMUX_COMMON_CACHEDIR/older-ndk/android-ndk-r17c/build/tools/make_standalone_toolchain.py \
+				--arch $_NDK_ARCH --api $TERMUX_PKG_API_LEVEL --install-dir $TOOLCHAIN_DIR
+
+	# Modify sysroot
+	pushd $TOOLCHAIN_DIR
+
+	# See https://github.com/android/ndk/issues/215#issuecomment-524293090
+	sed -i "s/include_next <stddef.h>/include <stddef.h>/" include/c++/4.9.x/cstddef
+
+	cd $TOOLCHAIN_DIR/sysroot
+
+	# Apply patches
+	for f in $TERMUX_PKG_BUILDER_DIR/ndk-patches-r17c/*.patch; do
+		echo "Applying ndk-patch: $(basename $f)"
+		sed "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" "$f" | \
+			sed "s%\@TERMUX_HOME\@%${TERMUX_ANDROID_HOME}%g" | \
+			patch --silent -p1;
+	done
+
+	# libintl.h: Inline implementation gettext functions.
+	# langinfo.h: Inline implementation of nl_langinfo().
+	cp "$TERMUX_SCRIPTDIR"/ndk-patches/{libintl.h,langinfo.h} usr/include
+
+	# Remove <sys/capability.h> because it is provided by libcap.
+	# Remove <sys/shm.h> from the NDK in favour of that from the libandroid-shmem.
+	# Remove <sys/sem.h> as it doesn't work for non-root.
+	# Remove <glob.h> as we currently provide it from libandroid-glob.
+	# Remove <iconv.h> as it's provided by libiconv.
+	# Remove <zlib.h> and <zconf.h> as we build our own zlib.
+	# Remove KRH/khrplatform.h provided by mesa.
+	rm usr/include/{sys/{capability,shm,sem},{glob,iconv,zlib,zconf},KHR/khrplatform}.h
+
+	sed -i "s/define __ANDROID_API__ __ANDROID_API_FUTURE__/define __ANDROID_API__ $TERMUX_PKG_API_LEVEL/" \
+		usr/include/android/api-level.h
+	popd
+}
+
+_setup_toolchain_ndk_r17c() {
+	export OLD_NDK_TOOLCHAIN=$TERMUX_PKG_TMPDIR/android-ndk-r17c-$TERMUX_ARCH
+
+	export CFLAGS=""
+	export CPPFLAGS=""
+	export LDFLAGS="-L${TERMUX_PREFIX}/lib"
+
+	export AS=$TERMUX_HOST_PLATFORM-gcc
+	export CC=$TERMUX_HOST_PLATFORM-gcc
+	export CXX=$TERMUX_HOST_PLATFORM-g++
+	export CPP=$TERMUX_HOST_PLATFORM-cpp
+	export LD=$TERMUX_HOST_PLATFORM-ld
+	export AR=$TERMUX_HOST_PLATFORM-ar
+	export OBJCOPY=$TERMUX_HOST_PLATFORM-objcopy
+	export OBJDUMP=$TERMUX_HOST_PLATFORM-objdump
+	export RANLIB=$TERMUX_HOST_PLATFORM-ranlib
+	export READELF=$TERMUX_HOST_PLATFORM-readelf
+	export STRIP=$TERMUX_HOST_PLATFORM-strip
+	export NM=$TERMUX_HOST_PLATFORM-nm
+
+	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
+		export PATH=$OLD_NDK_TOOLCHAIN/bin:$PATH
+		export CC_FOR_BUILD=gcc
+		export PKG_CONFIG=$TERMUX_STANDALONE_TOOLCHAIN/bin/pkg-config
+		export PKGCONFIG=$PKG_CONFIG
+		export CCTERMUX_HOST_PLATFORM=$TERMUX_HOST_PLATFORM$TERMUX_PKG_API_LEVEL
+		if [ $TERMUX_ARCH = arm ]; then
+			CCTERMUX_HOST_PLATFORM=armv7a-linux-androideabi$TERMUX_PKG_API_LEVEL
+		fi
+		LDFLAGS+=" -Wl,-rpath=$TERMUX_PREFIX/lib"
+	else
+		export CC_FOR_BUILD=$CC
+		# Some build scripts use environment variable 'PKG_CONFIG', so
+		# using this for on-device builds too.
+		export PKG_CONFIG=pkg-config
+	fi
+	export PKG_CONFIG_LIBDIR="$TERMUX_PKG_CONFIG_LIBDIR"
+
+	if [ "$TERMUX_ARCH" = "arm" ]; then
+		# https://developer.android.com/ndk/guides/standalone_toolchain.html#abi_compatibility:
+		# "We recommend using the -mthumb compiler flag to force the generation of 16-bit Thumb-2 instructions".
+		# With r13 of the ndk ruby 2.4.0 segfaults when built on arm with clang without -mthumb.
+		CFLAGS+=" -march=armv7-a -mfpu=neon -mfloat-abi=softfp -mthumb"
+		LDFLAGS+=" -march=armv7-a"
+	elif [ "$TERMUX_ARCH" = "i686" ]; then
+		# From $NDK/docs/CPU-ARCH-ABIS.html:
+		CFLAGS+=" -march=i686 -msse3 -mstackrealign -mfpmath=sse"
+		# i686 seem to explicitly require -fPIC, see
+		# https://github.com/termux/termux-packages/issues/7215#issuecomment-906154438
+		CFLAGS+=" -fPIC"
+	elif [ "$TERMUX_ARCH" = "aarch64" ]; then
+		:
+	elif [ "$TERMUX_ARCH" = "x86_64" ]; then
+		:
+	else
+		termux_error_exit "Invalid arch '$TERMUX_ARCH' - support arches are 'arm', 'i686', 'aarch64', 'x86_64'"
+	fi
+
+	# Android 7 started to support DT_RUNPATH (but not DT_RPATH).
+	LDFLAGS+=" -Wl,--enable-new-dtags"
+
+	# Avoid linking extra (unneeded) libraries.
+	LDFLAGS+=" -Wl,--as-needed"
+
+	# Basic hardening.
+	CFLAGS+=" -fstack-protector-strong"
+	LDFLAGS+=" -Wl,-z,relro,-z,now"
+
+	if [ "$TERMUX_DEBUG_BUILD" = "true" ]; then
+		CFLAGS+=" -g3 -O1"
+		CPPFLAGS+=" -D_FORTIFY_SOURCE=2 -D__USE_FORTIFY_LEVEL=2"
+	else
+		CFLAGS+=" -O3"
+	fi
+
+	export CXXFLAGS="$CFLAGS"
+	CPPFLAGS+=" -I${TERMUX_PREFIX}/include"
+
+	# If libandroid-support is declared as a dependency, link to it explicitly:
+	if [ "$TERMUX_PKG_DEPENDS" != "${TERMUX_PKG_DEPENDS/libandroid-support/}" ]; then
+		LDFLAGS+=" -Wl,--no-as-needed,-landroid-support,--as-needed"
+	fi
+
+	export ac_cv_func_getpwent=no
+	export ac_cv_func_endpwent=yes
+	export ac_cv_func_getpwnam=no
+	export ac_cv_func_getpwuid=no
+	export ac_cv_func_sigsetmask=no
+	export ac_cv_c_bigendian=no
+
+	_setup_standalone_toolchain_ndk_r17c $OLD_NDK_TOOLCHAIN
+}
