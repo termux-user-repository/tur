a8d40e91282d695bedc12bd64b2d01a757b98304
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-08-09 22:10:44 +0800

    new package: ruby-2

---
+++ tur/ruby-2/ruby-is-ruby2.subpackage.sh
@@ -0,0 +1,17 @@
+TERMUX_SUBPKG_DESCRIPTION="Symlinks ruby to ruby2"
+TERMUX_SUBPKG_BREAKS="ruby"
+TERMUX_SUBPKG_CONFLICTS="ruby"
+TERMUX_SUBPKG_INCLUDE="
+bin/bundle
+bin/bundler
+bin/erb
+bin/gem
+bin/irb
+bin/racc
+bin/racc2y
+bin/rake
+bin/rdoc
+bin/ruby
+bin/y2racc
+lib/libruby.so
+"
---
+++ tur/ruby-2/ruby-ri-2.subpackage.sh
@@ -0,0 +1,7 @@
+TERMUX_SUBPKG_INCLUDE="
+bin/ri-2
+opt/ruby-2/share/ri-2
+opt/ruby-2/share/man/man1/ri-2.1.gz
+opt/ruby-2/bin/ri-2"
+TERMUX_SUBPKG_DESCRIPTION="Ruby documentation index"
+TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
