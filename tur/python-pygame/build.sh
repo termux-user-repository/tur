c2d9ac2d26f2bbfc380d498dbb50477c8fb960b6
ifurther <55025025+ifurther@users.noreply.github.com>
2022-08-12 19:40:56 +0800

    fix sdl2-config and src build

--- tur/python-pygame/build.sh
+++ tur/python-pygame/build.sh
@@ -6,8 +6,8 @@ TERMUX_PKG_VERSION=2.1.3.dev4
 TERMUX_PKG_SRCURL=https://github.com/pygame/pygame/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=bbe14ca0efc6f711e36cbf3cffc3801e0e72ba11d3beb7b2a2859805dd1d5fbe
 TERMUX_PKG_DEPENDS="freetype, portmidi, python, sdl2, sdl2-image, sdl2-mixer, sdl2-ttf"
-TERMUX_PKG_BUILD_DEPENDS="xorgproto ,sdl2 ,sdl2-image, sdl2-mixer, sdl2-ttf"
-
+TERMUX_PKG_BUILD_DEPENDS="xorgproto"
+TERMUX_PKG_BUILD_IN_SRC=true
 _PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
 
 TERMUX_PKG_RM_AFTER_INSTALL="
@@ -31,16 +31,16 @@ termux_step_pre_configure() {
         patch --silent -p1 < $TERMUX_PKG_BUILDER_DIR/setuptools-44.1.1-no-bdist_wininst.diff || :
         popd
 
-        build-pip install cython
-
         LDFLAGS+=" -lpython${_PYTHON_VERSION}"
-
-        python setup.py install --force
+	CPPFLAGS+="
+		-I$TERMUX_PREFIX/include/python${_PYTHON_VERSION}
+		-I$TERMUX_PREFIX/include/python${_PYTHON_VERSION}/cpython
+		"
 }
 
 termux_step_make_install() {
         export PYTHONPATH=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
-        export PATH=$TERMUX_PREFIX/bin
-        ls $TERMUX_PREFIX/bin | grep sdl2
+	export PATH=$PATH:$TERMUX_PREFIX/bin
+
         python setup.py install --force --prefix $TERMUX_PREFIX
 }
