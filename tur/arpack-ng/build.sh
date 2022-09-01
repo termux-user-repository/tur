7d19053d0edafed8a2f2124379478c5625dd6b31
Further <55025025+ifurther@users.noreply.github.com>
2022-09-01 16:06:06 +0800

    new package: arpack-ng (#54)
    Co-authored-by: Chongyun Lee <45286352+licy183@users.noreply.github.com>

---
+++ tur/arpack-ng/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/opencollab/arpack-ng
+TERMUX_PKG_DESCRIPTION="Collection of Fortran77 subroutines designed to solve large scale eigenvalue problems."
+TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION=3.8.0
+TERMUX_PKG_SRCURL=https://github.com/opencollab/arpack-ng/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=ada5aeb3878874383307239c9235b716a8a170c6d096a6625bfd529844df003d
+TERMUX_PKG_DEPENDS="openblas"
+
+source $TERMUX_SCRIPTDIR/common-files/setup_toolchain_ndk_r17c.sh
+source $TERMUX_SCRIPTDIR/common-files/setup_cmake_with_gcc.sh
+
+termux_step_pre_configure() {
+	_setup_toolchain_ndk_r17c_gcc_11
+	_override_configure_cmake_for_gcc
+}
