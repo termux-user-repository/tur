dbf079d6e04267476ad4c21b67418b0eb8f0216d
Ansh Dadwal <68729523+T-Dynamos@users.noreply.github.com>
2022-09-08 23:48:35 +0530

    fix cf

--- tur/cf/build.sh
+++ tur/cf/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://wiki.teamssix.com/cf
 TERMUX_PKG_DESCRIPTION="Cloud Exploitation Framework"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@UtermuxBlog"
-TERMUX_PKG_VERSION="0.2.4"
+TERMUX_PKG_VERSION="0.4.0"
 TERMUX_PKG_SRCURL=https://github.com/teamssix/cf/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=401779498ec69f937bddcf78f816ed99ced730a6858cd2f3062c712667d38376
+TERMUX_PKG_SHA256=c0d68d36851e0cb638b41da00e300f08032c5a378b89029498fc05966512ff4a
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
@@ -16,10 +16,11 @@ termux_step_pre_configure() {
 }
 
 termux_step_make() {
-        cd $TERMUX_PKG_SRCDIR/command/scan
+        cd $TERMUX_PKG_SRCDIR
 	go build -o cf
 }
 
 termux_step_make_install() {
-	install -Dm700 -t "${TERMUX_PREFIX}"/bin "$TERMUX_PKG_SRCDIR"/command/scan/cf
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin "$TERMUX_PKG_SRCDIR"/cf
 }
+
