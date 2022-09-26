42bc1c95ac96f60657aa6cf9314749d3504f6b70
flosnvjx <96630237+flosnvjx@users.noreply.github.com>
2022-09-26 12:01:16 +0800

    new package: feed2imap-go (#102)

---
+++ tur/feed2imap-go/build.sh
@@ -0,0 +1,23 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/Necoro/feed2imap-go
+TERMUX_PKG_DESCRIPTION="feed2imap(1) reimplemented in Go that aggregating RSS/Atom/jsonfeed into folders of your IMAP mailbox"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@flosnvjx"
+TERMUX_PKG_VERSION=1.5.1
+TERMUX_PKG_SRCURL="https://github.com/Necoro/feed2imap-go/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=10872fa1a9bfabdb53fd8935a8676d672dbb361af2d9db2b459cd8aa744f933b
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SUGGESTS="anacron"
+
+termux_step_make() {
+	termux_setup_golang
+	mkdir bin
+	go build -o ./bin -trimpath -ldflags "-s -w -X main.Version=${TERMUX_PKG_VERSION}"
+
+	sed -e 's%^cache: "feed\.cache"$%cache: "'$TERMUX_CACHE_DIR/feed2imap-go/feed'"%' -i config.yml.example
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin bin/*
+	install -Dm600 -t $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME README.* config.yml.example
+}
