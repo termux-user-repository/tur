5544f1b8e482fb0af01f173bd33cb1f12f81a718
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-09-30 00:22:16 +0800

    new package: gcc-default

---
+++ tur/gcc-default/gcc-default-12.subpackage.sh
@@ -0,0 +1,17 @@
+TERMUX_SUBPKG_DESCRIPTION="Use GNU Compiler Collections as default compiler suit (Version 12)"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=no
+TERMUX_SUBPKG_DEPENDS="gcc-12"
+TERMUX_SUBPKG_BREAKS="gcc-default-9, gcc-default-10, gcc-default-11"
+TERMUX_SUBPKG_CONFLICTS="gcc-default-9, gcc-default-10, gcc-default-11"
+TERMUX_SUBPKG_INCLUDE="share/$TERMUX_PKG_NAME/.placeholder-12"
+
+termux_step_create_subpkg_debscripts() {
+	local _GCC_DEFAULT_VERSION=12
+	echo "interest-noawait $TERMUX_PREFIX/bin/clang" >> ./triggers
+	for script_type in postinst prerm; do
+		sed "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" $TERMUX_PKG_BUILDER_DIR/$script_type.sh.in |
+			sed "s|@TERMUX_PKG_NAME@|$TERMUX_PKG_NAME|g" |
+			sed "s|@DEFAULT_GCC_VERSION@|$_GCC_DEFAULT_VERSION|g" > ./$script_type
+		chmod 0755 ./$script_type
+	done
+}
