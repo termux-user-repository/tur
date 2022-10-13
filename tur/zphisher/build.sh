82a72f545b5d0eaf37c129946be6f3b7d8aeacfd
Mr.Derek <74646789+E343IO@users.noreply.github.com>
2022-10-13 05:41:42 +0530

    zphisher: update to 2.3.2 (#123)

--- tur/zphisher/build.sh
+++ tur/zphisher/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/htr-tech/zphisher
 TERMUX_PKG_DESCRIPTION="An automated phishing tool with 30+ templates"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=2.3.1
+TERMUX_PKG_VERSION=2.3.2
 TERMUX_PKG_SRCURL=https://github.com/htr-tech/zphisher/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=77d20e300c2763ccd71de6fd7a46377c79a698625b50d9825d80b0f53fd24441
+TERMUX_PKG_SHA256=c2b167d7fea616792f6ad2fe970e804815e555d801ab8f2bb19c966ac9c1ca1c
 TERMUX_PKG_DEPENDS="curl, php, unzip, ncurses-utils, proot, resolv-conf"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
