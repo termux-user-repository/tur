bbf0ba1b8b4ec083e335dd38fb2df0a63564cb68
T-Dynamos <anshdadwal298@gmail.com>
2022-10-21 20:47:38 +0530

    new package : trigger-rally

---
+++ tur/trigger-rally/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE=https://trigger-rally.sourceforge.io
+TERMUX_PKG_DESCRIPTION="A free 3D rally car racing game"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@fervi"
+TERMUX_PKG_VERSION=0.6.6.1
+TERMUX_PKG_REVISION=10
+TERMUX_PKG_SRCURL=https://netcologne.dl.sourceforge.net/project/trigger-rally/trigger-${TERMUX_PKG_VERSION}/trigger-rally-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=7f086e13d142b8bb07e808ab9111e5553309c1413532f56c754ce3cfa060cb04
+TERMUX_PKG_DEPENDS="make, libphysfs, glew, libtinyxml2, openal-soft, openalut, sdl2, ndk-multilib, sdl2-image, pulseaudio, libxi"
+
+termux_step_pre_configure(){
+	export OPTIMS=""
+	cd src
+	make install
+}
