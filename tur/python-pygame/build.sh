c39518efa6e9b36ef6c2bd39501d8b04f0ba427b
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-10-30 16:24:19 +0800

    python-pygame: bump revision after python update

--- tur/python-pygame/build.sh
+++ tur/python-pygame/build.sh
@@ -2,13 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://pygame.org/
 TERMUX_PKG_DESCRIPTION="pygame is a python game library"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=2.1.3.dev4
+TERMUX_PKG_VERSION=2.1.3.dev8
 TERMUX_PKG_SRCURL=https://github.com/pygame/pygame/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=bbe14ca0efc6f711e36cbf3cffc3801e0e72ba11d3beb7b2a2859805dd1d5fbe
+TERMUX_PKG_SHA256=1d8a55b5a769eb9521b9d346688d4a060dca3e4497300efc78fb4021bc370199
 TERMUX_PKG_DEPENDS="x11-repo, freetype, portmidi, python, sdl2, sdl2-image, sdl2-mixer, sdl2-ttf"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_REVISION=3
 
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/
@@ -26,44 +25,12 @@ termux_step_pre_configure() {
 	. ${_CROSSENV_PREFIX}/bin/activate
 
 	LDFLAGS+=" -lpython${_PYTHON_VERSION}"
+	build-pip install wheel
 }
 
 termux_step_make_install() {
 	export PYTHONPATH=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
-	export PATH=$PATH:$TERMUX_PREFIX/bin
+	export PATH="$PATH:$TERMUX_PREFIX/bin"
 
-	python setup.py install --force --prefix $TERMUX_PREFIX
-
-	pushd $PYTHONPATH
-	_PYGAME_EGGDIR=
-	for f in pygame-${TERMUX_PKG_VERSION}-py${_PYTHON_VERSION}-linux-*.egg; do
-		if [ -f "$f" ]|| [ -d "$f" ]; then
-			_PYGAME_EGGDIR="$f"
-			break
-		fi
-	done
-	test -n "${_PYGAME_EGGDIR}"
-	popd
-}
-
-termux_step_post_make_install() {
-	# Delete the easy-install related files, since we use postinst/prerm to handle it.
-	pushd $TERMUX_PREFIX
-	rm -rf lib/python${_PYTHON_VERSION}/site-packages/__pycache__
-	rm -rf lib/python${_PYTHON_VERSION}/site-packages/easy-install.pth
-	rm -rf lib/python${_PYTHON_VERSION}/site-packages/site.py
-	popd
-}
-
-termux_step_create_debscripts() {
-	cat <<- EOF > ./postinst
-	#!$TERMUX_PREFIX/bin/sh
-	echo "Installing pygame..."
-	echo "./${_PYGAME_EGGDIR}" >> $TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages/easy-install.pth
-	EOF
-
-	cat <<- EOF > ./prerm
-	#!$TERMUX_PREFIX/bin/sh
-	sed -i "/\.\/${_PYGAME_EGGDIR//./\\.}/d" $TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages/easy-install.pth
-	EOF
+	pip install --no-deps . --prefix $TERMUX_PREFIX
 }
