fb7fca433810f71005d0e754c5480f981caf0367
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-10-30 16:21:12 +0800

    python-cairo: bump revision after python update

--- tur/python-cairo/build.sh
+++ tur/python-cairo/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="This package contains modules that allow you to use the 
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@fervi"
 TERMUX_PKG_VERSION=1.21.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/pygobject/pycairo/releases/download/v${TERMUX_PKG_VERSION}/pycairo-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=251907f18a552df938aa3386657ff4b5a4937dde70e11aa042bc297957f4b74b
 TERMUX_PKG_DEPENDS="python, libcairo"
@@ -21,56 +22,10 @@ termux_step_configure() {
 	. ${_CROSSENV_PREFIX}/bin/activate
 
 	LDFLAGS+=" -lpython${_PYTHON_VERSION}"
-}
-
-termux_step_make() {
-	python setup.py install --force
+	build-pip install wheel
 }
 
 termux_step_make_install() {
 	export PYTHONPATH=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
-	python setup.py install --force --prefix $TERMUX_PREFIX
-
-	pushd $PYTHONPATH
-	_EGGDIR=
-	for f in pycairo-${TERMUX_PKG_VERSION}-py${_PYTHON_VERSION}-linux-*.egg; do
-		if [ -d "$f" ]; then
-			_EGGDIR="$f"
-			break
-		fi
-	done
-	test -n "${_EGGDIR}" || (termux_error_exit "Failed to find the egg file/directory, does the .egg-info file exists?")
-	# XXX: Fix the EXT_SUFFIX. More investigation is needed to find the underlying cause.
-	pushd "${_EGGDIR}"
-	local old_suffix=".cpython-310-$TERMUX_HOST_PLATFORM.so"
-	local new_suffix=".cpython-310.so"
-
-	find . \
-		-name '*'"$old_suffix" \
-		-exec sh -c '_f="{}"; mv -- "$_f" "${_f%'"$old_suffix"'}'"$new_suffix"'"' \;
-	popd
-	popd
-}
-
-termux_step_post_make_install() {
-	# Delete the easy-install related files, since we use postinst/prerm to handle it.
-	pushd $TERMUX_PREFIX
-	rm -rf lib/python${_PYTHON_VERSION}/site-packages/__pycache__
-	rm -rf lib/python${_PYTHON_VERSION}/site-packages/easy-install.pth
-	rm -rf lib/python${_PYTHON_VERSION}/site-packages/site.py
-	popd
-}
-
-termux_step_create_debscripts() {
-	cat <<- EOF > ./postinst
-	#!$TERMUX_PREFIX/bin/sh
-	echo "Installing $TERMUX_PKG_NAME..."
-	echo "./${_EGGDIR}" >> $TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages/easy-install.pth
-	EOF
-
-	cat <<- EOF > ./prerm
-	#!$TERMUX_PREFIX/bin/sh
-	echo "Removing $TERMUX_PKG_NAME..."
-	sed -i "/\.\/${_EGGDIR//./\\.}/d" $TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages/easy-install.pth
-	EOF
+	pip install --no-deps . --prefix $TERMUX_PREFIX
 }
