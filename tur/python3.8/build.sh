cdabf3d807baaaec97803e4fdaa4544cad94d6ac
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-11-22 14:35:44 +0800

    new package: python3.8

---
+++ tur/python3.8/build.sh
@@ -0,0 +1,111 @@
+TERMUX_PKG_HOMEPAGE=https://python.org/
+TERMUX_PKG_DESCRIPTION="Python programming language intended to enable clear programs (version 3.8)"
+TERMUX_PKG_LICENSE="PythonPL"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+_MAJOR_VERSION=3.8
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.15
+TERMUX_PKG_SRCURL=https://www.python.org/ftp/python/${TERMUX_PKG_VERSION}/Python-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=5114fc7918a2a5e20eb5aac696b30c36f412c6ef24b13f5c9eb9e056982d9550
+TERMUX_PKG_DEPENDS="gdbm, libandroid-posix-semaphore, libandroid-support, libbz2, libcrypt, libffi, liblzma, libsqlite, ncurses, ncurses-ui-libs, openssl, readline, zlib"
+TERMUX_PKG_RECOMMENDS="clang, make, pkg-config"
+TERMUX_PKG_SUGGESTS="python${_MAJOR_VERSION}-tkinter"
+TERMUX_PKG_BREAKS="python2 (<= 2.7.15)"
+TERMUX_PKG_HOSTBUILD=true
+# Set ac_cv_func_wcsftime=no to avoid errors such as "character U+ca0025 is not in range [U+0000; U+10ffff]"
+# when executing e.g. "from time import time, strftime, localtime; print(strftime(str('%Y-%m-%d %H:%M'), localtime()))"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_file__dev_ptmx=yes ac_cv_file__dev_ptc=no ac_cv_func_wcsftime=no"
+# Avoid trying to include <sys/timeb.h> which does not exist on android-21:
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_ftime=no"
+# Avoid trying to use AT_EACCESS which is not defined:
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_faccessat=no"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --build=$TERMUX_BUILD_TUPLE --with-system-ffi --without-ensurepip"
+# Hard links does not work on Android 6:
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_linkat=no"
+# Do not assume getaddrinfo is buggy when cross compiling:
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_buggy_getaddrinfo=no"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --enable-loadable-sqlite-extensions"
+# Fix https://github.com/termux/termux-packages/issues/2236:
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_little_endian_double=yes"
+# Force enable posix semaphores.
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_posix_semaphores_enabled=yes"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_sem_open=yes"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_sem_timedwait=yes"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_sem_getvalue=yes"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_sem_unlink=yes"
+# Force enable posix shared memory.
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_shm_open=yes"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_shm_unlink=yes"
+
+TERMUX_PKG_RM_AFTER_INSTALL="
+lib/python${_MAJOR_VERSION}/test
+lib/python${_MAJOR_VERSION}/*/test
+lib/python${_MAJOR_VERSION}/*/tests
+"
+
+termux_step_pre_configure() {
+	export PATH="$TERMUX_PKG_HOSTBUILD_DIR:$PATH"
+	# -O3 gains some additional performance on at least aarch64.
+	CFLAGS="${CFLAGS/-Oz/-O3}"
+	# Needed when building with clang, as setup.py only probes
+	# gcc for include paths when finding headers for determining
+	# if extension modules should be built (specifically, the
+	# zlib extension module is not built without this):
+	CPPFLAGS+=" -I$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/include"
+	LDFLAGS+=" -L$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib"
+	if [ $TERMUX_ARCH = x86_64 ]; then LDFLAGS+=64; fi
+
+	if [ "$TERMUX_ON_DEVICE_BUILD" = "true" ]; then
+		# Python's configure script fails with
+		#    Fatal: you must define __ANDROID_API__
+		# if __ANDROID_API__ is not defined.
+		CPPFLAGS+=" -D__ANDROID_API__=$(getprop ro.build.version.sdk)"
+	fi
+}
+
+termux_step_make_install() {
+	make altinstall
+	ln -sfr $TERMUX_PREFIX/bin/python3.8 $TERMUX_PREFIX/bin/python
+	ln -sfr $TERMUX_PREFIX/bin/python3.8 $TERMUX_PREFIX/bin/python3
+}
+
+termux_step_post_massage() {
+	# Verify that desired modules have been included:
+	for module in _bz2 _curses _lzma _sqlite3 _ssl _tkinter zlib; do
+		if [ ! -f "${TERMUX_PREFIX}/lib/python${_MAJOR_VERSION}/lib-dynload/${module}".*.so ]; then
+			termux_error_exit "Python module library $module not built"
+		fi
+	done
+}
+
+termux_step_create_debscripts() {
+	# Post-installation script for setting up pip.
+	cat <<- POSTINST_EOF > ./postinst
+	#!$TERMUX_PREFIX/bin/sh
+
+	echo "Setting up pip..."
+
+	${TERMUX_PREFIX}/bin/python${_MAJOR_VERSION} -m ensurepip --altinstall --upgrade
+
+	exit 0
+	POSTINST_EOF
+
+	# Pre-rm script to cleanup runtime-generated files.
+	cat <<- PRERM_EOF > ./prerm
+	#!$TERMUX_PREFIX/bin/sh
+
+	if [ "$TERMUX_PACKAGE_FORMAT" != "pacman" ] && [ "\$1" != "remove" ]; then
+	    exit 0
+	fi
+
+	echo "Uninstalling python modules..."
+	pip${_MAJOR_VERSION} freeze 2>/dev/null | xargs pip${_MAJOR_VERSION} uninstall -y >/dev/null 2>/dev/null
+	rm -f $TERMUX_PREFIX/bin/pip${_MAJOR_VERSION} $TERMUX_PREFIX/bin/easy_install-${_MAJOR_VERSION}
+
+	echo "Deleting remaining files from site-packages..."
+	rm -Rf $TERMUX_PREFIX/lib/python${_MAJOR_VERSION}/site-packages/*
+
+	exit 0
+	PRERM_EOF
+
+	chmod 0755 postinst prerm
+}
