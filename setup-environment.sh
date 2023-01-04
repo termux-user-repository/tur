28318ba8090d0dfac74a70b445ee449da1c1c3cf
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-01-04 22:23:24 +0800

    ci: update setup-environment.sh
    [skip ci]

--- setup-environment.sh
+++ setup-environment.sh
@@ -10,13 +10,13 @@ cd $basedir
 # Checkout the master branch of termux/termux-packages
 if [ -d "./termux-packages" ] && [ -d "./termux-packages/.git" ]; then
 	echo "Pulling termux-packages..."
-    pushd ./termux-packages
-    git reset --hard origin/master
-    git pull --rebase
-    popd
+	pushd ./termux-packages
+	git reset --hard origin/master
+	git pull --rebase
+	popd
 else
-    rm -rf ./termux-packages
-    git clone https://github.com/termux/termux-packages.git
+	rm -rf ./termux-packages
+	git clone https://github.com/termux/termux-packages.git
 fi
 
 # Remove old stuffs
@@ -26,8 +26,13 @@ rm -rf {ndk-patches,packages,x11-packages,root-packages,scripts,build-all.sh,bui
 mv ./termux-packages/{ndk-patches,packages,x11-packages,root-packages,scripts,build-all.sh,build-package.sh,clean.sh} ./
 
 # Apply script patches.
-patch --silent -p1 < ./common-files/script-patches.patch
+shopt -s nullglob
+_patch=
+for _patch in ./common-files/building-system-patches/*.patch; do
+	patch --silent -p1 < $_patch
+done
+unset _patch
+shopt -u nullglob
 
 # Remove files
-rm build-package.sh.orig
-
+rm -f build-package.sh.orig
