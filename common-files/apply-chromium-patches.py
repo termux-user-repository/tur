eb3124b225c0a52487c06df81bc5696573e26098
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-01-06 12:10:48 +0800

    chromium-patches: add patches for 4961
    Needed by electron-19.1.8

--- common-files/apply-chromium-patches.py
+++ common-files/apply-chromium-patches.py
@@ -2,31 +2,118 @@
 # apply-chromium-patches.py - Apply patches for chromium based on versions.
 
 import argparse
+from collections import defaultdict
+import heapq
 import json
 import logging
 import os
 import subprocess
-from subprocess import check_call
 
 PATCHES_DIR = os.path.join(os.path.dirname(__file__), "chromium-patches")
 MATADATA_FILE = os.path.join(PATCHES_DIR, "metadata.json")
 
 logger = logging.getLogger(__name__)
 
-def apply_patch(patch_file):
+class MetadataRangeConflictError(Exception):
+  def __init__(self, a, b):
+    msg = (f"Conlict is detected between {a.path} and {b.path}. " +
+                       f"{a.desc()}, but {b.desc()}.")
+    super().__init__(msg)
+
+class MetadataRangeGapError(Exception):
+  def __init__(self, k, a, b):
+    super().__init__(f"Gap is detected in {k}. {a.desc()}, but {b.desc()}.")
+
+class MeatadataRange:
+  def __init__(self, path, start, end):
+    self.path = path
+    self.start = start
+    self.end = end
+    if start > end:
+      raise IndexError(f"Invalid range in {path}, {start} > {end}.")
+
+  def __eq__(self, other):
+    return self.start == other.start and self.end == other.end
+
+  def __lt__(self, other):
+    if self.end < other.start:
+      return True
+    elif other.end < self.start:
+      return False
+    raise MetadataRangeConflictError(self, other)
+
+  def desc(self):
+    return f"`{self.path}` has range [{self.start}, {self.end}]"
+
+  def __repr__(self):
+    return f"MeatadataRange(path={repr(self.path)}, start={self.start}, end={self.end})"
+
+class UniqueMeatadataRangeHeapQueue:
+  def __init__(self):
+    self._queue = []
+
+  def push(self, value):
+    for _v in self._queue:
+      if _v == value:
+        raise MetadataRangeConflictError(_v, value)
+    heapq.heappush(self._queue, value)
+
+  def pop(self):
+    return heapq.heappop(self._queue)
+
+  def __len__(self):
+    return len(self._queue)
+
+class MetadataChecker:
+  def __init__(self):
+    self._ranges = defaultdict(UniqueMeatadataRangeHeapQueue)
+  
+  def set_range(self, path, start, end):
+    name, _ = path.split("/")
+    r = MeatadataRange(path, start, end)
+    self._ranges[name].push(r)
+
+  def ranges_check(self):
+    for k, v in self._ranges.items():
+      if len(v) < 2: continue
+      a = v.pop()
+      while len(v) > 0:
+        b = v.pop()
+        if a.end + 1 != b.start:
+          raise MetadataRangeGapError(k, a, b)
+        a = b
+
+def check_metadata(metadata_dict):
+  checker = MetadataChecker()
+  for k, v in metadata_dict.items():
+    start = v.get("start", 0)
+    end = v.get("end", float("+inf"))
+    checker.set_range(k, start, end)
+  checker.ranges_check()
+  return metadata_dict
+
+def apply_patch(patch_file, dry_run):
   try:
-    subprocess.check_call(["patch", "-s", "--dry-run", "-t", "-p1", "-i", patch_file], stdin=subprocess.DEVNULL)
+    subprocess.check_call(
+      ["patch", "-s", "--dry-run", "-t", "-p1", "-i", patch_file],
+      stdin=subprocess.DEVNULL
+    )
   except:
     return False
+  if dry_run: return True
   subprocess.check_call(["patch", "-s", "-p1", "-t", "-i", patch_file])
   return True
 
-def revert_patch(patch_file):
+def revert_patch(patch_file, dry_run):
   try:
-    check_call(["patch", "-s", "-R", "-f", "--dry-run", "-p1", "-i", patch_file], stdin=subprocess.DEVNULL)
+    subprocess.check_call(
+      ["patch", "-s", "-R", "-f", "--dry-run", "-p1", "-i", patch_file],
+      stdin=subprocess.DEVNULL
+    )
   except:
     return False
-  check_call(["patch", "-s", "-R", "-f", "-p1", "-i", patch_file])
+  if dry_run: return True
+  subprocess.check_call(["patch", "-s", "-R", "-f", "-p1", "-i", patch_file])
   return True
 
 def parse_chromium_version(version_str, p):
@@ -42,13 +129,14 @@ def parse_metadata(filepath):
     with open(filepath, "r") as fp:
       res = json.load(fp)
       assert isinstance(res, dict)
-      return res
-  except:
+      return check_metadata(res)
+  except Exception as e:
     logger.error("Invalid json format on metadata.json")
-    exit(1)
+    raise e
 
 def execute(args, p):
   is_revert_mode = args.revert
+  is_dry_run_mode = args.dry_run
   _, _, build_v, patch_v = parse_chromium_version(args.CHROMIUM_VERSION, p)
   logger.debug("Got chromium version %s", args.CHROMIUM_VERSION)
   metadata = parse_metadata(MATADATA_FILE)
@@ -60,7 +148,7 @@ def execute(args, p):
       ope_func = revert_patch if is_revert_mode else apply_patch
       ope_str = "revert" if is_revert_mode else "apply"
       logger.info("%sing %s...", ope_str.capitalize(), patch_path)
-      if not ope_func(os.path.join(PATCHES_DIR, patch_path)):
+      if not ope_func(os.path.join(PATCHES_DIR, patch_path), is_dry_run_mode):
         logger.error("Failed to %s %s: ignoring", ope_str, patch_path)
 
 def main():
@@ -82,12 +170,19 @@ def main():
     help="Set to revert mode.",
   )
   p.add_argument(
+    "--dry-run",
+    action="store_true",
+    dest="dry_run",
+    default=False,
+    help="Set to dry-run mode.",
+  )
+  p.add_argument(
     "-C",
     "--chdir",
     action="store",
     dest="workdir",
     default=None,
-    help="Set to revert mode.",
+    help="Change workdir.",
   )
   p.add_argument(
     "CHROMIUM_VERSION",
@@ -104,4 +199,4 @@ def main():
   execute(args, p)
 
 if __name__ == '__main__':
-    main()
+  main()
