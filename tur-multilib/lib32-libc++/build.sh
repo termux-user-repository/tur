114a07263c286567f86c6b6a41e8c8dc22d81c27
Uchiha Kakashi <45286352+licy183@users.noreply.github.com>
2023-01-17 01:28:38 +0800

    new package: lib32-libc++

---
+++ tur-multilib/lib32-libc++/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/termux-user-repository/tur
+TERMUX_PKG_DESCRIPTION="A metapackage that provides libc++ multilib library"
+TERMUX_PKG_LICENSE="Public Domain"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION=25b
+TERMUX_PKG_DEPENDS="lib32-libc++-shared | ndk-multilib"
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_SKIP_SRC_EXTRACT=true
+TERMUX_PKG_METAPACKAGE=true
