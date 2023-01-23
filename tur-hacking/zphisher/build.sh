a428e37899def5c7f1ca62e3c7f702d58433dc91
Mustakim Ahmed Sifat <67186139+BDhackers009@users.noreply.github.com>
2023-01-23 14:27:52 +0600

    zphisher: update to 2.3.5

--- tur-hacking/zphisher/build.sh
+++ tur-hacking/zphisher/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/htr-tech/zphisher
 TERMUX_PKG_DESCRIPTION="An automated phishing tool with 30+ templates"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=2.3.2
+TERMUX_PKG_VERSION=2.3.5
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/htr-tech/zphisher/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c2b167d7fea616792f6ad2fe970e804815e555d801ab8f2bb19c966ac9c1ca1c
+TERMUX_PKG_SHA256=1c4fb7f0f35987468c0fc1683033bf68cf8292b6a5789aeced26b91c1f8c6e0a
 TERMUX_PKG_DEPENDS="curl, php, unzip, ncurses-utils, proot, resolv-conf"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
