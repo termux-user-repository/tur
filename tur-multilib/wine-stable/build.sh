3b58177a1ae2af7892a54f6c7a3f2b0dafd6e559
Uchiha Kakashi <45286352+licy183@users.noreply.github.com>
2023-01-30 07:27:50 +0800

    new package: wine-stable (#244)

---
+++ tur-multilib/wine-stable/build.sh
@@ -0,0 +1,72 @@
+TERMUX_PKG_HOMEPAGE=https://www.winehq.org/
+TERMUX_PKG_DESCRIPTION="A compatibility layer for running Windows programs"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_LICENSE_FILE="\
+LICENSE
+LICENSE.OLD
+COPYING.LIB"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION=7.0.1
+TERMUX_PKG_SRCURL=https://dl.winehq.org/wine/source/${TERMUX_PKG_VERSION:0:3}/wine-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=807caa78121b16250f240d2828a07ca4e3c609739e5524ef0f4cf89ae49a816c
+TERMUX_PKG_DEPENDS="libc++, libgmp, libgnutls"
+TERMUX_PKG_NO_STATICSPLIT=true
+TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS="
+--without-x
+--disable-tests
+"
+
+# TODO: Enable X
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+enable_wineandroid_drv=no
+exec_prefix=$TERMUX_PREFIX
+--without-x
+--without-vulkan
+--with-wine-tools=$TERMUX_PKG_HOSTBUILD_DIR
+--enable-nls
+--disable-tests
+"
+
+_setup_llvm_mingw_toolchain() {
+	# LLVM-mingw's version number must not be the same as the NDK's.
+	local _llvm_mingw_version=13
+	local _version="20211002"
+	local _url="https://github.com/mstorsjo/llvm-mingw/releases/download/$_version/llvm-mingw-$_version-ucrt-ubuntu-18.04-x86_64.tar.xz"
+	local _path="$TERMUX_PKG_CACHEDIR/$(basename $_url)"
+	local _sha256sum=30e9400783652091d9278ce21e5c170d01a5f44e4f1a25717b63cd9ad9fbe13b
+	termux_download $_url $_path $_sha256sum
+	local _extract_path="$TERMUX_PKG_CACHEDIR/llvm-mingw-toolchain-$_llvm_mingw_version"
+	if [ ! -d "$_extract_path" ]; then
+		mkdir -p "$_extract_path"-tmp
+		tar -C "$_extract_path"-tmp --strip-component=1 -xf "$_path"
+		mv "$_extract_path"-tmp "$_extract_path"
+	fi
+	export PATH="$PATH:$_extract_path/bin"
+}
+
+termux_step_host_build() {
+	# Setup llvm-mingw toolchain
+	_setup_llvm_mingw_toolchain
+
+	# Make host wine-tools
+	# TODO: Find out how to make wine-tools only
+	(unset sudo; sudo apt update; sudo apt install libfreetype-dev:i386 -yqq)
+	"$TERMUX_PKG_SRCDIR/configure" ${TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS}
+	make -j "$TERMUX_MAKE_PROCESSES"
+}
+
+termux_step_pre_configure() {
+	# Setup llvm-mingw toolchain
+	_setup_llvm_mingw_toolchain
+
+	# Fix overoptimization
+	CFLAGS="${CFLAGS/-Oz/}"
+	CXXFLAGS="${CFLAGS/-Oz/}"
+
+	# Enable win64 on 64-bit arches.
+	# TODO: Enable win32 after TUR has full support for mutilib 
+	if [ "$TERMUX_ARCH_BITS" = 64 ]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --enable-win64"
+	fi
+}
