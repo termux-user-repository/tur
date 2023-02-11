5ed15dc2645c85ae68ce1fdca5b4ad2516a5c7d9
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-02-11 21:10:54 +0800

    python-pandas: change maintainer

--- tur/python-pandas/build.sh
+++ tur/python-pandas/build.sh
@@ -2,7 +2,7 @@
 TERMUX_PKG_HOMEPAGE=https://pandas.pydata.org/
 TERMUX_PKG_DESCRIPTION="Powerful Python data analysis toolkit"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=1.5.3
 TERMUX_PKG_SRCURL=https://github.com/pandas-dev/pandas/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d8abf9c2bf33cac75b28f32c174c29778414eb249e5e2ccb69b1079b97a8fc66
