f9acd29ad57bb8d14bda8e1e7a2b85a47edef636
Ansh Dadwal <68729523+T-Dynamos@users.noreply.github.com>
2023-02-15 07:27:55 +0530

    bump(vhs): 0.2.0

--- tur/vhs/build.sh
+++ tur/vhs/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/charmbracelet/vhs
 TERMUX_PKG_DESCRIPTION="Your CLI home video recorder"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=0.1.1
+TERMUX_PKG_VERSION=0.2.0
 TERMUX_PKG_SRCURL=https://github.com/charmbracelet/vhs/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d5d6dddd8f9fd2beb6d1ea232efaa1c9dbfa4e53011d2aebdbe830d952665776
+TERMUX_PKG_SHA256=3be752dafa1d5637cd3feb5ea72f9396d2ffb7d559a10eff26f430d4a7540689
 TERMUX_PKG_DEPENDS="ttyd"
 TERMUX_PKG_BUILD_IN_SRC=true
 
