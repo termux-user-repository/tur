fca616ec6723cd9122296d54c754533f41cf62d2
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-02-25 14:58:13 +0800

    mesa-zink: enable lavapipe

--- tur/mesa-zink/mesa-zink-vulkan-icd-freedreno.subpackage.sh
+++ tur/mesa-zink/mesa-zink-vulkan-icd-freedreno.subpackage.sh
@@ -2,5 +2,5 @@ TERMUX_SUBPKG_DESCRIPTION="Mesa's Freedreno Vulkan ICD"
 TERMUX_SUBPKG_DEPENDS="vulkan-loader-generic"
 TERMUX_SUBPKG_INCLUDE="
 lib/libvulkan_freedreno.so
-share/vulkan/icd.d/freedreno_icd.aarch64.json
+share/vulkan/icd.d/freedreno_icd.*.json
 "
