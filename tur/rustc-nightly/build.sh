54dcde483d93cb1c67161d6ccfc3e080074a58b4
Ludea <ludovicw35@hotmail.com>
2023-03-01 07:58:25 +0100

    revert

--- tur/rustc-nightly/build.sh
+++ tur/rustc-nightly/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Rust compiler and utilities (nightly version)"
 TERMUX_PKG_DEPENDS="libc++, clang, openssl, lld, zlib, libllvm"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_REVISION=2
-TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="1.67.1-nightly"
-TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/rustc-nightly-src.tar.xz
-TERMUX_PKG_SHA256=345395db595fdd79ce651e587d86cc0f81b7b60a5507c3ec4fa32b19cf99bc4f
+_DATE="2023-02-27"
+TERMUX_PKG_VERSION="1.67.1-${_DATE//-/.}-nightly"
+TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/$_DATE/rustc-nightly-src.tar.xz
+TERMUX_PKG_SHA256=ceb53d6a8cc18434efa3093e3debe5484dde8201dcba4fdbe83e83f32b8dad74
 TERMUX_PKG_RM_AFTER_INSTALL="bin/llvm-* bin/llc bin/opt"
 
 termux_step_pre_configure() {
