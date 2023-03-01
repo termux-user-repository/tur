e8a6ef024b849b45c248bcdde5e6dd9addab85c3
Ludea <ludovicw35@hotmail.com>
2023-03-01 08:03:22 +0100

    include src

--- tur/rustc-nightly/rust-src-nightly.subpackage.sh
+++ tur/rustc-nightly/rust-src-nightly.subpackage.sh
@@ -1,3 +1,7 @@
 TERMUX_SUBPKG_DESCRIPTION="rust src"
 TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
 TERMUX_SUBPKG_INCLUDE="opt/rust-nightly/lib/rustlib/src"
+_DATE="2023-02-27"
+TERMUX_PKG_VERSION="1.67.1-${_DATE//-/.}-nightly"
+TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/$_DATE/rustc-nightly-src.tar.xz
+TERMUX_PKG_SHA256=ceb53d6a8cc18434efa3093e3debe5484dde8201dcba4fdbe83e83f32b8dad74
