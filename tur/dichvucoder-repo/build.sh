ab0aab480d176f04918bac3079c27ee7da79c47e
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-03-07 14:23:21 +0800

    dichvucoder-repo: bump to 1.0.1

--- tur/dichvucoder-repo/build.sh
+++ tur/dichvucoder-repo/build.sh
@@ -2,11 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://repo.dichvucoder.com
 TERMUX_PKG_DESCRIPTION="Community repository maintained by Dichvucoder"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@Dichvucoder"
-TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_VERSION=1.0.1
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
+	local _folder
+	for _folder in bin etc include lib lib64 libexec share tmp vars; do
+		mkdir -p $TERMUX_PREFIX/Dichvucoder/$_folder
+		mkdir -p $TERMUX_PREFIX/dichvucoder/$_folder
+		touch $TERMUX_PREFIX/dichvucoder/$_folder/.placeholder
+		touch $TERMUX_PREFIX/Dichvucoder/$_folder/.placeholder
+	done
+	install -Dm600 $TERMUX_PKG_BUILDER_DIR/repo.json $TERMUX_PREFIX/dichvucoder/etc/
+	install -Dm600 $TERMUX_PKG_BUILDER_DIR/repo.json $TERMUX_PREFIX/Dichvucoder/etc/
+
 	mkdir -p $TERMUX_PREFIX/etc/apt/sources.list.d
 	echo "deb https://repo.dichvucoder.com dvc-packages dvc" > $TERMUX_PREFIX/etc/apt/sources.list.d/dichvucoder.list
 	## dvc gpg key
