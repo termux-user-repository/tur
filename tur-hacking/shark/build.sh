1a53e6c58641757612f6c8d3023e6ee2ef6e701a
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-03-23 23:41:28 +0800

    fix(tur-hacking/shark): Fixed 7z extraction

--- tur-hacking/shark/build.sh
+++ tur-hacking/shark/build.sh
@@ -1,21 +1,18 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/Bhaviktutorials/shark
+TERMUX_PKG_HOMEPAGE=https://github.com/E343IO/shark
 TERMUX_PKG_DESCRIPTION="🦈Future Of Phishing With less delay!!🦈"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@E343IO"
-TERMUX_PKG_VERSION=2.5.1.0-beta
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/Bhaviktutorials/shark/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c57958d775af9119f28f36c05d713092aa0749e8d0e72738290cd538fd762642
+TERMUX_PKG_VERSION=2.5.1.0-git
+_GIT_COMMIT=490e7a572a0a1b1b5a7768b20782771314917365
+TERMUX_PKG_SRCURL=https://github.com/E343IO/shark/archive/$_GIT_COMMIT.zip
+TERMUX_PKG_SHA256=ebda6793b75eaeaacf8b991daf6c002e10dbd41ec51017499f6dc442c3598163
 TERMUX_PKG_DEPENDS="curl, php, unzip, p7zip, ncurses-utils, proot, resolv-conf, sox, ffmpeg, dialog"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="curl, php, unzip, p7zip, ncurses-utils, proot, resolv-conf, sox, ffmpeg, dialog"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install(){
-	env -i PATH="$PATH" sudo apt update
-	env -i PATH="$PATH" sudo apt install -yq p7zip-full
 	mkdir $TERMUX_PREFIX/share/shark
 	cd $TERMUX_PKG_SRCDIR
-	cp shark $TERMUX_PREFIX/bin/shark
-	cp -fr file setup shark  $TERMUX_PREFIX/share/shark/
-	7z x file && rm -rf file
-	chmod +x $TERMUX_PREFIX/bin/shark
+	install -Dm700 -t $TERMUX_PREFIX/bin shark
+	install -Dm600 -t $TERMUX_PREFIX/share/shark file setup shark
 }
