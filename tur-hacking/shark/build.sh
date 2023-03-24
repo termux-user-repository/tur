cf62976f6fa31748071762c07b6a93939ac0a26a
Mr.Derek <74646789+E343IO@users.noreply.github.com>
2023-03-24 07:10:31 +0530

    bump(tur-hacking/shark): 2.5.1 (#329)

--- tur-hacking/shark/build.sh
+++ tur-hacking/shark/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://github.com/E343IO/shark
 TERMUX_PKG_DESCRIPTION="🦈Future Of Phishing With less delay!!🦈"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@E343IO"
-TERMUX_PKG_VERSION=2.5.1.0-git
-_GIT_COMMIT=490e7a572a0a1b1b5a7768b20782771314917365
-TERMUX_PKG_SRCURL=https://github.com/E343IO/shark/archive/$_GIT_COMMIT.zip
-TERMUX_PKG_SHA256=ebda6793b75eaeaacf8b991daf6c002e10dbd41ec51017499f6dc442c3598163
-TERMUX_PKG_DEPENDS="curl, php, unzip, p7zip, ncurses-utils, proot, resolv-conf, sox, ffmpeg, dialog"
-TERMUX_PKG_ANTI_BUILD_DEPENDS="curl, php, unzip, p7zip, ncurses-utils, proot, resolv-conf, sox, ffmpeg, dialog"
+TERMUX_PKG_VERSION=2.5.1
+#_GIT_COMMIT=490e7a572a0a1b1b5a7768b20782771314917365
+TERMUX_PKG_SRCURL=https://github.com/E343IO/shark/archive/refs/tags/v${TERMUX_PKG_VERSION}.zip
+TERMUX_PKG_SHA256=b6cf74584f85acd9a110c04f398b1498972159c4d70a617abd0444e5c777f382
+TERMUX_PKG_DEPENDS="curl, wget, php, unzip, p7zip, ncurses-utils, proot, resolv-conf, sox, ffmpeg, dialog"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="curl, wget, php, unzip, p7zip, ncurses-utils, proot, resolv-conf, sox, ffmpeg, dialog"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install(){
