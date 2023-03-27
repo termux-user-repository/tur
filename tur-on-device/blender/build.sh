ed7687664dc1005583232ccdd307bd4e15db009e
T-Dynamos <anshdadwal298@gmail.com>
2023-03-27 22:13:52 +0530

    fix(tur-on-device/blender): minor fixes

--- tur-on-device/blender/build.sh
+++ tur-on-device/blender/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_MAINTAINER="@T-Dynamos"
 BLENDER_MAJOR_VERSION=3 
 BLENDER_MINOR_VERSION=6
 BLENDER_VERSION_STAGE=alpha
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_VERSION=${BLENDER_MAJOR_VERSION}.${BLENDER_MINOR_VERSION}-${BLENDER_VERSION_STAGE}
 TERMUX_PKG_SRCURL=git+https://github.com/blender/blender
 TERMUX_PKG_GIT_BRANCH=main
@@ -12,7 +13,8 @@ TERMUX_PKG_GIT_BRANCH=main
 # Blender does not support 32bit
 TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
-TERMUX_PKG_DEPENDS="libpng, libtiff, python, python-pip, python-numpy, openexr, desktop-file-utils, potrace, shared-mime-info, hicolor-icon-theme, glew, openjpeg, freetype, ffmpeg, fftw, alembic, libsndfile, ptex, sdl2, libspnav, openal-soft, opencolorio, libblosc, sse2neon, libandroid-execinfo, brotli, libepoxy, openimageio, pybind11, openvdb, libraw"
+TERMUX_PKG_DEPENDS="libpng, libtiff, python, python-pip, python-numpy, openexr, desktop-file-utils, potrace, shared-mime-info, hicolor-icon-theme, glew, openjpeg, freetype, ffmpeg, fftw, alembic, libsndfile, ptex, sdl2, libspnav, openal-soft, opencolorio, libblosc, sse2neon, libandroid-execinfo, brotli, libepoxy, openimageio, pybind11, openvdb, libraw, libpugixml"
+TERMUX_PKG_BUILD_DEPENDS="subversion"
 TERMUX_PKG_PYTHON_COMMON_DEPS="requests, zstandard, Cython"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DPYTHON_LIBRARY=$TERMUX_PREFIX/lib/python$TERMUX_PYTHON_VERSION
@@ -24,6 +26,13 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DWITH_CYCLES_EMBREE=OFF
 -DWITH_INSTALL_PORTABLE=OFF
 -DWITH_PYTHON_INSTALL=OFF
+"
+
+TERMUX_PKG_RM_AFTER_INSTALL="
+lib/python$TERMUX_PYTHON_VERSION/__pycache__/*
+lib/python$TERMUX_PYTHON_VERSION/site-packages/pip/_vendor/distro/__pycache__/*
+bin/c*
+bin/normalizer
 "
 
 termux_step_pre_configure(){
