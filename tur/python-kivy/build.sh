9480eeb3229d8a9f07b96034cd4d421278bf42e4
T-Dynamos <anshdadwal298@gmail.com>
2023-04-02 17:51:09 +0530

    bump(tur/python-kivy): 2.2.0.dev0

--- tur/python-kivy/build.sh
+++ tur/python-kivy/build.sh
@@ -2,38 +2,27 @@ TERMUX_PKG_HOMEPAGE=https://kivy.org/
 TERMUX_PKG_DESCRIPTION="Open source UI framework written in Python"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=2.1.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/kivy/kivy/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c4e611b823bd5440cfccbfdfe6501037bd82d21c45c26f7ec0f22155e0286bd4
-TERMUX_PKG_DEPENDS="mesa, mtdev, python, sdl2, sdl2-image, sdl2-mixer, sdl2-ttf"
+KIVY_VERSION_STATE=dev0
+TERMUX_PKG_VERSION=2.2.0.${KIVY_VERSION_STATE}
+TERMUX_PKG_SRCURL=git+https://github.com/kivy/kivy
+TERMUX_PKG_GIT_BRANCH="master"
+TERMUX_PKG_DEPENDS="mesa, mtdev, python, sdl2, sdl2-image, sdl2-mixer, sdl2-ttf, python-pillow"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
+TERMUX_PKG_PYTHON_COMMON_DEPS="Cython, wheel"
+TERMUX_PKG_PYTHON_BUILD_DEPS="${TERMUX_PKG_PYTHON_COMMON_DEPS}, packaging"
 _PKG_PYTHON_DEPENDS="'Kivy-Garden>=0.1.4' docutils pygments"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/
 "
 
 termux_step_pre_configure() {
-	_PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
-	termux_setup_python_crossenv
-	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
-	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
-	python${_PYTHON_VERSION} -m crossenv \
-		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
-		${_CROSSENV_PREFIX}
-	popd
-	. ${_CROSSENV_PREFIX}/bin/activate
-
-	LDFLAGS+=" -lpython${_PYTHON_VERSION}"
-	build-pip install cython wheel
-
+	LDFLAGS+=" -lpython${TERMUX_PYTHON_VERSION}"
 	export KIVY_SDL2_PATH=$TERMUX_PREFIX/include/SDL2
 }
 
 termux_step_make_install() {
-	export PYTHONPATH=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
+	export PYTHONPATH=$TERMUX_PREFIX/lib/python${TERMUX_PYTHON_VERSION}/site-packages
 	pip install --no-deps . --prefix $TERMUX_PREFIX
 }
 
