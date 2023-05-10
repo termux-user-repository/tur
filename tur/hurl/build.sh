d3b2adc130217c5c4d49b4a5530d8b76aff5bcd0
Sunlight <73415680+SunPodder@users.noreply.github.com>
2023-05-10 12:35:57 +0600

    new package: hurl (#400)

---
+++ tur/hurl/build.sh
@@ -0,0 +1,20 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/Orange-OpenSource/hurl
+TERMUX_PKG_DESCRIPTION="Hurl, run and test HTTP requests with plain text"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@SunPodder"
+TERMUX_PKG_VERSION="3.0.0"
+TERMUX_PKG_SRCURL="https://github.com/Orange-OpenSource/hurl/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=7ad9a1043129edb4850727c085a83010b916b3515c2af5afddd0809c1e2bd85c
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_BUILD_DEPENDS="openssl, libxml2"
+
+termux_step_make() {
+    termux_setup_rust
+    cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+}
+
+termux_step_make_install() {
+	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/hurl
+}
+
