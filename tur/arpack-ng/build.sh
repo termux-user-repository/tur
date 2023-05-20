385d2be73ac926241ca65144319d79623157b241
MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>
2023-05-21 01:52:17 +0900

    update arpack-ng to 3.9.0

--- tur/arpack-ng/build.sh
+++ tur/arpack-ng/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/opencollab/arpack-ng
 TERMUX_PKG_DESCRIPTION="Collection of Fortran77 subroutines designed to solve large scale eigenvalue problems."
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=3.8.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=3.9.0
 TERMUX_PKG_SRCURL=https://github.com/opencollab/arpack-ng/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ada5aeb3878874383307239c9235b716a8a170c6d096a6625bfd529844df003d
+TERMUX_PKG_SHA256=24f2a2b259992d3c797d80f626878aa8e2ed5009d549dad57854bbcfb95e1ed0
 TERMUX_PKG_DEPENDS="libopenblas"
 
 source $TERMUX_SCRIPTDIR/common-files/setup_toolchain_gcc.sh
