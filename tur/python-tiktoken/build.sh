45394d12fda6186472fb3336a115d553226c06c4
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-05-21 15:37:56 +0800

    new package: python-tiktoken

---
+++ tur/python-tiktoken/build.sh
@@ -0,0 +1,28 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/openai/tiktoken
+TERMUX_PKG_DESCRIPTION="A fast BPE tokeniser for use with OpenAI's models"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="0.4.0"
+TERMUX_PKG_SRCURL=https://github.com/openai/tiktoken/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=5a4c3ddc6c86a8ce26800262ad521053012e195e286ad21927eeeca6fa4a9c62
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, python"
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, setuptools-rust"
+TERMUX_PKG_PYTHON_TARGET_DEPS="'regex>=2022.1.18', 'requests>=2.26.0'"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	termux_setup_rust
+	export CARGO_BUILD_TARGET=${CARGO_TARGET_NAME}
+	export PYO3_CROSS_LIB_DIR=$TERMUX_PREFIX/lib
+	export PYTHONPATH=$TERMUX_PREFIX/lib/python${TERMUX_PYTHON_VERSION}/site-packages
+	LDFLAGS+=" -Wl,--no-as-needed -lpython${TERMUX_PYTHON_VERSION}"
+}
+
+termux_step_create_debscripts() {
+	cat <<- EOF > ./postinst
+	#!$TERMUX_PREFIX/bin/sh
+	echo "Installing dependencies through pip..."
+	pip3 install ${TERMUX_PKG_PYTHON_TARGET_DEPS//, / }
+	EOF
+}
