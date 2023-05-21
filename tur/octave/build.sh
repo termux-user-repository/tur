7d7a9908e9df7e4f5d512bcf8b8cb9e8769db723
MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>
2023-05-22 03:41:35 +0900

    update octave to 8.2.0

--- tur/octave/build.sh
+++ tur/octave/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://octave.org
 TERMUX_PKG_DESCRIPTION="GNU Octave is a high-level language, primarily intended for numerical computations. (only CLI)"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=7.3.0
+TERMUX_PKG_VERSION=8.2.0
 TERMUX_PKG_SRCURL=https://ftpmirror.gnu.org/octave/octave-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=a508ee6aebccfa68967c9e7e0a08793c4ca8e4ddace723aabdb8f71ad34d57f1
+TERMUX_PKG_SHA256=b7b9d6e5004ff039450cfedd2a59ddbe2a3c22296df927a8af994182eb2670de
 TERMUX_PKG_DEPENDS="zlib, bzip2, openssl, libiconv, libandroid-glob, pcre, readline, libcurl, libhdf5, qhull, rapidjson, fftw, glpk, libopenblas, arpack-ng, qrupdate-ng, suitesparse, sundials, fontconfig, freetype, graphicsmagick, libsndfile, portaudio"
 TERMUX_PKG_BUILD_DEPENDS="gnuplot, less"
 TERMUX_PKG_RECOMMENDS="gnuplot, less"
@@ -13,6 +13,7 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-x
 --enable-link-all-dependencies
+--disable-openmp
 --with-blas=openblas
 --with-openssl=yes
 --with-libiconv-prefix=$TERMUX_PREFIX
@@ -39,7 +40,7 @@ source $TERMUX_SCRIPTDIR/common-files/setup_toolchain_gcc.sh
 termux_step_pre_configure() {
 	_setup_toolchain_ndk_with_gfortran_11
 	autoreconf -fiv
-	LDFLAGS="${LDFLAGS/-static-openmp/} -Wl,-rpath,$TERMUX_PREFIX/lib/octave/$TERMUX_PKG_VERSION"
+	LDFLAGS+="-Wl,-rpath,$TERMUX_PREFIX/lib/octave/$TERMUX_PKG_VERSION"
 	NDK_ARCH=$TERMUX_ARCH
         test $NDK_ARCH == 'i686' && NDK_ARCH='i386'
 	if [ $NDK_ARCH == 'arm' ]; then
@@ -54,7 +55,7 @@ termux_step_pre_configure() {
 	fi
 	
         # clang 13+ requires libunwind on Android.
-        cp "$TERMUX_STANDALONE_TOOLCHAIN/lib64/clang/14.0.6/lib/linux/$NDK_ARCH/libunwind.a" \
+        cp "$TERMUX_STANDALONE_TOOLCHAIN/lib64/clang/"14.*"/lib/linux/$NDK_ARCH/libunwind.a" \
 	   "$TERMUX_PKG_BUILDDIR" || exit 1
 	cp "$GCC_STANDALONE_TOOLCHAIN/lib/gcc/$GCC_TRIPLET/"11.*/libgcc.a \
 	   "$TERMUX_PKG_BUILDDIR" || exit 1
