db791a7989548f432fefa6b3d952cbe1b262e465
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-05-25 02:23:22 +0800

    lib32-libgmp: remove info and man dir

--- tur-multilib/lib32-libgmp/build.sh
+++ tur-multilib/lib32-libgmp/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Library for arbitrary precision arithmetic"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=6.2.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/gmp/gmp-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=fd4829912cddd12f84181c3451cc752be224643e87fac497b69edddadc49b4f2
 TERMUX_PKG_DEPENDS="lib32-libc++"
@@ -15,6 +16,11 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --exec-prefix=$TERMUX_PREFIX/$TUR_MULTILIB_ARCH_TRIPLE
 --includedir=$TERMUX_PREFIX/$TUR_MULTILIB_ARCH_TRIPLE/include
 --libdir=$TERMUX_PREFIX/$TUR_MULTILIB_ARCH_TRIPLE/lib
+"
+
+TERMUX_PKG_RM_AFTER_INSTALL="
+share/info
+share/man
 "
 
 termux_step_pre_configure() {
