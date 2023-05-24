8c828a6fc38a845c30bf4f7111f52ef3f448f2eb
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-05-24 23:19:06 +0800

    new package: lib-tls-client

---
+++ tur/lib-tls-client/build.sh
@@ -0,0 +1,28 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/bogdanfinn/tls-client/
+TERMUX_PKG_DESCRIPTION="Shared library for catopen/catgets/catclose of Bionic Libc"
+# LICENSE: BSD-4-Clause
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="LICENSE"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION=1.3.12
+TERMUX_PKG_SRCURL="https://github.com/bogdanfinn/tls-client/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=a560c0be61e7b5ca517157fcbbcb828d08b4480fe5725a260a1c8688990d6876
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy
+}
+
+termux_step_make() {
+    cd cffi_dist
+	go build -buildmode=c-shared -o ./dist/lib-tls-client.so
+}
+
+termux_step_make_install() {
+	install -Dm600 -t "${TERMUX_PREFIX}"/lib "$TERMUX_PKG_SRCDIR"/cffi_dist/dist/lib-tls-client.so
+	install -Dm600 -t "${TERMUX_PREFIX}"/include "$TERMUX_PKG_SRCDIR"/cffi_dist/dist/lib-tls-client.h
+}
