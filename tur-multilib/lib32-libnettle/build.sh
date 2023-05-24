9174f139d06925e7999d651c7af23cbdfcccc9b5
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-05-25 00:44:55 +0800

    lib32-libnettle: bump to 3.9

--- tur-multilib/lib32-libnettle/build.sh
+++ tur-multilib/lib32-libnettle/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.lysator.liu.se/~nisse/nettle/
 TERMUX_PKG_DESCRIPTION="Cryptographic library that is designed to fit easily in more or less any context"
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=3.8.1
+TERMUX_PKG_VERSION=3.9
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/nettle/nettle-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=364f3e2b77cd7dcde83fd7c45219c834e54b0c75e428b6f894a23d12dd41cbfe
+TERMUX_PKG_SHA256=0ee7adf5a7201610bb7fe0acbb7c9b3be83be44904dd35ebbcd965cd896bfeaa
 TERMUX_PKG_DEPENDS="lib32-libgmp"
 
 source $TERMUX_SCRIPTDIR/common-files/setup_multilib_toolchain.sh
@@ -18,7 +18,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 TERMUX_PKG_RM_AFTER_INSTALL="
 share/info
-share/man/man7
+share/man
 "
 
 termux_step_pre_configure() {
