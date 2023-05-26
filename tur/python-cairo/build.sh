287337db4470eb3dcd86915cebe442bff45a8127
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-05-26 14:05:23 +0800

    python-cairo: fix build
    Closes #429
    [skip ci]

--- tur/python-cairo/build.sh
+++ tur/python-cairo/build.sh
@@ -6,25 +6,11 @@ TERMUX_PKG_VERSION="1.23.0"
 TERMUX_PKG_SRCURL=https://github.com/pygobject/pycairo/releases/download/v${TERMUX_PKG_VERSION}/pycairo-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9b61ac818723adc04367301317eb2e814a83522f07bbd1f409af0dada463c44c
 TERMUX_PKG_DEPENDS="python, libcairo"
+TERMUX_PKG_PYTHON_BUILD_DEPS="wheel"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_configure() {
-	_PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
-	termux_setup_python_crossenv
-	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
-	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
-	python${_PYTHON_VERSION} -m crossenv \
-		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
-		${_CROSSENV_PREFIX}
-	popd
-	. ${_CROSSENV_PREFIX}/bin/activate
-
-	LDFLAGS+=" -lpython${_PYTHON_VERSION}"
-	build-pip install wheel
-}
-
-termux_step_make_install() {
-	export PYTHONPATH=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
-	pip install --no-deps . --prefix $TERMUX_PREFIX
+termux_step_pre_configure() {
+	LDFLAGS+=" -lpython${TERMUX_PYTHON_VERSION}"
+	rm -f meson.build
 }
