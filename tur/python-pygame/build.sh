e30255cf46c909f624fba7154b6bba589d787bc6
Uchiha Kakashi <45286352+licy183@users.noreply.github.com>
2023-05-26 23:18:28 +0800

    python-pygame: bump to 2.4.0 (#431)

--- tur/python-pygame/build.sh
+++ tur/python-pygame/build.sh
@@ -2,11 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://pygame.org/
 TERMUX_PKG_DESCRIPTION="pygame is a python game library"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=2.1.3.dev8
+TERMUX_PKG_VERSION=2.4.0
 TERMUX_PKG_SRCURL=https://github.com/pygame/pygame/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1d8a55b5a769eb9521b9d346688d4a060dca3e4497300efc78fb4021bc370199
+TERMUX_PKG_SHA256=7d7efd65812fa0d8a83a92afd22116dee7997a38bd7126097230ae1dd6dec4b8
 TERMUX_PKG_DEPENDS="x11-repo, freetype, portmidi, python, sdl2, sdl2-image, sdl2-mixer, sdl2-ttf"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
+TERMUX_PKG_PYTHON_BUILD_DEPS="wheel"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_PKG_RM_AFTER_INSTALL="
@@ -14,23 +16,11 @@ bin/
 "
 
 termux_step_pre_configure() {
-	_PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
-	termux_setup_python_crossenv
-	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
-	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
-	python${_PYTHON_VERSION} -m crossenv \
-		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
-		${_CROSSENV_PREFIX}
-	popd
-	. ${_CROSSENV_PREFIX}/bin/activate
+	export CFLAGS+=" -Wno-error=incompatible-function-pointer-types"
+	export CPPFLAGS+=" -Wno-error=incompatible-function-pointer-types"
+	export CXXFLAGS+=" -Wno-error=incompatible-function-pointer-types"
 
-	LDFLAGS+=" -lpython${_PYTHON_VERSION}"
-	build-pip install wheel
-}
-
-termux_step_make_install() {
-	export PYTHONPATH=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
 	export PATH="$PATH:$TERMUX_PREFIX/bin"
 
-	pip install --no-deps . --prefix $TERMUX_PREFIX
+	LDFLAGS+=" -lpython${TERMUX_PYTHON_VERSION}"
 }
