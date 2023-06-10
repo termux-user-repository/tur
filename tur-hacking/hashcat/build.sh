711fbafd61a06b9a9564eca160fb6c1903987107
Kevin Williams <admin@utermux.dev>
2023-06-10 11:01:36 +0800

    add(tur-hacking): new package hashcat
    Signed-off-by: Kevin Williams <admin@utermux.dev>

---
+++ tur-hacking/hashcat/build.sh
@@ -0,0 +1,40 @@
+TERMUX_PKG_HOMEPAGE=https://hashcat.net/hashcat/
+TERMUX_PKG_DESCRIPTION="World's fastest and most advanced password recovery utility"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE_FILE="docs/license.txt"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="6.2.6"
+TERMUX_PKG_SRCURL=https://github.com/hashcat/hashcat/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=b25e1077bcf34908cc8f18c1a69a2ec98b047b2cbcf0f51144dcf3ba1e0b7b2a
+TERMUX_PKG_DEPENDS="opencl-vendor-driver, libiconv, tsu"
+# fatal error: error in backend: Cannot select: intrinsic %llvm.arm.crc32b
+TERMUX_PKG_BLACKLISTED_ARCHES="arm"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_make() {
+	rm -rf $TERMUX_PREFIX/opt/hashcat/
+	mkdir -p $TERMUX_PREFIX/opt/hashcat/
+
+	if [ $TERMUX_ARCH = "aarch64" ]; then
+	CFLAGS+=" -march=armv8-a+crc+crypto"
+	fi
+
+	make -j $TERMUX_MAKE_PROCESSES
+}
+
+termux_step_make_install() {
+	make install
+
+        cat <<- EOF > "$TERMUX_PREFIX"/bin/hashcat
+	#!$TERMUX_PREFIX/bin/sh
+	LD_LIBRARY_PATH="$TERMUX_PREFIX/opt/vendor/lib:$TERMUX_PREFIX/lib" $TERMUX_PREFIX/opt/hashcat/bin/hashcat "\$@"
+	EOF
+	chmod 700 "$TERMUX_PREFIX"/bin/hashcat
+}
+
+termux_step_create_debscripts() {
+	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
+	echo 'mkdir -p $HOME/.local/share/hashcat/' >> postinst
+	
+}
