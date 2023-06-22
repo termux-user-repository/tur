30a661190bd019d41e0f31d204d26fa3cae3f9d1
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-06-22 18:17:52 +0000

    trigger-rally: revise dependencies
    Closes #434
    
    [skip ci]

--- tur/trigger-rally/build.sh
+++ tur/trigger-rally/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION=0.6.6.1
 TERMUX_PKG_REVISION=10
 TERMUX_PKG_SRCURL=https://netcologne.dl.sourceforge.net/project/trigger-rally/trigger-${TERMUX_PKG_VERSION}/trigger-rally-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=7f086e13d142b8bb07e808ab9111e5553309c1413532f56c754ce3cfa060cb04
-TERMUX_PKG_DEPENDS="make, libphysfs, glew, libtinyxml2, openal-soft, openalut, sdl2, ndk-multilib, sdl2-image, pulseaudio, libxi"
+TERMUX_PKG_DEPENDS="glew, libphysfs, libtinyxml2, libxi, libxinerama, libxxf86vm, make, ndk-multilib, openal-soft, openalut, pulseaudio, sdl2, sdl2-image"
 
 termux_step_pre_configure(){
 	export OPTIMS=""
