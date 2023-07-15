28e7c98f9b8a6b31662815cc2c0dd7c541a0bc1a
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-07-15 22:41:14 +0800

    new package: libftgl2

---
+++ tur/libftgl2/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/frankheckenbach/ftgl
+TERMUX_PKG_DESCRIPTION="library to render text in OpenGL using FreeType"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@fervi"
+TERMUX_PKG_VERSION="2.4.0"
+TERMUX_PKG_SRCURL="https://github.com/frankheckenbach/ftgl/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=aa97da1c3442a8fd3941037655df18016d70b5266381c81d81e8b5335f196ea8
+TERMUX_PKG_DEPENDS="freetype, glu, opengl"
