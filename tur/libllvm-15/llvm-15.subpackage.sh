5cde15b78cd0371bd0eb5fe11d68349948aa3861
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-07-16 07:52:36 +0800

    new package: libllvm-15

---
+++ tur/libllvm-15/llvm-15.subpackage.sh
@@ -0,0 +1,15 @@
+TERMUX_SUBPKG_INCLUDE="
+opt/libllvm-15/bin/bugpoint
+opt/libllvm-15/bin/dsymutil
+opt/libllvm-15/bin/llc
+opt/libllvm-15/bin/lli
+opt/libllvm-15/bin/llvm*
+opt/libllvm-15/bin/obj2yaml
+opt/libllvm-15/bin/opt
+opt/libllvm-15/bin/sancov
+opt/libllvm-15/bin/sanstats
+opt/libllvm-15/bin/verify-uselistorder
+opt/libllvm-15/bin/yaml2obj
+opt/libllvm-15/share/opt-viewer
+"
+TERMUX_SUBPKG_DESCRIPTION="LLVM modular compiler and toolchain executables"
