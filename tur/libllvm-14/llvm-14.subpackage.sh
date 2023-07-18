87c4b7f62602aa14f4bfa8b12004c1a135bd87ec
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-07-18 08:26:52 +0800

    new package: libllvm-14

---
+++ tur/libllvm-14/llvm-14.subpackage.sh
@@ -0,0 +1,15 @@
+TERMUX_SUBPKG_INCLUDE="
+opt/libllvm-14/bin/bugpoint
+opt/libllvm-14/bin/dsymutil
+opt/libllvm-14/bin/llc
+opt/libllvm-14/bin/lli
+opt/libllvm-14/bin/llvm*
+opt/libllvm-14/bin/obj2yaml
+opt/libllvm-14/bin/opt
+opt/libllvm-14/bin/sancov
+opt/libllvm-14/bin/sanstats
+opt/libllvm-14/bin/verify-uselistorder
+opt/libllvm-14/bin/yaml2obj
+opt/libllvm-14/share/opt-viewer
+"
+TERMUX_SUBPKG_DESCRIPTION="LLVM modular compiler and toolchain executables"
