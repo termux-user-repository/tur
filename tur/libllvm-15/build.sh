d9175264e35356a24323aa59277d4039ea194971
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-07-21 18:44:43 +0800

    libllvm-15: enable clang

--- tur/libllvm-15/build.sh
+++ tur/libllvm-15/build.sh
@@ -4,22 +4,34 @@ TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_LICENSE_FILE="llvm/LICENSE.TXT"
 TERMUX_PKG_MAINTAINER="@buttaface, @termux-user-repository"
 TERMUX_PKG_VERSION=15.0.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=8b5fcb24b4128cf04df1b0b9410ce8b1a729cb3c544e6da885d234280dedeac6
 TERMUX_PKG_SRCURL=https://github.com/llvm/llvm-project/releases/download/llvmorg-$TERMUX_PKG_VERSION/llvm-project-$TERMUX_PKG_VERSION.src.tar.xz
 TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_DEPENDS="binutils, libc++, ncurses, ndk-sysroot, libffi, zlib"
 
+_INSTALL_PREFIX_R="opt/libllvm-15"
+_INSTALL_PREFIX="$TERMUX_PREFIX/$_INSTALL_PREFIX_R"
 # See http://llvm.org/docs/CMake.html:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DANDROID_PLATFORM_LEVEL=$TERMUX_PKG_API_LEVEL
 -DPYTHON_EXECUTABLE=$(command -v python3)
 -DLLVM_ENABLE_PIC=ON
+-DLLVM_ENABLE_PROJECTS=clang;compiler-rt;lld;openmp
 -DLLVM_ENABLE_LIBEDIT=OFF
 -DLLVM_INCLUDE_TESTS=OFF
+-DCLANG_DEFAULT_RTLIB=compiler-rt
+-DCLANG_DEFAULT_CXX_STDLIB=libc++
+-DCLANG_DEFAULT_LINKER=lld-15
+-DCLANG_INCLUDE_TESTS=OFF
+-DCLANG_TOOL_C_INDEX_TEST_BUILD=OFF
+-DCOMPILER_RT_USE_BUILTINS_LIBRARY=ON
 -DDEFAULT_SYSROOT=$(dirname $TERMUX_PREFIX)
 -DLLVM_LINK_LLVM_DYLIB=ON
+-DCLANG_TABLEGEN=$TERMUX_PKG_HOSTBUILD_DIR/bin/clang-tblgen
 -DLLVM_TABLEGEN=$TERMUX_PKG_HOSTBUILD_DIR/bin/llvm-tblgen
 -DLIBOMP_ENABLE_SHARED=FALSE
+-DOPENMP_ENABLE_LIBOMPTARGET=OFF
 -DLLVM_ENABLE_SPHINX=ON
 -DSPHINX_OUTPUT_MAN=ON
 -DSPHINX_WARNINGS_AS_ERRORS=OFF
@@ -27,7 +39,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DPERL_EXECUTABLE=$(command -v perl)
 -DLLVM_ENABLE_FFI=ON
 -DLLVM_INSTALL_UTILS=ON
--DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX/opt/libllvm-15
+-DCMAKE_INSTALL_PREFIX=$_INSTALL_PREFIX
 "
 
 if [ $TERMUX_ARCH_BITS = 32 ]; then
@@ -42,8 +54,10 @@ termux_step_host_build() {
 	termux_setup_cmake
 	termux_setup_ninja
 
-	cmake -G Ninja -DCMAKE_BUILD_TYPE=Release $TERMUX_PKG_SRCDIR/llvm
-	ninja -j $TERMUX_MAKE_PROCESSES llvm-tblgen
+	cmake -G Ninja "-DCMAKE_BUILD_TYPE=Release" \
+					"-DLLVM_ENABLE_PROJECTS=clang" \
+					$TERMUX_PKG_SRCDIR/llvm
+	ninja -j $TERMUX_MAKE_PROCESSES llvm-tblgen clang-tblgen
 }
 
 termux_step_pre_configure() {
@@ -53,6 +67,8 @@ termux_step_pre_configure() {
 	export LLVM_TARGET_ARCH
 	if [ $TERMUX_ARCH = "arm" ]; then
 		LLVM_TARGET_ARCH=ARM
+		CFLAGS=${CFLAGS//-mthumb /}
+		CXXFLAGS=${CXXFLAGS//-mthumb /}
 	elif [ $TERMUX_ARCH = "aarch64" ]; then
 		LLVM_TARGET_ARCH=AArch64
 	elif [ $TERMUX_ARCH = "i686" ] || [ $TERMUX_ARCH = "x86_64" ]; then
@@ -63,6 +79,10 @@ termux_step_pre_configure() {
 	# see CMakeLists.txt and tools/clang/CMakeLists.txt
 	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DLLVM_TARGET_ARCH=$LLVM_TARGET_ARCH"
 	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DLLVM_HOST_TRIPLE=$LLVM_DEFAULT_TARGET_TRIPLE"
+	_RPATH_FLAG="-Wl,-rpath=$TERMUX_PREFIX/lib"
+	_RPATH_FLAG_ADD="-Wl,-rpath='\$ORIGIN/../lib' -Wl,-rpath=$TERMUX_PREFIX/lib"
+	LDFLAGS="${LDFLAGS/$_RPATH_FLAG/$_RPATH_FLAG_ADD $_RPATH_FLAG}"
+	echo $LDFLAGS
 	export TERMUX_SRCDIR_SAVE=$TERMUX_PKG_SRCDIR
 	TERMUX_PKG_SRCDIR=$TERMUX_PKG_SRCDIR/llvm
 }
@@ -72,5 +92,9 @@ termux_step_post_configure() {
 }
 
 termux_step_post_make_install() {
-	ln -sfr $TERMUX_PREFIX/opt/libllvm-15/lib/libLLVM-15.so $TERMUX_PREFIX/lib/
+	ln -sfr $_INSTALL_PREFIX/bin/clang-15 $TERMUX_PREFIX/bin/
+	ln -sfr $_INSTALL_PREFIX/bin/lld $_INSTALL_PREFIX/bin/lld-15
+	ln -sfr $_INSTALL_PREFIX/bin/lld $TERMUX_PREFIX/bin/lld-15
+	ln -sfr $_INSTALL_PREFIX/bin/ld.lld $TERMUX_PREFIX/bin/ld.lld-15
+	ln -sfr $_INSTALL_PREFIX/lib/libLLVM-15.so $TERMUX_PREFIX/lib/
 }
