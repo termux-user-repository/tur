7513732c33fee9acba85b0c14a8a7155747c9da2
Krishna Kanhaiya <kcubeterm@gmail.com>
2023-08-24 16:08:51 +0530

    bump revision (#544)

--- tur-hacking/mdk4/build.sh
+++ tur-hacking/mdk4/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="MDK is a proof-of-concept tool to exploit common IEEE 80
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=4.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/aircrack-ng/mdk4/archive/c6a35994b2f0d87d79ce9a072b84a486775d8ee4.zip
 TERMUX_PKG_SHA256=9eb57c1b70d0d4da6baccea90c437fb095e401f33256b52336b4b420cf32dc9d
 TERMUX_PKG_DEPENDS="aircrack-ng, libnl, libpcap"
