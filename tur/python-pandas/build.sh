b64437a778af037bc863af91bf199de69fa96215
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-08-31 10:50:09 +0000

    python-pandas: bump to 2.1.0

--- tur/python-pandas/build.sh
+++ tur/python-pandas/build.sh
@@ -3,12 +3,12 @@ TERMUX_PKG_HOMEPAGE=https://pandas.pydata.org/
 TERMUX_PKG_DESCRIPTION="Powerful Python data analysis toolkit"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="2.0.3"
+TERMUX_PKG_VERSION="2.1.0"
 TERMUX_PKG_SRCURL=git+https://github.com/pandas-dev/pandas
 TERMUX_PKG_SHA256=d8abf9c2bf33cac75b28f32c174c29778414eb249e5e2ccb69b1079b97a8fc66
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, python, python-numpy, python-pip"
-TERMUX_PKG_PYTHON_COMMON_DEPS="Cython, numpy, wheel, 'setuptools==63.2.0', versioneer"
+TERMUX_PKG_PYTHON_COMMON_DEPS="'Cython==0.29.33', numpy, wheel, 'setuptools==63.2.0', versioneer"
 TERMUX_PKG_PYTHON_TARGET_DEPS="'python-dateutil>=2.8.2', 'pytz>=2020.1', 'tzdata>=2022.1'"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
@@ -20,8 +20,16 @@ termux_step_pre_configure() {
 	LDFLAGS+=" -lm"
 }
 
+termux_step_configure() {
+	:
+}
+
+termux_step_make() {
+	python setup.py bdist_wheel -vvv
+}
+
 termux_step_make_install() {
-	pip install --no-build-isolation --no-deps . --prefix $TERMUX_PREFIX -vvv
+	pip install ./dist/*.whl --no-deps --prefix=$PREFIX -vvv
 }
 
 termux_step_create_debscripts() {
