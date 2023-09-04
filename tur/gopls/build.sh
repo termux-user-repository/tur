bab06d583c4cca91385b5e1d637c09678fd1cc0d
Sunlight <73415680+SunPodder@users.noreply.github.com>
2023-09-04 11:34:34 +0600

    new package: gopls (#561)

---
+++ tur/gopls/build.sh
@@ -0,0 +1,20 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/golang/tools
+TERMUX_PKG_DESCRIPTION="The official Go language server"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_MAINTAINER="@SunPodder"
+TERMUX_PKG_VERSION=0.13.2
+TERMUX_PKG_SRCURL=https://github.com/golang/tools/archive/refs/tags/gopls/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=fa8a5d38c1e040686fda6018c3456801229d6ed992b6eecfbaba146e1fc772b2
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_make() {
+	termux_setup_golang
+
+	cd $TERMUX_PKG_SRCDIR/gopls
+	go build -o gopls
+}
+
+termux_step_make_install() {
+	install -Dm755 -t $TERMUX_PREFIX/bin $TERMUX_PKG_SRCDIR/gopls/gopls
+}
+
