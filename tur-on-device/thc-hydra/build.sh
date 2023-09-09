6489117435658d9b400b7d47f48d4bae1e3f1951
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-09-09 15:00:15 +0800

    thc-hydra: enable auto-updating
    [skip ci]

--- tur-on-device/thc-hydra/build.sh
+++ tur-on-device/thc-hydra/build.sh
@@ -8,6 +8,7 @@ TERMUX_PKG_SRCURL=git+https://github.com/vanhauser-thc/thc-hydra
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="libidn, libgcrypt, libssh, libssh2, openssl, openssl-1.1, pcre, pcre2, zlib"
 TERMUX_PKG_BUILD_DEPENDS="desktop-file-utils"
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
 	if [ "${TERMUX_ON_DEVICE_BUILD}" = false ]; then
