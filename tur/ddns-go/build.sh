57dc5eb162e3b6ebdb43c30f5941460f5f08f8b5
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-09-12 18:02:05 +0000

    new package: ddns-go

---
+++ tur/ddns-go/build.sh
@@ -0,0 +1,25 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/jeessy2/ddns-go
+TERMUX_PKG_DESCRIPTION="A simple, easy-to-use ddns service"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="5.6.2"
+TERMUX_PKG_SRCURL=https://github.com/jeessy2/ddns-go/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=ac74c1ef96688838ffeab18dd011616b49b156c533cecf859eeee01e5c1a6e40
+TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	termux_setup_golang
+}
+
+termux_step_make() {
+	local _buildtime=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+	go build -ldflags="-X main.version=$TERMUX_PKG_VERSION -X main.buildTime=$_buildtime -extldflags -s -w" \
+		-trimpath -buildmode=pie -o ddns-go .
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin "$TERMUX_PKG_SRCDIR"/ddns-go
+}
