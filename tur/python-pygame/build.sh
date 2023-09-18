742c9a3db9c393e2217288c794ad975c51f25184
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-09-18 11:06:54 +0800

    python-pygame: bump to 2.5.2

--- tur/python-pygame/build.sh
+++ tur/python-pygame/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://pygame.org/
 TERMUX_PKG_DESCRIPTION="pygame is a python game library"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="2.5.1"
+TERMUX_PKG_VERSION="2.5.2"
 TERMUX_PKG_SRCURL=https://github.com/pygame/pygame/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f29d5ca72ec487df8f5f19169d66e6e5cecfcdb841c28b3c711cfd4f263a672a
+TERMUX_PKG_SHA256=46427b2b35060303b06a06a56c07a9e8e55b26cd4c434b792ed78244d2023e05
 TERMUX_PKG_DEPENDS="x11-repo, freetype, portmidi, python, sdl2, sdl2-image, sdl2-mixer, sdl2-ttf"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
-TERMUX_PKG_PYTHON_BUILD_DEPS="wheel"
+TERMUX_PKG_PYTHON_BUILD_DEPS="'Cython<3', wheel"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
