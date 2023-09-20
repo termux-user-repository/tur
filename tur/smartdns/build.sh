2a2865a99be663a4589eb0e24f77eac8a2cf8a40
Chongyun Lee <uchkks@protonmail.com>
2023-09-20 22:02:17 +0800

    smartdns: bump to 43

--- tur/smartdns/build.sh
+++ tur/smartdns/build.sh
@@ -2,11 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://github.com/pymumu/smartdns
 TERMUX_PKG_DESCRIPTION="A local DNS server to obtain the fastest website IP for the best Internet experience, support DoT, DoH"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=42
+TERMUX_PKG_VERSION=43
 TERMUX_PKG_SRCURL=https://github.com/pymumu/smartdns/archive/refs/tags/Release$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=83bb3c588672dff7fe702223538d6e61a4d475e592643a57d1416aade0b363d0
+TERMUX_PKG_SHA256=2a5e7869603ecb7b0d94e153d938e798f0b8f260cc6062cc095a39116386d8b3
 TERMUX_PKG_DEPENDS="libc++, openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_configure() {
 	:
