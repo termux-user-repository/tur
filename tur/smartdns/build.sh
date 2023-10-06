c30c5f8cbfb51d1aab8f7947952948bd83b90254
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-10-06 23:33:59 +0800

    smartdns: fix auto-update
    [skip ci]

--- tur/smartdns/build.sh
+++ tur/smartdns/build.sh
@@ -8,6 +8,19 @@ TERMUX_PKG_SHA256=2a5e7869603ecb7b0d94e153d938e798f0b8f260cc6062cc095a39116386d8
 TERMUX_PKG_DEPENDS="libc++, openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+"
+
+termux_pkg_auto_update() {
+	# Get the newest tag:
+	local tag
+	tag="$(termux_github_api_get_tag "${TERMUX_PKG_SRCURL}" "${TERMUX_PKG_UPDATE_TAG_TYPE}")"
+	# check if this is not a release:
+	if grep -qP "^Release${TERMUX_PKG_UPDATE_VERSION_REGEXP}\$" <<<"$tag"; then
+		termux_pkg_upgrade_version "$tag"
+	else
+		echo "WARNING: Skipping auto-update: Not a release($tag)"
+	fi
+}
 
 termux_step_configure() {
 	:
