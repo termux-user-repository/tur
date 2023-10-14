a560d59c0ee4063d3d2d1a6ef16598bf6993c197
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-10-15 06:56:27 +0800

    code-oss: bump to 1.82.0

--- tur/code-oss/build.sh
+++ tur/code-oss/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/microsoft/vscode
 TERMUX_PKG_DESCRIPTION="Visual Studio Code"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="1.81.1"
+TERMUX_PKG_VERSION="1.82.0"
 TERMUX_PKG_SRCURL=git+https://github.com/microsoft/vscode
 TERMUX_PKG_GIT_BRANCH="$TERMUX_PKG_VERSION"
 TERMUX_PKG_DEPENDS="electron-deps, libx11, libxkbfile, libsecret, ripgrep"
+TERMUX_PKG_BUILD_DEPENDS="electron-headers-for-code-oss"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STATICSPLIT=true
 TERMUX_PKG_HOSTBUILD=true
@@ -14,8 +15,8 @@ TERMUX_PKG_BLACKLISTED_ARCHES="i686"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 
-_setup_nodejs_16() {
-	local NODEJS_VERSION=16.17.1
+_setup_nodejs_18() {
+	local NODEJS_VERSION=18.18.0
 	local NODEJS_FOLDER=${TERMUX_PKG_CACHEDIR}/build-tools/nodejs-${NODEJS_VERSION}
 
 	if [ ! -x "$NODEJS_FOLDER/bin/node" ]; then
@@ -23,13 +24,20 @@ _setup_nodejs_16() {
 		local NODEJS_TAR_FILE=$TERMUX_PKG_TMPDIR/nodejs-$NODEJS_VERSION.tar.xz
 		termux_download https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-x64.tar.xz \
 			"$NODEJS_TAR_FILE" \
-			06ba2eb34aa385967f5f58c87a44753f83212f6cccea892b33f80a2e7fda8384
+			3008408e9098f2462f7b1a0f6a48b8a46079beb1c92b6ec43b04713265c96978
 		tar -xf "$NODEJS_TAR_FILE" -C "$NODEJS_FOLDER" --strip-components=1
 	fi
 	export PATH=$NODEJS_FOLDER/bin:$PATH
 }
 
 termux_step_post_get_source() {
+	# Check whether the electron version matches the node headers version
+	local _electron_verion="$(jq -r '.devDependencies.electron' $TERMUX_PKG_SRCDIR/package.json)"
+	local _header_version="$(. $TERMUX_SCRIPTDIR/tur/electron-headers-for-code-oss/build.sh; echo $TERMUX_PKG_VERSION)"
+	if [ "$_electron_verion" != "$_header_version" ]; then
+		termux_error_exit "Version mismatch: electron version $_electron_verion, header version $_header_version."
+	fi
+
 	# Parse yarn.lock and get native-keymap verion
 	python3 -m venv $TERMUX_PKG_CACHEDIR/venv-dir
 	(. $TERMUX_PKG_CACHEDIR/venv-dir/bin/activate
@@ -51,9 +59,9 @@ termux_step_host_build() {
 		rm -rf $TERMUX_PREFIX/bin.bp
 		mv -f $TERMUX_PREFIX/bin $TERMUX_PREFIX/bin.bp
 	fi
-	_setup_nodejs_16
-	npm install yarn
-	export PATH="$(npm bin):$PATH"
+	_setup_nodejs_18
+	npm install yarn node-gyp
+	export PATH="$TERMUX_PKG_HOSTBUILD_DIR/node_modules/.bin:$PATH"
 	if [ -e "$TERMUX_PREFIX/bin.bp" ]; then
 		rm -rf $TERMUX_PREFIX/bin
 		mv -f $TERMUX_PREFIX/bin.bp $TERMUX_PREFIX/bin
@@ -61,7 +69,7 @@ termux_step_host_build() {
 }
 
 termux_step_configure() {
-	_setup_nodejs_16
+	_setup_nodejs_18
 	export PATH="$TERMUX_PKG_HOSTBUILD_DIR/node_modules/.bin:$PATH"
 }
 
@@ -87,6 +95,7 @@ termux_step_make() {
 		termux_error_exit "Unsupported arch: $TERMUX_ARCH"
 	fi
 	export npm_config_arch=$NPM_CONFIG_ARCH
+	export npm_config_nodedir=$TERMUX_PREFIX/opt/electron-headers-for-code-oss/node_headers
 
 	export CXX="$CXX -v -L$TERMUX_PREFIX/lib"
 
