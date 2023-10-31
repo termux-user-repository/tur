f4ef26acc0faf26aa43212f06bc1753193dbd393
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-10-31 19:27:19 +0800

    libllvm-16: bump to 16.0.6

--- tur/libllvm-16/libcompiler-rt-16.subpackage.sh
+++ tur/libllvm-16/libcompiler-rt-16.subpackage.sh
@@ -1,5 +1,13 @@
 TERMUX_SUBPKG_DESCRIPTION="Compiler runtime libraries for clang"
 TERMUX_SUBPKG_INCLUDE="
-$_INSTALL_PREFIX_R/.placeholder-libcompiler-rt-16
+$_INSTALL_PREFIX_R/lib/clang/*/bin/asan_device_setup
+$_INSTALL_PREFIX_R/lib/clang/*/bin/hwasan_symbolize
+$_INSTALL_PREFIX_R/lib/clang/*/include/fuzzer/FuzzedDataProvider.h
+$_INSTALL_PREFIX_R/lib/clang/*/include/profile/InstrProfData.inc
+$_INSTALL_PREFIX_R/lib/clang/*/include/sanitizer/
+$_INSTALL_PREFIX_R/lib/clang/*/include/xray/
+$_INSTALL_PREFIX_R/lib/clang/*/lib/linux/
+$_INSTALL_PREFIX_R/lib/clang/*/share/asan_ignorelist.txt
+$_INSTALL_PREFIX_R/lib/clang/*/share/cfi_ignorelist.txt
+$_INSTALL_PREFIX_R/lib/clang/*/share/hwasan_ignorelist.txt
 "
-TERMUX_SUBPKG_DEPENDS="libcompiler-rt"
--- tur/libllvm-16/llvm-16.subpackage.sh
+++ tur/libllvm-16/llvm-16.subpackage.sh
@@ -1,5 +1,21 @@
 TERMUX_SUBPKG_INCLUDE="
-$_INSTALL_PREFIX_R/.placeholder-llvm-16
+$_INSTALL_PREFIX_R/bin/bugpoint
+$_INSTALL_PREFIX_R/bin/dsymutil
+$_INSTALL_PREFIX_R/bin/llc
+$_INSTALL_PREFIX_R/bin/lli
+$_INSTALL_PREFIX_R/bin/llvm*
+$_INSTALL_PREFIX_R/bin/opt
+$_INSTALL_PREFIX_R/bin/sancov
+$_INSTALL_PREFIX_R/bin/sanstats
+$_INSTALL_PREFIX_R/bin/split-file
+$_INSTALL_PREFIX_R/bin/verify-uselistorder
+$_INSTALL_PREFIX_R/share/man/man1/bugpoint.1.gz
+$_INSTALL_PREFIX_R/share/man/man1/dsymutil.1.gz
+$_INSTALL_PREFIX_R/share/man/man1/llc.1.gz
+$_INSTALL_PREFIX_R/share/man/man1/lli.1.gz
+$_INSTALL_PREFIX_R/share/man/man1/llvm*
+$_INSTALL_PREFIX_R/share/man/man1/opt.1.gz
+$_INSTALL_PREFIX_R/share/man/man1/*tblgen.1.gz
+$_INSTALL_PREFIX_R/share/opt-viewer
 "
 TERMUX_SUBPKG_DESCRIPTION="LLVM modular compiler and toolchain executables"
-TERMUX_SUBPKG_DEPENDS="llvm"
