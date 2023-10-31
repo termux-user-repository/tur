8240b21761b6c836c4220b5ee45b8d74e84e0f69
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-10-31 19:27:19 +0800

    libllvm-16: bump to 16.0.6

--- tur/libllvm-16/mlir-16.subpackage.sh
+++ tur/libllvm-16/mlir-16.subpackage.sh
@@ -1,5 +1,9 @@
 TERMUX_SUBPKG_INCLUDE="
-$_INSTALL_PREFIX_R/.placeholder-mlir-16
+$_INSTALL_PREFIX_R/bin/mlir-*
+$_INSTALL_PREFIX_R/include/mlir*
+$_INSTALL_PREFIX_R/lib/cmake/mlir/
+$_INSTALL_PREFIX_R/lib/libMLIR.so
+$_INSTALL_PREFIX_R/lib/libmlir*so
 "
 TERMUX_SUBPKG_DESCRIPTION="A Multi-Level Intermediate Representation for compilers from LLVM"
-TERMUX_SUBPKG_DEPENDS="libc++, ncurses, mlir"
+TERMUX_SUBPKG_DEPENDS="libc++, ncurses"
