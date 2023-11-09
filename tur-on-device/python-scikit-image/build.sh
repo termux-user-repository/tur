b451558c40b4ad738e89e02c1c3d4489e897f3db
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-11-09 23:32:17 +0800

    python-scikit-image: depends on python-scipy-2

--- tur-on-device/python-scikit-image/build.sh
+++ tur-on-device/python-scikit-image/build.sh
@@ -3,10 +3,11 @@ TERMUX_PKG_DESCRIPTION="Image processing in Python"
 TERMUX_PKG_LICENSE="BSD 2-Clause, BSD 3-Clause, MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="0.22.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/scikit-image/scikit-image/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=2875c81ffb224f9f25a1274734b502fb993cc729a2c790009d38035e52e6a123
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, python, python-numpy, python-pillow, python-pywavelets, python-scipy"
+TERMUX_PKG_DEPENDS="libc++, python, python-numpy, python-pillow, python-pywavelets, python-scipy-2"
 TERMUX_PKG_BUILD_DEPENDS="python-pip"
 TERMUX_PKG_SETUP_PYTHON=true
 TERMUX_PKG_PYTHON_TARGET_DEPS_="'networkx>=2.8', 'imageio>=2.27', 'tifffile>=2022.8.12', 'packaging>=21', 'lazy_loader>=0.3'"
