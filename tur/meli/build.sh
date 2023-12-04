8cc78c664137341ed75c7eeaf64c6348e3ca819b
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-12-04 15:37:27 +0000

    meli: switch to `latest-release-tag`
    [skip ci]

--- tur/meli/build.sh
+++ tur/meli/build.sh
@@ -4,7 +4,6 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="0.8.4"
 TERMUX_PKG_SRCURL=git+https://github.com/meli/meli
-TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 # gpgme and notmuch are dlopened.
 TERMUX_PKG_DEPENDS="gpgme, libcurl, libsqlite, notmuch, openssl, pcre2"
@@ -12,6 +11,8 @@ TERMUX_PKG_ANTI_BUILD_DEPENDS="gpgme, notmuch"
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 # `meli` assumes that `(u)size_t` is 64-bit, but it is not true on 32-bit Android.
 TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 
 termux_step_pre_configure() {
 	termux_setup_rust
