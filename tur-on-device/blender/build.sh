a923105dd2f9acfe61e0648b69a6a78ab9483084
Ansh Dadwal <anshdadwal298@gmail.com>
2023-12-23 09:44:24 +0530

    tur-on-device(blender): update to latest commit

--- tur-on-device/blender/build.sh
+++ tur-on-device/blender/build.sh
@@ -5,16 +5,17 @@ TERMUX_PKG_MAINTAINER="@T-Dynamos"
 BLENDER_MAJOR_VERSION=4 
 BLENDER_MINOR_VERSION=0
 BLENDER_ADDONS_COMMIT=92dd274c0bf08ad4786e7dfe715dee327f3ee43f 
-_COMMIT=0d7b0045c6795ba5ebd73bc588cb808d85ed10da
-_COMMIT_DATE=2023.08.03
+_COMMIT=63e9cead5ff002921eb4921037971e24d1c61abc
+_COMMIT_DATE=2023.12.18
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_VERSION=${BLENDER_MAJOR_VERSION}.${BLENDER_MINOR_VERSION}-${_COMMIT:0:8}-${_COMMIT_DATE}
 TERMUX_PKG_SRCURL=git+https://github.com/blender/blender
-TERMUX_PKG_GIT_BRANCH=main 
+TERMUX_PKG_GIT_BRANCH=blender-v4.0-release
 
 # Blender does not support 32bit
 TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
-TERMUX_PKG_DEPENDS="libpng, libtiff, python, python-pip, python-numpy, openexr, desktop-file-utils, potrace, shared-mime-info, hicolor-icon-theme, glew, openjpeg, freetype, ffmpeg, fftw, alembic, libsndfile, ptex, sdl2, libspnav, openal-soft, opencolorio, libblosc, sse2neon, libandroid-execinfo, brotli, libepoxy, openimageio, pybind11, openvdb, libraw, libpugixml"
+TERMUX_PKG_DEPENDS="libpng, libtiff, python, python-pip, python-numpy, openexr, desktop-file-utils, potrace, shared-mime-info, hicolor-icon-theme, glew, openjpeg, freetype, ffmpeg, fftw, alembic, libsndfile, ptex, sdl2, libspnav, openal-soft, opencolorio, libblosc, sse2neon, libandroid-execinfo, brotli, libepoxy, openimageio, pybind11, openvdb, libraw, libpugixml, shaderc"
 TERMUX_PKG_BUILD_DEPENDS="subversion"
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_PYTHON_COMMON_DEPS="requests, zstandard, Cython"
@@ -40,29 +41,30 @@ bin/normalizer
 
 termux_step_post_get_source() {
     git fetch --unshallow
-	git checkout $_COMMIT
-	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
-	if [ "$version" != "$_COMMIT_DATE" ]; then
-		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
-		echo " is different from what is expected to be: \"$version\""
-		return 1
-	fi
+    git checkout $_COMMIT
+    local version="$(git log -1 --format=%cs | sed 's/-/./g')"
+    if [ "$version" != "$_COMMIT_DATE" ]; then
+        echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
+        echo " is different from what is expected to be: \"$version\""
+        return 1
+    fi
 }
 
 termux_step_pre_configure(){
-	if [ "${TERMUX_ON_DEVICE_BUILD}" = false ]; then
-		termux_error_exit "This package doesn't support cross-compiling."
-	fi
-
+    if [ "${TERMUX_ON_DEVICE_BUILD}" = false ]; then
+        termux_error_exit "This package doesn't support cross-compiling."
+    fi 
     # ld.lld: error: undefined symbol: backtrace
     LDFLAGS+=" -landroid-execinfo"
+    # ld.lld: error: version script assignment 
+    LDFLAGS+=" -Wl,--undefined-version"
     # Position independent executables are not supported on android
     sed -i "s/no-pie/pie/g" $TERMUX_PKG_SRCDIR/build_files/cmake/platform/platform_unix.cmake
 }
 
 termux_step_post_make_install() {
-    export MODIR=$TERMUX_PREFIX/share/blender/$BLENDER_MAJOR_VERSION.$BLENDER_MINOR_VERSION/scripts/modules
-    curl -L https://github.com/blender/blender-addons/archive/$BLENDER_ADDONS_COMMIT.tar.gz | tar xvz -C $MODIR/
-    cp -r $MODIR/blender-addons-$BLENDER_ADDONS_COMMIT/* $MODIR/
+    export MODIR=$TERMUX_PREFIX/share/blender/$BLENDER_MAJOR_VERSION.$BLENDER_MINOR_VERSION/scripts/modules 
+    curl -L https://github.com/blender/blender-addons/archive/$BLENDER_ADDONS_COMMIT.tar.gz | tar xvz -C $MODIR/ 
+    cp -r $MODIR/blender-addons-$BLENDER_ADDONS_COMMIT/* $MODIR/ 
     rm -rf $MODIR/blender-addons-$BLENDER_ADDONS_COMMIT
 }
