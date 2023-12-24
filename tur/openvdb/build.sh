e0737f3c9b4fe8002ac220f61d38ca728d169e4f
Ansh Dadwal <anshdadwal298@gmail.com>
2023-12-24 19:41:22 +0530

    tur(openvdb): bump to rebuild against boost (#751)

--- tur/openvdb/build.sh
+++ tur/openvdb/build.sh
@@ -3,9 +3,11 @@ TERMUX_PKG_DESCRIPTION="A large suite of tools for the efficient storage and man
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
+# 11.0.0 is not supported for OpenImageIO
+# https://github.com/AcademySoftwareFoundation/OpenImageIO/pull/4023
 TERMUX_PKG_VERSION=10.0.1
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/dreamworksanimation/openvdb/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=2
+TERMUX_PKG_SRCURL=https://github.com/AcademySoftwareFoundation/openvdb/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=887a3391fbd96b20c77914f4fb3ab4b33d26e5fc479aa036d395def5523c622f
 TERMUX_PKG_DEPENDS="boost, libtbb, zlib, libblosc"
 TERMUX_PKG_BUILD_DEPENDS="mesa, glfw, glu"
