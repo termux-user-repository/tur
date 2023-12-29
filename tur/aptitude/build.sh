0f32fdf9c0c4a0869a455e4b3ee1edf3acf07237
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-12-30 00:51:00 +0800

    aptitude: rebuild with clang

--- tur/aptitude/build.sh
+++ tur/aptitude/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="terminal-based package manager"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=0.8.13
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=http://deb.debian.org/debian/pool/main/a/aptitude/aptitude_$TERMUX_PKG_VERSION.orig.tar.xz
 TERMUX_PKG_SHA256=0ef50cb5de27215dd30de74dd9b46b318f017bd0ec3f5c4735df7ac0beb40248
-TERMUX_PKG_DEPENDS="apt, boost, gettext, libcwidget, libsigc++-2.0, libsqlite, libxapian, ncurses"
+TERMUX_PKG_DEPENDS="apt, boost, libcwidget, libsigc++-2.0, libsqlite, libxapian, ncurses"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers, googletest"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-tests
@@ -15,18 +15,11 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-boost=$TERMUX_PREFIX
 --with-package-state-loc=$TERMUX_PREFIX/var/lib/aptitude
 --with-lock-loc=$TERMUX_PREFIX/var/lock/aptitude
---enable-nls
-nls_cv_use_gnu_gettext=yes
+--disable-nls
 "
 
-source $TERMUX_SCRIPTDIR/common-files/setup_toolchain_gcc.sh
-
 termux_step_pre_configure() {
-	_setup_toolchain_ndk_gcc_13
-	_override_configure_cmake_for_gcc
-
 	CXXFLAGS+=" -DNCURSES_WIDECHAR=1"
-	CXXFLAGS+=" -I$TERMUX_PREFIX/share/gettext"
 }
 
 termux_step_create_debscripts() {
