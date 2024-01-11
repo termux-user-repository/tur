0a142dd7a29ad486f2316a49535bdc3014135ad9
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-01-11 20:54:22 +0800

    suitesparse: bump to 7.5.0

--- tur/suitesparse/build.sh
+++ tur/suitesparse/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/DrTimothyAldenDavis/SuiteSparse
 TERMUX_PKG_DESCRIPTION="A Suite of Sparse matrix packages."
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=1:7.4.0
+TERMUX_PKG_VERSION=1:7.5.0
 TERMUX_PKG_SRCURL=https://github.com/DrTimothyAldenDavis/SuiteSparse/archive/refs/tags/v${TERMUX_PKG_VERSION#*:}.tar.gz
-TERMUX_PKG_SHA256=f9a5cc2316a967198463198f7bf10fb8c4332de6189b0e405419a7092bc921b7
+TERMUX_PKG_SHA256=9090ead43f462737369f1b6f8f269decc7f98adbb3276db299a2d4f18d481328
 TERMUX_PKG_DEPENDS="blas-openblas, libopenblas, libgmp, libmpfr"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_FORCE_CMAKE=true
@@ -88,7 +88,7 @@ lib/libccolamd.so.3
 lib/libcholmod.so.5
 lib/libcolamd.so.3
 lib/libcxsparse.so.4
-lib/libgraphblas.so.8
+lib/libgraphblas.so.9
 lib/libklu.so.2
 lib/libklu_cholmod.so.2
 lib/liblagraph.so.1
