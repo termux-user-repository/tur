608c8c723d03dbc487d7c409364daabb8d940419
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-02-21 21:42:19 +0800

    python-scipy: bump to 1.12.0

--- tur/python-scipy/build.sh
+++ tur/python-scipy/build.sh
@@ -2,11 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://scipy.org/
 TERMUX_PKG_DESCRIPTION="Fundamental algorithms for scientific computing in Python"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="1:1.11.4"
+TERMUX_PKG_VERSION="1:1.12.0"
 TERMUX_PKG_SRCURL=git+https://github.com/scipy/scipy
 TERMUX_PKG_DEPENDS="libc++, libopenblas, python, python-numpy"
 TERMUX_PKG_BUILD_DEPENDS="python-numpy-static"
-TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, 'Cython>=3.0.4', meson-python, build"
+_NUMPY_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python-numpy/build.sh; echo $TERMUX_PKG_VERSION)
+TERMUX_PKG_PYTHON_BUILD_DEPS="'pybind11>=2.10.4', 'numpy==$_NUMPY_VERSION'"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 
@@ -22,95 +24,60 @@ TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 # XXX: Although it doesn't seem to work fine, I'd like to enable this package as it happens only on some functions.
 # TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
+TERMUX_MESON_NUMPY_CROSSFILE="$TERMUX_PKG_TMPDIR/numpy-cross-file.txt"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dblas=openblas
+-Dlapack=openblas
+-Duse-pythran=false
+--cross-file $TERMUX_MESON_NUMPY_CROSSFILE
+"
+
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/
 "
 
 source $TERMUX_SCRIPTDIR/common-files/setup_toolchain_gcc.sh
 
-termux_step_post_get_source() {
-	cp _setup.py setup.py
-}
-
-termux_step_configure() {
+termux_step_pre_configure() {
 	if $TERMUX_ON_DEVICE_BUILD; then
 		termux_error_exit "Package '$TERMUX_PKG_NAME' is not available for on-device builds."
 	fi
 
-	_NUMPY_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python-numpy/build.sh; echo $TERMUX_PKG_VERSION)
-
 	_setup_toolchain_ndk_gcc_11
 
-	# XXX: `python` from main repo is built by TERMUX_STANDALONE_TOOLCHAIN and its _sysconfigdata.py
-	# XXX: contains some FLAGS which is not supported by GNU Compiler Collections, such as '-Oz',  
-	# XXX: `-static-openmp`. So we need to modify the _sysconfigdata.py.
-	SYS_CONFIG_DATA_FILE="$(find $TERMUX_PREFIX/lib/python${TERMUX_PYTHON_VERSION} -name "_sysconfigdata*.py")"
-	rm -rf  $TERMUX_PREFIX/lib/python${TERMUX_PYTHON_VERSION}/__pycache__
-	cp $SYS_CONFIG_DATA_FILE $TERMUX_PKG_TMPDIR/$(basename $SYS_CONFIG_DATA_FILE)
-	sed -E 's|-O[123sz]|-Os|g;s|-static-openmp||g' $TERMUX_PKG_TMPDIR/$(basename $SYS_CONFIG_DATA_FILE) |
-		sed "s|$TERMUX_HOST_PLATFORM-clang++|$TERMUX_HOST_PLATFORM-g++|g" |
-		sed "s|$TERMUX_HOST_PLATFORM-clang|$TERMUX_HOST_PLATFORM-gcc|g" > $SYS_CONFIG_DATA_FILE
-	rm $TERMUX_PKG_TMPDIR/$(basename $SYS_CONFIG_DATA_FILE)
+	LDFLAGS+=" -Wl,--no-as-needed,-lpython${TERMUX_PYTHON_VERSION},--as-needed"
 
-	termux_setup_python_pip
+	# FIXME: Don't know why NDK's libc++ should link against clang's libunwind,
+	# FIXME: otherwise pybind11's `register_local_exception_translator` won't
+	# FIXME: work properly, causing crash on `scipy.io._mmio`.
+	mkdir -p $TERMUX_PKG_TMPDIR/_libunwind_libdir
+	local _NDK_ARCH=$TERMUX_ARCH
+	test $_NDK_ARCH == 'i686' && _NDK_ARCH='i386'
+	cp $NDK/toolchains/llvm/prebuilt/linux-x86_64/lib/clang/*/lib/linux/$_NDK_ARCH/libunwind.a \
+		$TERMUX_PKG_TMPDIR/_libunwind_libdir/libunwind.a
+	LDFLAGS="-L$TERMUX_PKG_TMPDIR/_libunwind_libdir -l:libunwind.a ${LDFLAGS}"
+}
 
-	LDFLAGS+=" -Wl,--no-as-needed,-lpython${TERMUX_PYTHON_VERSION}"
+termux_step_configure() {
+	termux_setup_meson
+
+	cp -f $TERMUX_MESON_CROSSFILE $TERMUX_MESON_NUMPY_CROSSFILE
+	sed -i 's|^\(\[binaries\]\)$|\1\npython = '\'$(command -v python)\''|g' \
+		$TERMUX_MESON_NUMPY_CROSSFILE
+	sed -i 's|^\(\[properties\]\)$|\1\nnumpy-include-dir = '\'$PYTHON_SITE_PKG/numpy/core/include\''|g' \
+		$TERMUX_MESON_NUMPY_CROSSFILE
+
+	termux_step_configure_meson
 }
 
 termux_step_make() {
-	pip --no-cache-dir install wheel
-	build-pip install numpy==$_NUMPY_VERSION pybind11 "Cython<3" pythran wheel
-
-	BUILD_SITE=${TERMUX_PYTHON_CROSSENV_PREFIX}/build/lib/python${TERMUX_PYTHON_VERSION}/site-packages
-	DEVICE_SITE=$TERMUX_PREFIX/lib/python${TERMUX_PYTHON_VERSION}/site-packages
-
-	BUILD_INCDIR=$(find $BUILD_SITE -path '*/numpy/core/include')
-	DEVICE_INCDIR=$(find $DEVICE_SITE -path '*/numpy/core/include')
-
-	echo "Build INCDIR: $BUILD_INCDIR"
-	echo "Device INCDIR: $DEVICE_INCDIR"
-
-	rm -rf $BUILD_INCDIR
-	ln -sf $DEVICE_INCDIR $BUILD_INCDIR
-
-	cp $DEVICE_SITE/numpy/core/lib/libnpymath.a $TERMUX_PREFIX/lib
-	cp $DEVICE_SITE/numpy/random/lib/libnpyrandom.a $TERMUX_PREFIX/lib
-
-	cat <<- EOF > site.cfg
-	[openblas]
-	libraries = openblas
-	library_dirs = $TERMUX_PREFIX/lib
-	include_dirs = $TERMUX_PREFIX/include
-	EOF
-
-	# XXX: It's hard for pip to work, as scipy uses meson-python as backend.
-	# XXX: More investigations are needed.
-	F90=$FC F77=$FC python setup.py bdist_wheel -v
+	pushd $TERMUX_PKG_SRCDIR
+	python -m build -w -n -x --config-setting builddir=$TERMUX_PKG_BUILDDIR .
+	popd
 }
 
 termux_step_make_install() {
-	export PYTHONPATH="$DEVICE_SITE"
-	F90=$FC F77=$FC pip install ./dist/*.whl --no-deps --prefix=$TERMUX_PREFIX
-}
-
-termux_step_post_make_install() {
-	# Remove these dummy files.
-	rm $TERMUX_PREFIX/lib/libnpymath.a
-	rm $TERMUX_PREFIX/lib/libnpyrandom.a
-	# Remove __pycache__ and _sysconfigdata.py
-	rm $SYS_CONFIG_DATA_FILE
-	rm -rf $TERMUX_PREFIX/lib/python${TERMUX_PYTHON_VERSION}/__pycache__
-}
-
-termux_step_create_debscripts() {
-	cat <<- EOF > ./postinst
-	#!$TERMUX_PREFIX/bin/sh
-	INSTALLED_NUMPY_VERSION=\$(dpkg --list python-numpy | grep python-numpy | awk '{print \$3; exit;}')
-	if [ "\${INSTALLED_NUMPY_VERSION%%-*}" != "$_NUMPY_VERSION" ]; then
-		echo "WARNING: python-scipy is compiled with numpy $_NUMPY_VERSION, but numpy \${INSTALLED_NUMPY_VERSION%%-*} is installed. It seems that python-numpy has been upgraded. Please report it to https://github.com/termux-user-repository/tur if any bug happens."
-	fi
-	if [ "$TERMUX_ARCH" = "arm" ] || [ "$TERMUX_ARCH" = "i686" ]; then
-		echo "WARNING: python-numpy doesn't work fine on 32-bit arches. See https://github.com/termux-user-repository/tur/pull/21#issue-1295483266 for detail."
-	fi
-	EOF
+	local _pyv="${TERMUX_PYTHON_VERSION/./}"
+	local _whl="scipy-${TERMUX_PKG_VERSION#*:}-cp$_pyv-cp$_pyv-linux_$TERMUX_ARCH.whl"
+	pip install --no-deps --prefix=$TERMUX_PREFIX $TERMUX_PKG_SRCDIR/dist/$_whl
 }
