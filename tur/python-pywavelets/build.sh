879181a6edc49753c747c2bfc1af6a744911ace2
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-03-05 18:19:56 +0800

    python-pywavelets: revbump to rebuild

---
+++ tur/python-pywavelets/build.sh
@@ -0,0 +1,49 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/PyWavelets/pywt
+TERMUX_PKG_DESCRIPTION="Wavelet Transforms in Python"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="1.5.0"
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/PyWavelets/pywt/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=5aedfa9bd629f104a04fda88b92582bda38ab22282ce5048b5760b5d18e83fc9
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, python, python-pip, python-numpy"
+TERMUX_PKG_BUILD_DEPENDS="python-numpy-static"
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, 'Cython>=3.0.4', meson-python, build"
+
+TERMUX_MESON_PYWT_CROSSFILE="$TERMUX_PKG_TMPDIR/pywt-cross-file.txt"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--cross-file $TERMUX_MESON_PYWT_CROSSFILE
+"
+
+termux_step_pre_configure() {
+	if $TERMUX_ON_DEVICE_BUILD; then
+		termux_error_exit "Package '$TERMUX_PKG_NAME' is not available for on-device builds."
+	fi
+
+	LDFLAGS+=" -Wl,--no-as-needed -lpython${TERMUX_PYTHON_VERSION}"
+}
+
+termux_step_configure() {
+	termux_setup_meson
+
+	cp -f $TERMUX_MESON_CROSSFILE $TERMUX_MESON_PYWT_CROSSFILE
+	sed -i 's|^\(\[binaries\]\)$|\1\npython = '\'$(command -v python)\''|g' \
+		$TERMUX_MESON_PYWT_CROSSFILE
+	sed -i 's|^\(\[properties\]\)$|\1\nnumpy-include-dir = '\'$PYTHON_SITE_PKG/numpy/core/include\''|g' \
+		$TERMUX_MESON_PYWT_CROSSFILE
+
+	termux_step_configure_meson
+}
+
+termux_step_make() {
+	pushd $TERMUX_PKG_SRCDIR
+	python -m build -w -n -x --config-setting builddir=$TERMUX_PKG_BUILDDIR .
+	popd
+}
+
+termux_step_make_install() {
+	local _pyv="${TERMUX_PYTHON_VERSION/./}"
+	local _whl="pywavelets-${TERMUX_PKG_VERSION#*:}-cp$_pyv-cp$_pyv-linux_$TERMUX_ARCH.whl"
+	pip install --no-deps --prefix=$TERMUX_PREFIX $TERMUX_PKG_SRCDIR/dist/$_whl
+}
