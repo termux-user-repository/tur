af59a94904a307b6d4f7a88852edba5df4643c43
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-03-06 17:05:05 +0800

    python-scikit-image: revbump to rebuild

---
+++ tur/python-scikit-image/build.sh
@@ -0,0 +1,52 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/scikit-image/scikit-image
+TERMUX_PKG_DESCRIPTION="Image processing in Python"
+TERMUX_PKG_LICENSE="BSD 2-Clause, BSD 3-Clause, MIT"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="0.22.0"
+TERMUX_PKG_REVISION=3
+TERMUX_PKG_SRCURL=https://github.com/scikit-image/scikit-image/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=2875c81ffb224f9f25a1274734b502fb993cc729a2c790009d38035e52e6a123
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, python, python-pip, python-numpy, python-pillow, python-pywavelets, python-scipy"
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, 'Cython>=3.0.4', meson-python, build"
+_NUMPY_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python-numpy/build.sh; echo $TERMUX_PKG_VERSION)
+TERMUX_PKG_PYTHON_BUILD_DEPS="pythran, 'numpy==$_NUMPY_VERSION'"
+TERMUX_PKG_SETUP_PYTHON=true
+
+TERMUX_MESON_SKIMAGE_CROSSFILE="$TERMUX_PKG_TMPDIR/skimage-cross-file.txt"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--cross-file $TERMUX_MESON_SKIMAGE_CROSSFILE
+"
+
+termux_step_pre_configure() {
+	if $TERMUX_ON_DEVICE_BUILD; then
+		termux_error_exit "Package '$TERMUX_PKG_NAME' is not available for on-device builds."
+	fi
+
+	LDFLAGS+=" -Wl,--no-as-needed -lpython${TERMUX_PYTHON_VERSION}"
+}
+
+termux_step_configure() {
+	termux_setup_meson
+
+	cp -f $TERMUX_MESON_CROSSFILE $TERMUX_MESON_SKIMAGE_CROSSFILE
+	sed -i 's|^\(\[binaries\]\)$|\1\npython = '\'$(command -v python)\''|g' \
+		$TERMUX_MESON_SKIMAGE_CROSSFILE
+	sed -i 's|^\(\[properties\]\)$|\1\nnumpy-include-dir = '\'$PYTHON_SITE_PKG/numpy/core/include\''|g' \
+		$TERMUX_MESON_SKIMAGE_CROSSFILE
+
+	termux_step_configure_meson
+}
+
+termux_step_make() {
+	unset PYTHONPATH
+	pushd $TERMUX_PKG_SRCDIR
+	python -m build -w -n -x --config-setting builddir=$TERMUX_PKG_BUILDDIR .
+	popd
+}
+
+termux_step_make_install() {
+	local _pyv="${TERMUX_PYTHON_VERSION/./}"
+	local _whl="scikit_image-${TERMUX_PKG_VERSION#*:}-cp$_pyv-cp$_pyv-linux_$TERMUX_ARCH.whl"
+	pip install --no-deps --prefix=$TERMUX_PREFIX $TERMUX_PKG_SRCDIR/dist/$_whl
+}
