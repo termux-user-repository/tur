77d6f9c638df55393f2cbf45c076cf03fc7377d7
Biswapriyo Nath <nathbappai@gmail.com>
2024-03-17 05:17:21 +0000

    bump(tur/python-yt-dlp): 2024.03.10

--- tur/python-yt-dlp/build.sh
+++ tur/python-yt-dlp/build.sh
@@ -2,14 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://github.com/yt-dlp/yt-dlp
 TERMUX_PKG_DESCRIPTION="A youtube-dl fork with additional features and fixes"
 TERMUX_PKG_LICENSE="Unlicense"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="2023.12.30"
+TERMUX_PKG_VERSION="2024.03.10"
 TERMUX_PKG_SRCURL=https://github.com/yt-dlp/yt-dlp/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=2878501990f3ab6aa8eabb7346c16d4b4cbc01984372a1f0b3a6be8d260c7aff
+TERMUX_PKG_SHA256=079dbf54586e8120c7d76e0dd1b7a60e6c13f4d23f9681fc919577f2fe17d8cd
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libexpat, openssl, python, python-brotli, python-pip, python-pycryptodomex"
 TERMUX_PKG_SUGGESTS="ffmpeg"
-TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, 'setuptools==67.8.0'"
-TERMUX_PKG_PYTHON_TARGET_DEPS="mutagen, pycryptodomex, websockets, certifi, brotli"
+TERMUX_PKG_PYTHON_COMMON_DEPS="hatchling, wheel, 'setuptools==67.8.0'"
+TERMUX_PKG_PYTHON_TARGET_DEPS="mutagen, pycryptodomex, websockets, certifi, brotli, requests, urllib3"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_HOSTBUILD=true
 
