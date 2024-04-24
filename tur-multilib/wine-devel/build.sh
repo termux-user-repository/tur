632adb9c897c43b3a891e529f410b6e82b20fad9
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-04-25 01:22:10 +0800

    wine-devel: bump to 9.7 (#959)

--- tur-multilib/wine-devel/build.sh
+++ tur-multilib/wine-devel/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="A compatibility layer for running Windows programs"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_LICENSE_FILE="LICENSE, LICENSE.OLD, COPYING.LIB"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=9.5
+TERMUX_PKG_VERSION=9.7
 _VERSION_FOLDER="$(test "${TERMUX_PKG_VERSION:2:1}" = 0 && echo ${TERMUX_PKG_VERSION:0:3} || echo ${TERMUX_PKG_VERSION:0:2}x)"
 TERMUX_PKG_SRCURL=https://dl.winehq.org/wine/source/${_VERSION_FOLDER}/wine-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=12cf2fb7098134e2351c49ea3ba8f4da2a674f1f8722bebd4c3a4a6ca6d2e975
+TERMUX_PKG_SHA256=d9f3c333656e88bd4cef5331f34b1c8b69c964a52759eef745d8ddae51a15353
 TERMUX_PKG_DEPENDS="fontconfig, freetype, krb5, libandroid-spawn, libc++, libgmp, libgnutls, libxcb, libxcomposite, libxcursor, libxfixes, libxrender, mesa, opengl, pulseaudio, sdl2, vulkan-loader, xorg-xrandr"
 TERMUX_PKG_ANTI_BUILD_DEPENDS="vulkan-loader"
 TERMUX_PKG_BUILD_DEPENDS="libandroid-spawn-static, vulkan-loader-generic"
@@ -75,11 +75,11 @@ exec_prefix=$TERMUX_PREFIX
 
 _setup_llvm_mingw_toolchain() {
 	# LLVM-mingw's version number must not be the same as the NDK's.
-	local _llvm_mingw_version=16
-	local _version="20230614"
+	local _llvm_mingw_version=18
+	local _version="20240417"
 	local _url="https://github.com/mstorsjo/llvm-mingw/releases/download/$_version/llvm-mingw-$_version-ucrt-ubuntu-20.04-x86_64.tar.xz"
 	local _path="$TERMUX_PKG_CACHEDIR/$(basename $_url)"
-	local _sha256sum=9ae925f9b205a92318010a396170e69f74be179ff549200e8122d3845ca243b8
+	local _sha256sum=d28ce4168c83093adf854485446011a0327bad9fe418014de81beba233ce76f1
 	termux_download $_url $_path $_sha256sum
 	local _extract_path="$TERMUX_PKG_CACHEDIR/llvm-mingw-toolchain-$_llvm_mingw_version"
 	if [ ! -d "$_extract_path" ]; then
@@ -118,7 +118,7 @@ termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-spawn"
 
 	# Enable win64 on 64-bit arches.
-	# TODO: Enable win32 after TUR has full support for mutilib 
+	# TODO: Enable win32 after TUR has full support for mutilib
 	if [ "$TERMUX_ARCH_BITS" = 64 ]; then
 		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --enable-win64"
 	fi
