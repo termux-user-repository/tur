a1ff16212c11b8a6218ae16fcb42c7eddb843527
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-05-10 18:02:24 +0800

    dotload: enable auto-update
    [skip ci]

--- tur/dotload/build.sh
+++ tur/dotload/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_VERSION="1.2.1"
 TERMUX_PKG_SRCURL="https://github.com/okineadev/dotload/archive/refs/tags/v$TERMUX_PKG_VERSION.zip"
 TERMUX_PKG_SHA256="e7dbf6f412d7eb9935c0d3a669a573a5376fed02a017e217b4a12a06ad1e4ba9"
 TERMUX_PKG_DEPENDS="git"
+TERMUX_PKG_AUTO_UPDATE=true
