495129b2301ed70787260617f5318f4e6f489186
Okinea Dev <81070564+okineadev@users.noreply.github.com>
2024-05-10 13:02:35 +0300

    new package: dotload (#1010)

---
+++ tur/dotload/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/okineadev/dotload"
+TERMUX_PKG_DESCRIPTION="This is a tool for installing dotfiles and other tools on any machine"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@okineadev"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_VERSION="1.2.1"
+TERMUX_PKG_SRCURL="https://github.com/okineadev/dotload/archive/refs/tags/v$TERMUX_PKG_VERSION.zip"
+TERMUX_PKG_SHA256="e7dbf6f412d7eb9935c0d3a669a573a5376fed02a017e217b4a12a06ad1e4ba9"
+TERMUX_PKG_DEPENDS="git"
+TERMUX_PKG_AUTO_UPDATE=true
