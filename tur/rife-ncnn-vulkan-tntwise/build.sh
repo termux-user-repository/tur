45f3f33414f7ed655cbd75998c3e004c33c27869
Ronald Y. <46513942+knyipab@users.noreply.github.com>
2024-05-15 14:33:34 +0000

    rife-ncnn-vulkan: some fixes

--- tur/rife-ncnn-vulkan-tntwise/build.sh
+++ tur/rife-ncnn-vulkan-tntwise/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_SRCURL=git+https://github.com/TNTwise/rife-ncnn-vulkan
 TERMUX_PKG_DEPENDS="libwebp, vulkan-loader"
 TERMUX_PKG_ANTI_BUILD_DEPENDS="vulkan-loader"
 TERMUX_PKG_BUILD_DEPENDS="vulkan-loader-generic"
-TERMUX_PKG_GIT_BRANCH=20240326
+TERMUX_PKG_GIT_BRANCH="$TERMUX_PKG_VERSION"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
@@ -15,15 +15,29 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DUSE_SYSTEM_WEBP=ON"
 TERMUX_PKG_CONFLICTS="rife-ncnn-vulkan-nihui"
 
 termux_step_pre_configure () {
-	git submodule update --init --recursive "$TERMUX_PKG_SRCDIR"
 	mv "$TERMUX_PKG_SRCDIR/src/"* "$TERMUX_PKG_SRCDIR/"
 	LDFLAGS+=" -llog -landroid"
 }
 
 termux_step_make_install () {
 	mkdir -p "$TERMUX_PREFIX/opt/rife-ncnn-vulkan"
+	
+	# install binary
 	install -Dm700 rife-ncnn-vulkan "$TERMUX_PREFIX/opt/rife-ncnn-vulkan"
-	ln -s "$TERMUX_PREFIX/opt/rife-ncnn-vulkan/rife-ncnn-vulkan" "$TERMUX_PREFIX/bin/rife-ncnn-vulkan"
+	local system_lib="/system/lib"
+	[[ "${TERMUX_ARCH_BITS}" == "64" ]] && system_lib+="64"
+	system_lib+="/libvulkan.so"
+	local prefix_lib="${TERMUX_PREFIX}/lib/libvulkan.so"
+	
+	cat <<- EOF > "$TERMUX_PREFIX/bin/rife-ncnn-vulkan"
+	#!${TERMUX_PREFIX}/bin/sh
+	if [ -e "${system_lib}" ]; then
+	export LD_PRELOAD="${system_lib}:\$LD_PRELOAD"
+	fi
+	"$TERMUX_PREFIX/opt/rife-ncnn-vulkan" "\$@"
+	EOF
+	
+	# install models
 	cp -r models/rife-v2.3 "$TERMUX_PREFIX/opt/rife-ncnn-vulkan/rife-v2.3" # default value of model arg
 	cp -r models/rife-v4.6 "$TERMUX_PREFIX/opt/rife-ncnn-vulkan/rife-v4.6" # nihui's repo latest model
 	max_ver=0.0
