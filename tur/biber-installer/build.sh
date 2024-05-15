fa51602f36445084bcbdf8ba0700055d69ba424c
Henrik Grimler <grimler@termux.dev>
2024-05-15 21:42:11 +0200

    bump(tur/biber-installer): 2.20

--- tur/biber-installer/build.sh
+++ tur/biber-installer/build.sh
@@ -2,8 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://biblatex-biber.sourceforge.net
 TERMUX_PKG_DESCRIPTION="A Unicode-capable BibTeX replacement for biblatex users"
 TERMUX_PKG_LICENSE="Artistic-License-2.0"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=2.19
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.20
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_DEPENDS="clang, libxml2 (>> 2.9.10-3), libxslt, make, openssl-tool, perl (>> 5.34.0-3), perl-net-ssleay, pkg-config"
@@ -12,21 +11,18 @@ TERMUX_PKG_CONFLICTS="biber"
 
 EXTUTILS_LIBBUILDER_VERSION=0.08
 TEXT_BIBTEX_VERSION=0.89
-XML_LIBXML_VERSION=2.0208
 
 termux_step_make_install() {
 	mkdir -p $TERMUX_PREFIX/opt/biber
 	sed -e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
 		-e "s|@TEXT_BIBTEX_VERSION@|${TEXT_BIBTEX_VERSION}|g" \
 		-e "s|@EXTUTILS_LIBBUILDER_VERSION@|${EXTUTILS_LIBBUILDER_VERSION}|g" \
-		-e "s|@XML_LIBXML_VERSION@|${XML_LIBXML_VERSION}|g" \
 		-e "s|@BIBER_VERSION@|${TERMUX_PKG_VERSION}|g" \
 		"$TERMUX_PKG_BUILDER_DIR"/installer.sh \
 		> $TERMUX_PREFIX/bin/termux-install-biber
 	chmod 700 $TERMUX_PREFIX/bin/termux-install-biber
 	install -m600 "$TERMUX_PKG_BUILDER_DIR"/ExtUtils-LibBuilder.diff $TERMUX_PREFIX/opt/biber/
 	install -m600 "$TERMUX_PKG_BUILDER_DIR"/Text-BibTeX.diff $TERMUX_PREFIX/opt/biber/
-	install -m600 "$TERMUX_PKG_BUILDER_DIR"/XML-LibXML.diff $TERMUX_PREFIX/opt/biber/
 	# Uninstalling all dependencies on uninstall would be annoying, so
 	# lets leave that for the user to deal with..
 }
