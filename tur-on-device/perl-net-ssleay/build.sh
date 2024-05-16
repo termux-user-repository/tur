651ef03dc028aa96b709afbc6aaa0ce80150a070
Henrik Grimler <grimler@termux.dev>
2024-05-16 17:39:06 +0200

    bump(tur-on-device/perl-net-ssleay): 1.94 (#1022)

--- tur-on-device/perl-net-ssleay/build.sh
+++ tur-on-device/perl-net-ssleay/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://metacpan.org/dist/Net-SSLeay/
 TERMUX_PKG_DESCRIPTION="Perl bindings for OpenSSL and LibreSSL"
 TERMUX_PKG_LICENSE="Artistic-License-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=1.93-02
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.94
 TERMUX_PKG_SRCURL=https://github.com/radiator-software/p5-net-ssleay/archive/refs/tags/${TERMUX_PKG_VERSION/-/_}.tar.gz
-TERMUX_PKG_SHA256=bb7f315a9aadacc72f2bc50f02b0b084edaa6b608c1efc63c972df001a6b644f
+TERMUX_PKG_SHA256=a73aee174e400030ba10d78fa8c4cf60c4d1275302d66379c23271dc91c14e7c
 TERMUX_PKG_DEPENDS="perl, openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 
