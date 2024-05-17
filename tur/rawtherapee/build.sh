c7b7648b5611cd1bc41a48a1ca6f3c4f71e253c5
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-05-17 14:44:13 +0800

    rawtherapee: link against libjpeg-turbo 3.0.3

--- tur/rawtherapee/build.sh
+++ tur/rawtherapee/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="raw image converter and digital photo processor"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="5.10"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Beep6581/RawTherapee/releases/download/${TERMUX_PKG_VERSION}/rawtherapee-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=a799b53cd54dba4a211479e342ffc9c5db1c44d3d6c3a27d5cc13adf0debd2da
 TERMUX_PKG_DEPENDS="exiv2, fftw, glib, gtk3, gtkmm3, lensfun, libcanberra, libexpat, libglibmm-2.4, libiptcdata, libjpeg-turbo, libpng, librsvg, libsigc++-2.0, libtiff, littlecms, zlib"
