3edfca19236a0efa4c5ab46da73430e779f544da
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-07-20 00:29:11 +0800

    supertuxkart: split packages

---
+++ tur/supertuxkart/supertuxkart-data.subpackage.sh
@@ -0,0 +1,7 @@
+TERMUX_SUBPKG_DESCRIPTION="Data files for supertuxkart (metapackage)"
+TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
+TERMUX_SUBPKG_DEPEND_ON_PARENT=no
+TERMUX_SUBPKG_DEPENDS="supertuxkart-data-others, supertuxkart-data-textures, supertuxkart-data-tracks"
+TERMUX_SUBPKG_INCLUDE="
+share/supertuxkart/data/.placeholder
+"
