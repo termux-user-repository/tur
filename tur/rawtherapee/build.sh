f6681dd68fe91dcdf29b0aee7f5cc824280ee872
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-09-01 02:42:40 +0800

    rawtherapee: bump to 5.11

--- tur/rawtherapee/build.sh
+++ tur/rawtherapee/build.sh
@@ -2,14 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://www.rawtherapee.com/
 TERMUX_PKG_DESCRIPTION="raw image converter and digital photo processor"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="5.10"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="5.11"
 TERMUX_PKG_SRCURL=https://github.com/Beep6581/RawTherapee/releases/download/${TERMUX_PKG_VERSION}/rawtherapee-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=a799b53cd54dba4a211479e342ffc9c5db1c44d3d6c3a27d5cc13adf0debd2da
-TERMUX_PKG_DEPENDS="exiv2, fftw, glib, gtk3, gtkmm3, lensfun, libcanberra, libexpat, libglibmm-2.4, libiptcdata, libjpeg-turbo, libpng, librsvg, libsigc++-2.0, libtiff, littlecms, zlib"
+TERMUX_PKG_SHA256=e584c18dec112de29954b2b6471449a302a85e5cca4e42ede75fa333a36de724
+TERMUX_PKG_DEPENDS="exiv2, fftw, glib, gtk3, gtkmm3, lensfun, libcanberra, libexpat, libglibmm-2.4, libiptcdata, libjpeg-turbo, libpng, libraw, librsvg, libsigc++-2.0, libtiff, littlecms, zlib"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+"
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DBINARY_PACKAGE_BUILD=ON
+-DWITH_SYSTEM_LIBRAW=TRUE
 "
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -fopenmp -static-openmp"
+}
