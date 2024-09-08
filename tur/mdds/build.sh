cda59534264a90a2348b79db93072affa2661009
Ronald Y <knyipab@gmail.com>
2024-09-08 14:41:51 +0800

    new package: mdds

---
+++ tur/mdds/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://gitlab.com/mdds/mdds
+TERMUX_PKG_DESCRIPTION="A collection of multi-dimensional data structures and indexing algorithms"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION=2.1.1
+TERMUX_PKG_SRCURL=https://gitlab.com/api/v4/projects/mdds%2Fmdds/packages/generic/source/${TERMUX_PKG_VERSION}/mdds-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=1483d90cefb8aa4563c4d0a85cb7b243aa95217d235d422e9ca6722fd5b97e56
+TERMUX_PKG_AUTO_UPDATE=true
