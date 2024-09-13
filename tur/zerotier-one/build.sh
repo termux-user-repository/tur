8c7790df2f834b5d0d6d2f493211f69a04a7daf7
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-09-13 22:51:54 +0800

    zerotier-one: bump to 1.14.1

--- tur/zerotier-one/build.sh
+++ tur/zerotier-one/build.sh
@@ -4,12 +4,12 @@ TERMUX_PKG_DESCRIPTION="Creates virtual Ethernet networks of almost unlimited si
 TERMUX_PKG_LICENSE="non-free"
 TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.14.0"
+TERMUX_PKG_VERSION="1.14.1"
 TERMUX_PKG_SRCURL=https://github.com/zerotier/ZeroTierOne/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=4f9f40b27c5a78389ed3f3216c850921f6298749e5819e9f2edabb2672ce9ca0
 TERMUX_PKG_DEPENDS="miniupnpc, natpmpc, openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_SHA256=7191623a81b0d1b552b9431e8864dd3420783ee518394ac1376cee6aaf033291
 TERMUX_PKG_SERVICE_SCRIPT=(
 	'zerotier-one' 'exec su -c "'$TERMUX_PREFIX'/bin/zerotier-one -d"'
 )
@@ -18,15 +18,16 @@ termux_step_configure() {
 	termux_setup_rust
 
 	sed \
-	-e 's/usr\///g' \
-	-e 's/sbin/bin/g' \
-	-e 's/LDFLAGS=/LDFLAGS?=/' \
-	-e 's/RUSTFLAGS=/RUSTFLAGS?=/' \
-	-i make-linux.mk
+		-e 's/usr\///g' \
+		-e 's/sbin/bin/g' \
+		-e 's/LDFLAGS=/LDFLAGS?=/' \
+		-e 's/RUSTFLAGS=/RUSTFLAGS?=/' \
+		-i make-linux.mk
 	make echo_flags
 }
 
 termux_step_make() {
+	export ASFLAGS+=" -c"
 	export DESTDIR="${TERMUX_PREFIX}"
 	make selftest -j $TERMUX_PKG_MAKE_PROCESSES ${QUIET_BUILD=""} ${TERMUX_PKG_EXTRA_MAKE_ARGS=""}
 	# ./zerotier-selftest
