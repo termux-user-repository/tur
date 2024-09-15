80183d1cf9817bb6e50633e010bdd1c9ea2507b2
Ronald Y. <46513942+knyipab@users.noreply.github.com>
2024-09-15 16:54:47 +0800

    ollama: openblas backend (#1198)

--- tur/ollama/build.sh
+++ tur/ollama/build.sh
@@ -3,7 +3,9 @@ TERMUX_PKG_DESCRIPTION="Get up and running with large language models. "
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="0.3.10"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=git+https://github.com/ollama/ollama
+TERMUX_PKG_DEPENDS="libopenblas"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
@@ -12,9 +14,11 @@ TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 termux_step_pre_configure() {
 	termux_setup_golang
 	termux_setup_cmake
+	LDFLAGS+=" -fopenmp -static-openmp"
 }
 
 termux_step_make() {
+	export OLLAMA_CUSTOM_CPU_DEFS="-DGGML_BLAS=ON -DGGML_BLAS_VENDOR=OpenBLAS -DGGML_NATIVE=OFF -DGGML_OPENMP=ON -DGGML_AVX=OFF -DGGML_AVX2=OFF -DGGML_AVX512=OFF -DGGML_FMA=OFF -DGGML_F16C=OFF"
 	go generate './...'
 	go build
 }
