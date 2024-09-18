5de958ed65ade9aaee9435bc0cee30b66c240679
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-09-18 23:30:09 +0800

    libwps: revbump to rebuild

--- tur/libwps/build.sh
+++ tur/libwps/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="a Microsoft Works file word processor format import filt
 TERMUX_PKG_LICENSE="LGPL-2.1, MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=0.4.14
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/libwps/libwps/libwps-${TERMUX_PKG_VERSION}/libwps-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=365b968e270e85a8469c6b160aa6af5619a4e6c995dbb04c1ecc1b4dd13e80de
 TERMUX_PKG_DEPENDS="libwpd, librevenge"
