61fc4f9a8820883a566e53c416844d9afdf8ce11
Ronald Y. <46513942+knyipab@users.noreply.github.com>
2024-09-18 11:25:54 +0800

    clucene: include contribs-lib (#1207)

--- tur/clucene/build.sh
+++ tur/clucene/build.sh
@@ -3,10 +3,15 @@ TERMUX_PKG_DESCRIPTION="C++ port of the high-performance text search engine Luce
 TERMUX_PKG_LICENSE="Apache-2.0, LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=2.3.3.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/clucene/clucene-core-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ddfdc433dd8ad31b5c5819cc4404a8d2127472a3b720d3e744e8c51d79732eab
 TERMUX_PKG_DEPENDS="boost, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers"
+TERMUX_PKG_EXTRA_MAKE_ARGS="
+-DENABLE_ASCII_MODE=OFF
+-DBUILD_CONTRIBS_LIB:BOOL=ON
+"
 
 termux_step_pre_configure() {
 	CXXFLAGS+=" -std=c++11"
