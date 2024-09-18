999310cacc215c5ed6eb1d8dc6322ed87f49835c
Ronald Y <knyipab@gmail.com>
2024-09-18 15:24:19 +0800

    box2d: include files

--- tur/box2d/build.sh
+++ tur/box2d/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="2D rigid body simulation library for games"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=3.0.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/erincatto/Box2D/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=64ad759006cd2377c99367f51fb36942b57f0e9ad690ed41548dd620e6f6c8b1
 TERMUX_PKG_BUILD_DEPENDS="git, glfw, libxcursor, libxinerama, libxkbcommon, libxt, xorg-xrandr"
@@ -15,4 +16,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -lglfw -lGL"
+}
+
+termux_step_post_make_install() {
+	cp -rf "$TERMUX_PKG_SRCDIR/include" "$TERMUX_PREFIX/"
 }
