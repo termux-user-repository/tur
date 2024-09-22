2047554c21a8434806a69aa618cff4e7ab9bf7cb
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-09-22 13:09:05 +0800

    python-pandas: bump to 2.2.3

--- tur/python-pandas/build.sh
+++ tur/python-pandas/build.sh
@@ -3,13 +3,11 @@ TERMUX_PKG_HOMEPAGE=https://pandas.pydata.org/
 TERMUX_PKG_DESCRIPTION="Powerful Python data analysis toolkit"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="2.2.2"
+TERMUX_PKG_VERSION="2.2.3"
 TERMUX_PKG_SRCURL=git+https://github.com/pandas-dev/pandas
-TERMUX_PKG_SHA256=d8abf9c2bf33cac75b28f32c174c29778414eb249e5e2ccb69b1079b97a8fc66
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, python, python-numpy, python-pip"
-TERMUX_PKG_PYTHON_COMMON_DEPS="'Cython==3.0.5', numpy, wheel, 'setuptools==63.2.0', versioneer"
-TERMUX_PKG_PYTHON_TARGET_DEPS="'python-dateutil>=2.8.2', 'pytz>=2020.1', 'tzdata>=2022.1'"
+TERMUX_PKG_PYTHON_COMMON_DEPS="'Cython==3.0.5', 'numpy==1.26.4', wheel, 'setuptools==63.2.0', versioneer"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 
@@ -36,6 +34,6 @@ termux_step_create_debscripts() {
 	cat <<- EOF > ./postinst
 	#!$TERMUX_PREFIX/bin/sh
 	echo "Installing dependencies through pip..."
-	pip3 install ${TERMUX_PKG_PYTHON_TARGET_DEPS//, / }
+	pip3 install pandas
 	EOF
 }
