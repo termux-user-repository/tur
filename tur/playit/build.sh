72bffc77517dcbb13150e9d2fb2dc95eec13dbe1
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-09-22 12:53:36 +0800

    playit: bump to 0.15.23

--- tur/playit/build.sh
+++ tur/playit/build.sh
@@ -2,13 +2,14 @@ TERMUX_PKG_HOMEPAGE="https://playit.gg"
 TERMUX_PKG_DESCRIPTION="playit is a global proxy that allows anyone to host a server without port forwarding."
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@nisheri-ascar"
-TERMUX_PKG_VERSION="0.15.11"
+TERMUX_PKG_VERSION="0.15.23"
 TERMUX_PKG_SRCURL=https://github.com/playit-cloud/playit-agent/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ccdc750df5ebf208d9454fc431b9273321de87b0deca1c01b86a2428ef17daa8
+TERMUX_PKG_SHA256=b31f483b1fcb0950b67506d770af54816f4071627a0e9042f6cda22b4a2e6232
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
-		termux_setup_rust
+	termux_setup_rust
 }
 
 termux_step_make() {
