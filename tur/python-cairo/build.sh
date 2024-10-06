6a13d709eed6ebfe63edef96527a4698aa357cf4
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-10-07 00:51:05 +0800

    python-cairo: python 3.12 update

--- tur/python-cairo/build.sh
+++ tur/python-cairo/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="This package contains modules that allow you to use the 
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@fervi"
 TERMUX_PKG_VERSION="1.26.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/pygobject/pycairo/releases/download/v${TERMUX_PKG_VERSION}/pycairo-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a11b999ce55b798dbf13516ab038e0ce8b6ec299b208d7c4e767a6f7e68e8430
 TERMUX_PKG_DEPENDS="python, libcairo"
