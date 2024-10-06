0ec469956703fbdfd450511d4d4c7771ac20b42f
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-10-07 00:52:09 +0800

    python-pygame: python 3.12 update

--- tur/python-pygame/build.sh
+++ tur/python-pygame/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="pygame is a python game library"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="2.6.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/pygame/pygame/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6a5dd68af93a11ba6eb35c971fa220bf253cebf63b1f54cbe697b30fae9c7c72
 TERMUX_PKG_DEPENDS="x11-repo, freetype, portmidi, python, sdl2, sdl2-image, sdl2-mixer, sdl2-ttf"
