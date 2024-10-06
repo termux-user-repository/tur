04436b55f41ea18b329b107fb77326e5b722a054
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-10-07 00:53:24 +0800

    python-tls-client: python 3.12 update

--- tur/python-tls-client/build.sh
+++ tur/python-tls-client/build.sh
@@ -3,11 +3,12 @@ TERMUX_PKG_DESCRIPTION="Advanced HTTP Library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="1.0.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/FlorianREGAZ/Python-Tls-Client/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=789e2a1657113649772c9e20d77fc1f42db939045ca3e88b6a5714da7f2a1e8b
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, python, lib-tls-client"
-TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, 'setuptools==67.8.0'"
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_get_source() {
