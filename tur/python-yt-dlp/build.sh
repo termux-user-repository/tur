6d82bd4770904f2476eba19e66bcde95b8f0b958
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-10-07 00:26:25 +0800

    python-yt-dlp: python 3.12 update

--- tur/python-yt-dlp/build.sh
+++ tur/python-yt-dlp/build.sh
@@ -3,12 +3,13 @@ TERMUX_PKG_DESCRIPTION="A youtube-dl fork with additional features and fixes"
 TERMUX_PKG_LICENSE="Unlicense"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="2024.09.27"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/yt-dlp/yt-dlp/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=2b43e46d96321ad1186f7109c6972a887f55e90dbdcccccc3fdf4a499d8c4c75
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libexpat, openssl, python, python-brotli, python-pip, python-pycryptodomex"
 TERMUX_PKG_SUGGESTS="ffmpeg"
-TERMUX_PKG_PYTHON_COMMON_DEPS="hatchling, wheel, 'setuptools==67.8.0'"
+TERMUX_PKG_PYTHON_COMMON_DEPS="hatchling, wheel"
 TERMUX_PKG_PYTHON_TARGET_DEPS="mutagen, pycryptodomex, websockets, certifi, brotli, requests, urllib3"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_HOSTBUILD=true
