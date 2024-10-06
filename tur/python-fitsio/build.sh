759ffd8daa0765e8c52ed6f6426b0bbb5851d4ea
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-10-07 00:36:01 +0800

    python-fitsio: python 3.12 update

--- tur/python-fitsio/build.sh
+++ tur/python-fitsio/build.sh
@@ -3,10 +3,12 @@ TERMUX_PKG_DESCRIPTION="A python package for FITS input/output wrapping cfitsio"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="1.2.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/esheldon/fitsio/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=2b6d219674bb720dd25a793fd5be281b44a742d03a23d60803bac92e406a9afd
 TERMUX_PKG_DEPENDS="cfitsio, python, python-numpy"
-TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, numpy"
+_NUMPY_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python-numpy/build.sh; echo $TERMUX_PKG_VERSION)
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, 'numpy==$_NUMPY_VERSION'"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
