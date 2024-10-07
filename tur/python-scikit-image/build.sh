9719ca72b765243f93e3fc993d4a186e1ae3815b
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-10-08 01:56:10 +0800

    python-scikit-image: python 3.12 update (#1243)

--- tur/python-scikit-image/build.sh
+++ tur/python-scikit-image/build.sh
@@ -3,10 +3,11 @@ TERMUX_PKG_DESCRIPTION="Image processing in Python"
 TERMUX_PKG_LICENSE="BSD 2-Clause, BSD 3-Clause, MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="0.24.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/scikit-image/scikit-image/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=a2cf85577f8a9105ac46130277ad27e1627bfa3effecff7c1ef3ea851e5671ba
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, python, python-pip, python-numpy, python-pillow, python-pywavelets, python-scipy"
+TERMUX_PKG_DEPENDS="libandroid-complex-math, libc++, python, python-pip, python-numpy, python-pillow, python-pywavelets, python-scipy"
 TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, 'Cython>=3.0.4', meson-python, build"
 _NUMPY_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python-numpy/build.sh; echo $TERMUX_PKG_VERSION)
 TERMUX_PKG_PYTHON_BUILD_DEPS="pythran, 'numpy==$_NUMPY_VERSION'"
@@ -17,12 +18,30 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --cross-file $TERMUX_MESON_WHEEL_CROSSFILE
 "
 
+source $TERMUX_SCRIPTDIR/common-files/setup_toolchain_gcc.sh
+
 termux_step_pre_configure() {
 	if $TERMUX_ON_DEVICE_BUILD; then
 		termux_error_exit "Package '$TERMUX_PKG_NAME' is not available for on-device builds."
 	fi
 
+	# FIXME: NDK r27b cannot compile pythran due to https://github.com/llvm/llvm-project/issues/76358
+	_setup_toolchain_ndk_gcc_11
+
+	# ERROR: ./lib/python3.12/site-packages/skimage/measure/_marching_cubes_lewiner_cy.cpython-312.so contains undefined symbols:
+	#   139: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT  UND cpow
+	LDFLAGS+=" -landroid-complex-math"
 	LDFLAGS+=" -Wl,--no-as-needed -lpython${TERMUX_PYTHON_VERSION}"
+
+	# FIXME: Don't know why NDK's libc++ should link against clang's libunwind,
+	# FIXME: otherwise pybind11's `register_local_exception_translator` won't
+	# FIXME: work properly, causing crash on `scipy.io._mmio`.
+	mkdir -p $TERMUX_PKG_TMPDIR/_libunwind_libdir
+	local _NDK_ARCH=$TERMUX_ARCH
+	test $_NDK_ARCH == 'i686' && _NDK_ARCH='i386'
+	cp $NDK/toolchains/llvm/prebuilt/linux-x86_64/lib/clang/*/lib/linux/$_NDK_ARCH/libunwind.a \
+		$TERMUX_PKG_TMPDIR/_libunwind_libdir/libunwind.a
+	LDFLAGS="-L$TERMUX_PKG_TMPDIR/_libunwind_libdir -l:libunwind.a ${LDFLAGS}"
 }
 
 termux_step_configure() {
@@ -31,7 +50,7 @@ termux_step_configure() {
 	cp -f $TERMUX_MESON_CROSSFILE $TERMUX_MESON_WHEEL_CROSSFILE
 	sed -i 's|^\(\[binaries\]\)$|\1\npython = '\'$(command -v python)\''|g' \
 		$TERMUX_MESON_WHEEL_CROSSFILE
-	sed -i 's|^\(\[properties\]\)$|\1\nnumpy-include-dir = '\'$PYTHON_SITE_PKG/numpy/core/include\''|g' \
+	sed -i 's|^\(\[properties\]\)$|\1\nnumpy-include-dir = '\'$PYTHON_SITE_PKG/numpy/_core/include\''|g' \
 		$TERMUX_MESON_WHEEL_CROSSFILE
 
 	termux_step_configure_meson
