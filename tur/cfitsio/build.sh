25762b8f804046223d8e17f45ee4c71e41c6e574
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-10-07 16:46:25 +0800

    cfitsio: fix libdir

--- tur/cfitsio/build.sh
+++ tur/cfitsio/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="License.txt"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="4.5.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e4854fc3365c1462e493aa586bfaa2f3d0bb8c20b75a524955db64c27427ce09
 TERMUX_PKG_DEPENDS="curl, zlib"
