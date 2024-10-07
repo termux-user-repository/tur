59242bc3e775005dc6ccd9389f3d89775bfd5908
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-10-07 16:22:37 +0800

    astrometry.net: python 3.12 update

--- tur/astrometry.net/build.sh
+++ tur/astrometry.net/build.sh
@@ -4,11 +4,13 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="0.96"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/dstndstn/astrometry.net/releases/download/${TERMUX_PKG_VERSION}/astrometry.net-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fb3f2ec09cbe155d9ff461b9a60336f8493f5cb7804199e1782664e9034c9aac
 TERMUX_PKG_DEPENDS="cfitsio, curl, file, libandroid-glob, libbz2, libcairo, libjpeg-turbo, libpng, netpbm, python, python-numpy, python-fitsio, swig, wcslib, zlib"
 TERMUX_PKG_SUGGESTS="astrometry.net-data-basic"
-TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, setuptools, numpy"
+_NUMPY_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python-numpy/build.sh; echo $TERMUX_PKG_VERSION)
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, setuptools, 'numpy==$_NUMPY_VERSION'"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BLACKLISTED_ARCHES="arm" # FIXME: arm cross-compile C compiler fails for unknown reason
