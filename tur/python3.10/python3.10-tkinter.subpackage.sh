156301521b22038b6e8c7cf9ca353b25a680d4e0
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-10-09 17:09:05 +0800

    python3.10: bump to 3.10.15

--- tur/python3.10/python3.10-tkinter.subpackage.sh
+++ tur/python3.10/python3.10-tkinter.subpackage.sh
@@ -6,6 +6,6 @@ lib/python${_MAJOR_VERSION}/idlelib
 lib/python${_MAJOR_VERSION}/tkinter
 lib/python${_MAJOR_VERSION}/turtle.py
 lib/python${_MAJOR_VERSION}/turtledemo
-lib/python${_MAJOR_VERSION}/lib-dynload/_tkinter.cpython-${_MAJOR_VERSION/./}m.so
+lib/python${_MAJOR_VERSION}/lib-dynload/_tkinter.cpython-${_MAJOR_VERSION/./}.so
 lib/python${_MAJOR_VERSION}/__pycache__/turtle.*.pyc
 "
