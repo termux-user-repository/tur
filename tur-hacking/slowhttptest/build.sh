0bca6bdd8a9eea1b534bc8f9b24a72d7a16aefd2
1q23lyc45 <174172451+1q23lyc45@users.noreply.github.com>
2024-10-10 22:18:38 +0800

    addpkg(tur-hacking/slowhttptest): v1.9.0 (#1253)

---
+++ tur-hacking/slowhttptest/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/shekyan/slowhttptest
+TERMUX_PKG_DESCRIPTION="Application Layer DoS attack simulator"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="github@1q23lyc45"
+TERMUX_PKG_VERSION="1.9.0"
+TERMUX_PKG_SRCURL=https://github.com/shekyan/slowhttptest/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256="a3910b9b844e05ee55838aa17beddc6aa9d6c5c0012eab647a21cc9ccd6c8749"
+TERMUX_PKG_DEPENDS="openssl"
+TERMUX_PKG_BUILD_DEPENDS="openssl"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=false
