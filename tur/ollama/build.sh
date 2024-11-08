4d6cbd5182731ba1166f5c4dec449c93abb64fab
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-11-09 01:57:01 +0800

    ollama: bump to 0.4.0

--- tur/ollama/build.sh
+++ tur/ollama/build.sh
@@ -1,25 +1,21 @@
 TERMUX_PKG_HOMEPAGE=https://ollama.com/
-TERMUX_PKG_DESCRIPTION="Get up and running with large language models. "
+TERMUX_PKG_DESCRIPTION="Get up and running with large language models"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="0.3.14"
+TERMUX_PKG_VERSION="0.4.0"
 TERMUX_PKG_SRCURL=git+https://github.com/ollama/ollama
-TERMUX_PKG_DEPENDS="libopenblas"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
-termux_step_pre_configure() {
-	termux_setup_golang
-	termux_setup_cmake
-	LDFLAGS+=" -fopenmp -static-openmp"
-}
-
 termux_step_make() {
-	export OLLAMA_CUSTOM_CPU_DEFS="-DGGML_BLAS=ON -DGGML_BLAS_VENDOR=OpenBLAS -DGGML_NATIVE=OFF -DGGML_OPENMP=ON -DGGML_AVX=OFF -DGGML_AVX2=OFF -DGGML_AVX512=OFF -DGGML_FMA=OFF -DGGML_F16C=OFF"
+	termux_setup_golang
+
+	export ARCH="$GOARCH"
+	export VERSION="$TERMUX_PKG_VERSION"
 	go generate './...'
-	go build
+	go build -trimpath -ldflags="-w -s -X=github.com/ollama/ollama/version.Version=$TERMUX_PKG_VERSION -X=github.com/ollama/ollama/server.mode=release"
 }
 
 termux_step_make_install() {
