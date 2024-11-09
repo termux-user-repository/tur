35bc69a92ef48b912a5c32d6fc6442c777a194d2
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-11-09 18:03:24 +0800

    ffplayout: bump to 0.24.1

--- tur/ffplayout/build.sh
+++ tur/ffplayout/build.sh
@@ -2,41 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://ffplayout.github.io
 TERMUX_PKG_DESCRIPTION="Rust and ffmpeg based playout"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="0.23.2"
+TERMUX_PKG_VERSION="0.24.1"
 TERMUX_PKG_SRCURL=git+https://github.com/ffplayout/ffplayout
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 TERMUX_PKG_HOSTBUILD=true
-
-termux_step_get_source() {
-	local TMP_CHECKOUT=$TERMUX_PKG_CACHEDIR/tmp-checkout
-	local TMP_CHECKOUT_VERSION=$TERMUX_PKG_CACHEDIR/tmp-checkout-version
-
-	if [ ! -f $TMP_CHECKOUT_VERSION ] || [ "$(cat $TMP_CHECKOUT_VERSION)" != "$TERMUX_PKG_VERSION" ]; then
-		if [ "$TERMUX_PKG_GIT_BRANCH" == "" ]; then
-			TERMUX_PKG_GIT_BRANCH=v$TERMUX_PKG_VERSION
-		fi
-
-		rm -rf $TMP_CHECKOUT
-		git clone --depth 1 \
-			--branch $TERMUX_PKG_GIT_BRANCH \
-			${TERMUX_PKG_SRCURL:4} \
-			$TMP_CHECKOUT
-
-		# Set git submoudle url to https rather than ssh
-		sed -i 's|git@github.com:|https://github.com/|g' $TMP_CHECKOUT/.gitmodules
-
-		pushd $TMP_CHECKOUT
-		git submodule update --init --recursive --depth=1
-		popd
-
-		echo "$TERMUX_PKG_VERSION" > $TMP_CHECKOUT_VERSION
-	fi
-
-	rm -rf $TERMUX_PKG_SRCDIR
-	cp -Rf $TMP_CHECKOUT $TERMUX_PKG_SRCDIR
-}
 
 termux_step_post_get_source() {
 	# Remove this marker all the time
@@ -48,7 +19,7 @@ termux_step_host_build() {
 	pushd $TERMUX_PKG_SRCDIR
 	bash ./scripts/man_create.sh
 	rm -rf public
-	cd ffplayout-frontend
+	cd frontend
 	npm install
 	npm run generate
 	cp -r .output/public ../public
@@ -71,9 +42,8 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	cp ./target/$CARGO_TARGET_NAME/release/ffpapi .
 	cp ./target/$CARGO_TARGET_NAME/release/ffplayout .
-	tar -cvf "ffplayout-v${TERMUX_PKG_VERSION}_${CARGO_TARGET_NAME}.tar" --exclude='*.db' --exclude='*.db-shm' --exclude='*.db-wal' assets docker docs public LICENSE README.md CHANGELOG.md ffplayout ffpapi
+	tar -cvf "ffplayout-v${TERMUX_PKG_VERSION}_${CARGO_TARGET_NAME}.tar" --exclude='*.db' --exclude='*.db-shm' --exclude='*.db-wal' assets docker docs LICENSE README.md CHANGELOG.md ffplayout
 	mkdir -p $TERMUX_PREFIX/opt/ffplayout
 	tar -C $TERMUX_PREFIX/opt/ffplayout -xvf "ffplayout-v${TERMUX_PKG_VERSION}_${CARGO_TARGET_NAME}.tar"
 	ln -sfr $TERMUX_PREFIX/opt/ffplayout/ffpapi $TERMUX_PREFIX/bin
