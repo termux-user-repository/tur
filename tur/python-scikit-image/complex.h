af5836f4117793b968e0aabed2a79830f288ac66
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-11-10 13:51:33 +0800

    python-scikit-image: use ndk to compile

---
+++ tur/python-scikit-image/complex.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef __TERMUX_SCIKIT_IMAGE_COMPLEX__
+#define __TERMUX_SCIKIT_IMAGE_COMPLEX__
+
+#include_next <complex.h>
+
+double complex cpow(double complex __x, double complex __z);
+
+#endif // __TERMUX_SCIKIT_IMAGE_COMPLEX__
