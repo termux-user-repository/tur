045705d8f3749a00146ab37c13710c7a6a220428
Robert Kirkman <31490854+robertkirkman@users.noreply.github.com>
2024-11-24 09:19:11 -0600

    feat(tur/luanti): perform the migration from SDL2+GLX to libxi+EGL (#1327)

--- tur/luanti/build.sh
+++ tur/luanti/build.sh
@@ -3,12 +3,11 @@ TERMUX_PKG_DESCRIPTION="An open source voxel game engine."
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=1:5.10.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/minetest/minetest/archive/refs/tags/${TERMUX_PKG_VERSION:2}.zip
 TERMUX_PKG_SHA256=e74e994c0f1b188d60969477f553ad83b8ce20ee1e0e2dcd068120189cb0f56c
 TERMUX_PKG_AUTO_UPDATE=true
-# incomplete depends, i dont have time yet to write the perfect depends for this
-TERMUX_PKG_DEPENDS="sdl2, libc++, openal-soft, libvorbis, libsqlite, freetype, libpng, libcurl, libandroid-spawn"
+TERMUX_PKG_DEPENDS="freetype, libandroid-spawn, libc++, libcurl, libglvnd-dev, libjpeg-turbo, libiconv, libpng, libsqlite, libvorbis, libxi, openal-soft, zstd"
 
 termux_step_pre_configure() {
 	export LDFLAGS+=" -landroid-spawn"
