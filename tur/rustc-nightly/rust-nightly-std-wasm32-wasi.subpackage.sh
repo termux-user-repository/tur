ce6348ef7f7d8d61eb4243b5a4e460d8b364275d
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-11-26 23:36:41 +0800

    rustc-nightly: bump to 1.85.0-2024.11.25-nightly

--- tur/rustc-nightly/rust-nightly-std-wasm32-wasi.subpackage.sh
+++
@@ -1,6 +0,0 @@
-TERMUX_SUBPKG_DESCRIPTION="Rust std for target wasm32-wasi (nightly version)"
-TERMUX_SUBPKG_DEPEND_ON_PARENT=false
-TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
-TERMUX_SUBPKG_INCLUDE="
-opt/rust-nightly/lib/rustlib/wasm32-wasi
-"
