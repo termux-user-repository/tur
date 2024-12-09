cdcd260f4e56492114d8bc9faced70d98817975f
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-12-09 22:20:36 +0800

    electron-deps: revbump to rebuild

--- tur/electron-deps/build.sh
+++ tur/electron-deps/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A metapackage that contains dependencies for electron"
 TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=0.0.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_DEPENDS="atk, cups, dbus, fontconfig, freetype, gtk3, krb5, libc++, libevdev, libffi, libxkbcommon, libnss, libwayland, libx11, mesa, openssl, pango, pulseaudio"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
