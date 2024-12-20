91d0a65ce3a29fe590b2cc51ce2e1a7531042c3b
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-12-20 22:01:52 +0800

    vkmark: rebuild

--- tur/vkmark/build.sh
+++ tur/vkmark/build.sh
@@ -4,8 +4,8 @@ TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_LICENSE_FILE="COPYING-LGPL2.1"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 _COMMIT=ab6e6f34077722d5ae33f6bd40b18ef9c0e99a15
-TERMUX_PKG_VERSION="0.0.1-r139.${_COMMIT:0:7}"
-TERMUX_PKG_REVISION=1
+_COMMIT_DATE=2023.04.12
+TERMUX_PKG_VERSION="${_COMMIT_DATE}-r139.${_COMMIT:0:7}"
 TERMUX_PKG_SRCURL=git+https://github.com/vkmark/vkmark
 TERMUX_PKG_GIT_BRANCH="master"
 TERMUX_PKG_SHA256=2fde566a542fddc5c74a62c40dcb2d62e1151c7fbdc395adb1dc21857defa09d
@@ -15,8 +15,14 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dxcb=true"
 termux_step_post_get_source() {
 	git fetch --unshallow
 	git checkout $_COMMIT
+	local commit_date="$(git log -1 --format=%cs | sed 's/-/./g')"
+	if [ "$commit_date" != "$_COMMIT_DATE" ]; then
+		echo -n "ERROR: The specified commit date \"$_COMMIT_DATE\""
+		echo " is different from what is expected to be: \"$commit_date\""
+		return 1
+	fi
 
-	local version="$(printf "0.0.1-r%d.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")"
+	local version="$(printf "${_COMMIT_DATE}-r%d.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")"
 	if [ "$version" != "$TERMUX_PKG_VERSION" ]; then
 		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
 		echo " is different from what is expected to be: \"$version\""
