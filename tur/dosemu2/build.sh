688e86bde82b52795451f0867869f9cf5df9a731
Stas Sergeev <stsp@users.sourceforge.net>
2024-12-23 14:37:52 +0300

    new package: dosemu2
    Run DOS programs under linux.
    
    Closes https://github.com/termux-user-repository/tur/pull/1146
    Closes https://github.com/termux/termux-packages/issues/1605

---
+++ tur/dosemu2/build.sh
@@ -0,0 +1,24 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/dosemu2/dosemu2
+TERMUX_PKG_DESCRIPTION="Run DOS programs under linux."
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@stsp"
+TERMUX_PKG_VERSION="2.0pre9-git"
+TERMUX_PKG_SRCURL=git+https://github.com/dosemu2/dosemu2
+TERMUX_PKG_GIT_BRANCH=devel
+_COMMIT=58cd68c2bbbf1976bbb80d39c4ddd8edd2dd93ea
+TERMUX_PKG_SHA256=e2fcd6e79091442f22fc61f24cac3c9e21dbe47df31ca3ae3f65b8dc457c3c72
+TERMUX_PKG_BUILD_DEPENDS="libandroid-posix-semaphore, libandroid-glob, slang, libao, fluidsynth, ladspa-sdk, libslirp, libbsd, readline, json-c, libseccomp, libsearpc, fdpp, dj64dev"
+TERMUX_PKG_DEPENDS="fdpp, dj64dev, comcom64"
+
+termux_step_post_get_source() {
+	git checkout $_COMMIT
+	local s=$(git archive --format=tar HEAD | sha256sum -b)
+	if [[ "${s}" != "${TERMUX_PKG_SHA256} "* ]]; then
+		termux_error_exit "Checksum mismatch for source files."
+	fi
+}
+
+termux_step_pre_configure() {
+	cd $TERMUX_PKG_SRCDIR
+	./autogen.sh
+}
