79484b468aa4998f4fa55e5f20084e8bb4bc6b9d
Stas Sergeev <stsp@users.sourceforge.net>
2024-12-26 01:01:02 +0300

    new package: instfd
    FreeDOS installer for dosemu2.

---
+++ tur/instfd/build.sh
@@ -0,0 +1,14 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/dosemu2/install-freedos
+TERMUX_PKG_DESCRIPTION="FreeDOS installed for dosemu2"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@stsp"
+TERMUX_PKG_VERSION=0.3
+TERMUX_PKG_SRCURL=https://github.com/dosemu2/install-freedos/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=aeade1ec86902df35e0926f802319b625de8fafac33fe834a286b3fe971a086f
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="python"
+TERMUX_PKG_PYTHON_TARGET_DEPS="tqdm"
+
+termux_step_make_install() {
+	make -C $TERMUX_PKG_SRCDIR install
+}
