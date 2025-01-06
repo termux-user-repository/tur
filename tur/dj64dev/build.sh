7faee1d1be8c28b4dae97111bfecb556858c2712
Stas Sergeev <stsp@users.sourceforge.net>
2025-01-07 00:55:48 +0300

    dj64dev: add missing dep on libelf
    It appears build-deps are not automatically populate to deps.

--- tur/dj64dev/build.sh
+++ tur/dj64dev/build.sh
@@ -3,12 +3,12 @@ TERMUX_PKG_DESCRIPTION="development suite that allows to cross-build 64-bit prog
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@stsp"
 TERMUX_PKG_VERSION=0.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/stsp/dj64dev/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d6cd05e92b1f46f20190a4a0fb8dc5a37f0f4dd29380a35651cb974b6501a975
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_DEPENDS="ctags-cross, thunk-gen-cross, libelf"
-TERMUX_PKG_DEPENDS="djstub, thunk-gen"
+TERMUX_PKG_DEPENDS="djstub, thunk-gen, libelf"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
