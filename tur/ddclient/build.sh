363aaa1caf63d50928e565ae07bd39ef67002e5a
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-01-21 00:11:45 +0800

    ddclient: bump to 4.0.0 (#1451)

--- tur/ddclient/build.sh
+++ tur/ddclient/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/ddclient/ddclient
 TERMUX_PKG_DESCRIPTION="Update dynamic DNS entries for accounts on many dynamic DNS services"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="3.11.2"
+TERMUX_PKG_VERSION="4.0.0"
 TERMUX_PKG_SRCURL=https://github.com/ddclient/ddclient/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=243cd832abd3cdd2b49903e1b5ed7f450e2d9c4c0eaf8ce4fe692c244d3afd77
-TERMUX_PKG_DEPENDS="perl"
+TERMUX_PKG_SHA256=4b37c99ac0011102d7db62f1ece7ff899b06df3d4b172e312703931a3c593c93
+TERMUX_PKG_DEPENDS="curl, perl"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_AUTO_UPDATE=true
@@ -14,6 +14,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --prefix=$TERMUX_PREFIX
 --sysconfdir=$TERMUX_PREFIX/etc/ddclient
 --localstatedir=$TERMUX_PREFIX/var
+--with-curl=$TERMUX_PREFIX/bin/curl
 "
 
 termux_step_pre_configure() {
