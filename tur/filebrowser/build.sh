3892b7da1a4f6ebd2e94e0eb130290a07bff5982
Chongyun Lee <licy183@termux.dev>
2025-02-03 21:46:44 +0800

    filebrowser: bump to 2.32.0

--- tur/filebrowser/build.sh
+++ tur/filebrowser/build.sh
@@ -2,21 +2,41 @@ TERMUX_PKG_HOMEPAGE=https://filebrowser.org/
 TERMUX_PKG_DESCRIPTION="Web file browser based on Go"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="2.31.2"
+TERMUX_PKG_VERSION="2.32.0"
 TERMUX_PKG_SRCURL="https://github.com/filebrowser/filebrowser/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=bfda9ea7c44d4cb93c47a007c98b84f853874e043049b44eff11ca00157d8426
+TERMUX_PKG_SHA256=61e9de6b2d396614f45be477e5bb5aad189e7bb1155a3f88800e02421bd6cc2b
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_QUIET_BUILD=false
+TERMUX_PKG_HOSTBUILD=true
 
-termux_step_pre_configure() {
+termux_step_host_build() {
+	if [ -e "$TERMUX_PREFIX/bin" ]; then
+		rm -rf $TERMUX_PREFIX/bin.bp
+		mv -f $TERMUX_PREFIX/bin $TERMUX_PREFIX/bin.bp
+	fi
+	termux_setup_nodejs
+	npm install pnpm
+	export PATH="$TERMUX_PKG_HOSTBUILD_DIR/node_modules/.bin:$PATH"
+	if [ -e "$TERMUX_PREFIX/bin.bp" ]; then
+		rm -rf $TERMUX_PREFIX/bin
+		mv -f $TERMUX_PREFIX/bin.bp $TERMUX_PREFIX/bin
+	fi
+}
+
+termux_step_configure() {
 	termux_setup_golang
 	termux_setup_nodejs
+	export PATH="$TERMUX_PKG_HOSTBUILD_DIR/node_modules/.bin:$PATH"
 }
 
 termux_step_make() {
+	local _commit_hash=$(
+		curl https://api.github.com/repos/filebrowser/filebrowser/git/ref/tags/v$TERMUX_PKG_VERSION \
+		| jq -r .object.sha
+	)
+	local _module_name=$(env GO111MODULE=on go list -m)
 	make build-frontend
-	go build
+	go build -ldflags="-X $_module_name/version.Version=$TERMUX_PKG_VERSION -X $_module_name/version.CommitSHA=$_commit_hash"
 }
 
 termux_step_make_install() {
