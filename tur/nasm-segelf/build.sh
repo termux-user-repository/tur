079a38320cb1400ed3f2d2a68d744271385e2753
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-02-04 09:04:55 +0800

    nasm-segelf: fix auto update
    Closes #1425
    
    [skip ci]

--- tur/nasm-segelf/build.sh
+++ tur/nasm-segelf/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION="2.16.01-3"
 TERMUX_PKG_SRCURL=https://github.com/stsp/nasm-segelf/archive/refs/tags/nasm-segelf-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=87564b98746a5d291001130d7d444d221aecacc22016284131e97d0ac7febf6b
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_SED_REGEXP="s/nasm-segelf-//g"
 TERMUX_PKG_HOSTBUILD=true
 
 termux_step_host_build() {
