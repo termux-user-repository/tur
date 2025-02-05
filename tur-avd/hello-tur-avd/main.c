c4d183b2912a9523739e406771d2e2a505840f26
Chongyun Lee <licy183@termux.dev>
2025-02-06 00:50:37 +0800

    test waydroid

--- tur-avd/hello-tur-avd/main.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-int main() {
-    puts("Hello TUR AVD.");
-}
--- tur-waydroid/hello-tur-waydroid/build.sh
+++ tur-waydroid/hello-tur-waydroid/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux-user-repository/tur
-TERMUX_PKG_DESCRIPTION="Dummy test for TUR AVD"
+TERMUX_PKG_DESCRIPTION="Dummy test for TUR Waydroid"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=0.1
@@ -7,9 +7,9 @@ TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
-	$CC $CFLAGS $CPPFLAGS $TERMUX_PKG_BUILDER_DIR/main.c -o hello-tur-avd
+	$CC $CFLAGS $CPPFLAGS $TERMUX_PKG_BUILDER_DIR/main.c -o hello-tur-waydroid
 }
 
 termux_step_make_install() {
-	install -Dm700 hello-tur-avd $TERMUX_PREFIX/bin/hello-tur-avd
+	install -Dm700 hello-tur-waydroid $TERMUX_PREFIX/bin/hello-tur-waydroid
 }
