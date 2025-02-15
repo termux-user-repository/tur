05d3f0c3880414f72d495588f3739b5d49753f38
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-02-15 23:07:49 +0800

    hangover-wine: move to main
    [skip ci]

--- tur/hangover-wine/hangover-libwow64fex.subpackage.sh
+++
@@ -1,6 +0,0 @@
-TERMUX_SUBPKG_DESCRIPTION="FEX based x86 emulator library for Hangover"
-TERMUX_SUBPKG_DEPEND_ON_PARENT=false
-TERMUX_SUBPKG_INCLUDE="
-opt/hangover-wine/lib/wine/aarch64-windows/libwow64fex.dll
-share/doc/hangover-libwow64fex/copyright
-"
