7a09833ad733ad746a6592d4e34f89162e512648
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-02-19 22:14:39 +0800

    code-server: bump to 4.97.2

--- tur/code-server/build.sh
+++ tur/code-server/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/coder/code-server
 TERMUX_PKG_DESCRIPTION="Run VS Code on any machine anywhere and access it in the browser"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="4.96.4"
+TERMUX_PKG_VERSION="4.97.2"
 TERMUX_PKG_SRCURL=git+https://github.com/coder/code-server
 TERMUX_PKG_DEPENDS="libandroid-spawn, libsecret, krb5, nodejs-20, ripgrep"
 TERMUX_PKG_ANTI_DEPENDS="nodejs-20"
@@ -30,7 +30,7 @@ termux_step_post_get_source() {
 }
 
 _setup_nodejs_20() {
-	local NODEJS_VERSION=20.18.0
+	local NODEJS_VERSION=20.18.1
 	local NODEJS_FOLDER=${TERMUX_PKG_CACHEDIR}/build-tools/nodejs-${NODEJS_VERSION}
 
 	if [ ! -x "$NODEJS_FOLDER/bin/node" ]; then
@@ -38,7 +38,7 @@ _setup_nodejs_20() {
 		local NODEJS_TAR_FILE=$TERMUX_PKG_TMPDIR/nodejs-$NODEJS_VERSION.tar.xz
 		termux_download https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-x64.tar.xz \
 			"$NODEJS_TAR_FILE" \
-			4543670b589593f8fa5f106111fd5139081da42bb165a9239f05195e405f240a
+			c6fa75c841cbffac851678a472f2a5bd612fff8308ef39236190e1f8dbb0e567
 		tar -xf "$NODEJS_TAR_FILE" -C "$NODEJS_FOLDER" --strip-components=1
 	fi
 	export PATH="$NODEJS_FOLDER/bin:$PATH"
