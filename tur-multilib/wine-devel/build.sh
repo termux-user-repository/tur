8e03a02127250a672b602559aedd061bafd98676
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-03-04 00:12:47 +0800

    wine-devel: build wine-tools

--- tur-multilib/wine-devel/build.sh
+++ tur-multilib/wine-devel/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_LICENSE_FILE="LICENSE, LICENSE.OLD, COPYING.LIB"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="10.2"
+TERMUX_PKG_REVISION=1
 _REAL_VERSION="${TERMUX_PKG_VERSION/\~/-}"
 _VERSION_FOLDER="$(test "${_REAL_VERSION:3:1}" = 0 && echo ${_REAL_VERSION:0:4} || echo ${_REAL_VERSION:0:3}x)"
 TERMUX_PKG_SRCURL=https://dl.winehq.org/wine/source/${_VERSION_FOLDER}/wine-$_REAL_VERSION.tar.xz
@@ -22,6 +23,7 @@ TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS="
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 enable_wineandroid_drv=no
+enable_tools=yes
 --prefix=$TERMUX_PREFIX/opt/wine-devel
 --exec-prefix=$TERMUX_PREFIX/opt/wine-devel
 --libdir=$TERMUX_PREFIX/opt/wine-devel/lib
