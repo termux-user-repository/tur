ab9af2096dc6cae505f7215da7e93e2e77a9af2c
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-03-08 01:50:56 +0800

    fix build

--- tur/payload-dumper-go/build.sh
+++ tur/payload-dumper-go/build.sh
@@ -6,10 +6,28 @@ TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_SRCURL=https://github.com/ssut/payload-dumper-go/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d7ba33a80c539674c0b63443b8c6dd9c2040ec996323f38ffe72e024d302eb2d
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_BUILD_DEPENDS="xz-utils"
-TERMUX_PKG_DEPENDS="xz-utils"
+TERMUX_PKG_DEPENDS="liblzma"
+
+__prepare_zstd() {
+	pushd "$TERMUX_PKG_SRCDIR"
+	cd vendor/github.com/valyala/gozstd
+	make update-zstd-source
+	cd zstd/lib
+	ZSTD_LEGACY_SUPPORT=0 MOREFLAGS=-fPIC make clean libzstd.a -j $TERMUX_PKG_MAKE_PROCESSES
+	cd ../..
+	mv zstd/lib/libzstd.a libzstd_linux_$GOARCH.a
+	popd
+}
+
+termux_step_post_get_source() {
+	termux_setup_golang
+
+	go mod vendor
+}
 
 termux_step_make() {
+	__prepare_zstd
+
 	termux_setup_golang
 	go build -o payload-dumper-go
 }
