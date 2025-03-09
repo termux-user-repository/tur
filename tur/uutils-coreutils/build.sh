b76f9ddfeced57cdc0b8188b957909ddc8360135
Chongyun Lee <licy183@termux.dev>
2025-03-09 14:38:33 +0800

    uutils-coreutils: bump to 0.0.30

--- tur/uutils-coreutils/build.sh
+++ tur/uutils-coreutils/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://uutils.github.io/
 TERMUX_PKG_DESCRIPTION="Cross-platform Rust rewrite of the GNU coreutils"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="0.0.29"
+TERMUX_PKG_VERSION="0.0.30"
 TERMUX_PKG_SRCURL=https://github.com/uutils/coreutils/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=e1904ed6e5b8b441bedcba5afa7e8e8c744ef701f6d392de8c8dcc2ea17a34e2
+TERMUX_PKG_SHA256=732c0ac646be7cc59a51cdfdb2d0ff1a4d2501c28f900a2d447c77729fdfca22
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
@@ -16,9 +16,13 @@ termux_step_make() {
 	export $_renv="$CXXFLAGS"
 	unset CXXFLAGS
 
-	make PROFILE=release \
-		CARGO_TARGET_NAME="$CARGO_TARGET_NAME" \
+	make \
+		PROFILE=release \
+		MULTICALL=y \
+		CARGOFLAGS="--target $CARGO_TARGET_NAME" \
 		SELINUX_ENABLED=0 \
+		MANPAGES=0 \
+		COMPLETIONS=0 \
 		SKIP_UTILS="pinky uptime users who hostid"
 }
 
@@ -29,7 +33,10 @@ termux_step_make_install() {
 		PROG_PREFIX=uu- \
 		PROFILE=release \
 		MULTICALL=y \
-		CARGO_TARGET_NAME="$CARGO_TARGET_NAME" \
+		CARGOFLAGS="--target $CARGO_TARGET_NAME" \
+		CARGO_TARGET_DIR="$(pwd)/target/$CARGO_TARGET_NAME" \
 		SELINUX_ENABLED=0 \
+		MANPAGES=0 \
+		COMPLETIONS=0 \
 		SKIP_UTILS="pinky uptime users who hostid"
 }
