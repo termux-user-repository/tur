788ee4fa0a8d6a27a7e4dbc1235b85a91674d6ea
fervi <fervi@Renzan.intinte.org>
2025-03-13 17:54:16 +0100

    Newpkg: Supertux

--- tur/supertux/build.sh
+++ tur/supertux/build.sh
@@ -7,19 +7,18 @@ TERMUX_PKG_SRCURL="https://github.com/SuperTux/supertux/releases/download/v${TER
 TERMUX_PKG_SHA256=f7940e6009c40226eb34ebab8ffb0e3a894892d891a07b35d0e5762dd41c79f6
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_BUILD_DEPENDS="boost-headers"
-TERMUX_PKG_DEPENDS="boost, glm, sdl2, sdl2-image, sdl2-ttf, glew, openal-soft, libphysfs, supertux2-data"
+TERMUX_PKG_DEPENDS="boost, glm, sdl2, sdl2-image, sdl2-ttf, glew, openal-soft, libphysfs, supertux-data, freetype, libandroid-execinfo"
+TERMUX_PKG_FORCE_CMAKE=true
+TERMUX_ON_DEVICE_BUILD=false
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DBoost_INCLUDE_DIR=$TERMUX_PREFIX/include
 -DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX
 -DIS_SUPERTUX_RELEASE=true
+-DINSTALL_SUBDIR_BIN=bin
 "
 
 termux_step_pre_configure() {
-	termux_setup_cmake
-}
-
-termux_step_configure() {
-	export CMAKE_PREFIX_PATH=/data/data/com.termux/files/usr
-	cmake . $TERMUX_PKG_EXTRA_CONFIGURE_ARGS
+    export LDFLAGS+=" -Wl,--no-as-needed,-lOpenSLES,--as-needed"
+    export CMAKE_PREFIX_PATH=$TERMUX_PREFIX
 }
