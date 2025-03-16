36dd6340a24ee7e14a3a33329ce5e84dadea0a6c
lunsokhasovan <lunsokhasovan@proton.me>
2025-03-16 18:36:31 +0700

    Update build.sh (replace quad spaces to tabs)

--- tur/itstool/build.sh
+++ tur/itstool/build.sh
@@ -8,14 +8,14 @@ TERMUX_PKG_SHA256=6b9a7cd29a12bb95598f5750e8763cee78836a1a207f85b74d8b3275b27e87
 TERMUX_PKG_DEPENDS="libxml2-python"
 
 termux_step_pre_configure(){
-    cp -r $TERMUX_PKG_SRCDIR/its $TERMUX_PKG_BUILDDIR/its
-    autoreconf -vfi
+	cp -r $TERMUX_PKG_SRCDIR/its $TERMUX_PKG_BUILDDIR/its
+	autoreconf -vfi
 }
 
 termux_step_make(){
-    return
+	return
 }
 
 termux_step_make_install(){
-    return
+	return
 }
