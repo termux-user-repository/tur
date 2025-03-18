8278b6c617040d5b75a9195380341d43df870b94
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-03-18 21:34:39 +0800

    fix build

--- tur/itstool/build.sh
+++ tur/itstool/build.sh
@@ -6,9 +6,9 @@ TERMUX_PKG_VERSION=2.0.7
 TERMUX_PKG_SRCURL=http://files.itstool.org/itstool/itstool-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=6b9a7cd29a12bb95598f5750e8763cee78836a1a207f85b74d8b3275b27e87ca
 TERMUX_PKG_DEPENDS="libxml2-python"
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure(){
-	cp -r $TERMUX_PKG_SRCDIR/its $TERMUX_PKG_BUILDDIR/its
 	autoreconf -vf
 }
 
