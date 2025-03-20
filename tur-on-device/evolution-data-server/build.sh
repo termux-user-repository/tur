494ffe37aafec69263a44da60c6d5891c173081f
Sovan <lunsokhasovan@proton.me>
2025-03-20 13:24:13 +0700

    Update build.sh

--- tur-on-device/evolution-data-server/build.sh
+++ tur-on-device/evolution-data-server/build.sh
@@ -5,20 +5,18 @@ TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=3.56.0
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/evolution-data-server/${TERMUX_PKG_VERSION%.*}/evolution-data-server-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=7ae5482aa4ee2894467716c5be982500e1d511dddf4ab29b68fdb107d7f8a8ff
-TERMUX_PKG_DEPENDS="libcairo, glib, gtk4, json-glib, krb5, libcanberra, libdb, libical, libsoup3, libuuid, libxml2, libnspr, libnss, openldap, libsecret, libsqlite, pango, webkit2gtk-4.1, webkitgtk-6.0"
+TERMUX_PKG_DEPENDS="libcairo, glib, gtk4, gperf, json-glib, krb5, libcanberra, libdb, libical, libsoup3, libuuid, libxml2, libnspr, libnss, openldap, libsecret, libsqlite, pango, webkit2gtk-4.1, webkitgtk-6.0"
 TERMUX_PKG_BUILD_DEPENDS="intltool, python, valac"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX
 -DENABLE_OAUTH2=OFF
--DENABLE_GTK_DOC=OFF
 -DENABLE_WEATHER=OFF
 -DENABLE_GOA=OFF
 -DENABLE_UOA=OFF
 "
 termux_step_pre_configure(){
-	sed 's/libsecret-1/libsecret/g' CMakeLists.txt
 	sed -i 's/libical-glib/libical/g' CMakeLists.txt
 }
 
