cef3d2871bf8e7f80298628ab7a5dd2129e50f24
Sovan <lunsokhasovan@proton.me>
2025-03-24 16:22:09 +0700

    new package: qqwing (#1621)

---
+++ tur/qqwing/build.sh
@@ -0,0 +1,24 @@
+TERMUX_PKG_HOMEPAGE="https://qqwing.com/"
+TERMUX_PKG_DESCRIPTION="QQwing is software for generating and solving Sudoku puzzles"
+TERMUX_PKG_LICENSE="GPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION=1.3.4
+TERMUX_PKG_SRCURL=https://qqwing.com/qqwing-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=1753736c31feea0085f5cfac33143743204f8a7e66b81ccd17e249ecafba802f
+TERMUX_PKG_DEPENDS="glib"
+# Pkg doesn't support arm?
+TERMUX_PKG_BLACKLISTED_ARCHES="arm"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+
+termux_step_pre_configure(){
+	autoreconf -vf
+}
+
+termux_step_make(){
+	make -j $TERMUX_PKG_MAKE_PROCESSES
+}
+
+termux_step_make_install(){
+	make install
+}
