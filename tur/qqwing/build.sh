1683fabe86494c266af592644422c928c20b7eb0
lunsokhasovan <lunsokhasovan@proton.me>
2025-03-24 12:12:35 +0700

    Update

--- tur/qqwing/build.sh
+++ tur/qqwing/build.sh
@@ -5,6 +5,8 @@ TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=1.3.7
 TERMUX_PKG_SRCURL=https://qqwing.com/qqwing-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="glib"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_pre_configure(){
 	autoreconf -vf
