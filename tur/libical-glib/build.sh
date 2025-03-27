e662d615e0a5aeca14d1ed3d3776bdebf121b3af
Sovan <lunsokhasovan@proton.me>
2025-03-27 17:27:47 +0700

    Recode

--- tur/libical-glib/build.sh
+++ tur/libical-glib/build.sh
@@ -7,17 +7,5 @@ TERMUX_PKG_SRCURL=https://github.com/libical/libical/releases/download/v$TERMUX_
 TERMUX_PKG_SHA256=e73de92f5a6ce84c1b00306446b290a2b08cdf0a80988eca0a2c9d5c3510b4c2
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, libical, libxml2"
-TERMUX_PKG_BUILD_DEPENDS="perl"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" -DSHARED_ONLY=true -DICAL_GLIB=true -DUSE_BUILTIN_TZDATA=true"
-
-termux_step_post_get_source() {
-	# Do not forget to bump revision of reverse dependencies and rebuild them
-	# after SOVERSION is changed.
-	local _SOVERSION=3
-
-	local v=$(sed -En 's/^set\(LIBICAL_LIB_MAJOR_VERSION\s+"?([0-9]+).*/\1/p' \
-			CMakeLists.txt)
-	if [ "${v}" != "${_SOVERSION}" ]; then
-		termux_error_exit "SOVERSION guard check failed."
-	fi
-}
+TERMUX_PKG_BUILD_DEPENDS="icu-devtools, gtk-doc"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" -DICAL_GLIB=true -DUSE_BUILTIN_TZDATA=true"
