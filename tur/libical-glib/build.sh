ef0072282b3ffb4d3b3e17cfd213c8314a70fe81
Sovan <lunsokhasovan@proton.me>
2025-03-27 13:07:03 +0700

    Add depend

--- tur/libical-glib/build.sh
+++ tur/libical-glib/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION="3.0.20"
 TERMUX_PKG_SRCURL=https://github.com/libical/libical/releases/download/v$TERMUX_PKG_VERSION/libical-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=e73de92f5a6ce84c1b00306446b290a2b08cdf0a80988eca0a2c9d5c3510b4c2
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libical, libxml2"
+TERMUX_PKG_DEPENDS="glib, libical, libxml2"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" -DSHARED_ONLY=true -DICAL_GLIB=true -DUSE_BUILTIN_TZDATA=true -DPERL_EXECUTABLE=/usr/bin/perl"
 
 termux_step_post_get_source() {
