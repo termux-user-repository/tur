38ceb9f891602c94c74ead4963c752f21e8e694a
Chongyun Lee <licy183@termux.dev>
2025-03-28 23:12:39 +0800

    blueprint-compiler: depends on `pygobject`

--- tur/blueprint-compiler/build.sh
+++ tur/blueprint-compiler/build.sh
@@ -3,9 +3,11 @@ TERMUX_PKG_DESCRIPTION="A markup language for GTK user interface files"
 TERMUX_PKG_LICENSE="GPL-3.0-or-later"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=0.16.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/jwestman/blueprint-compiler/-/archive/v${TERMUX_PKG_VERSION}/blueprint-compiler-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=01feb8263fe7a450b0a9fed0fd54cf88947aaf00f86cc7da345f8b39a0e7bd30
-TERMUX_PKG_DEPENDS="python"
+TERMUX_PKG_DEPENDS="python, pygobject"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="pygobject"
 TERMUX_PKG_SETUP_PYTHON=true
 TERMUX_PKG_AUTO_UPDATE=true
 
