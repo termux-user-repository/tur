292352a9748487fbadfc8f0b063c74f406b291ec
Chongyun Lee <licy183@termux.dev>
2025-03-29 00:16:01 +0800

    qqwing: fix build

--- tur/qqwing/build.sh
+++ tur/qqwing/build.sh
@@ -3,16 +3,21 @@ TERMUX_PKG_DESCRIPTION="QQwing is software for generating and solving Sudoku puz
 TERMUX_PKG_LICENSE="GPL-2.0-or-later"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=1.3.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://qqwing.com/qqwing-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1753736c31feea0085f5cfac33143743204f8a7e66b81ccd17e249ecafba802f
 TERMUX_PKG_DEPENDS="glib"
-# Pkg doesn't support arm?
-TERMUX_PKG_BLACKLISTED_ARCHES="arm"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_pre_configure(){
 	autoreconf -vf
+
+	if [ "$TERMUX_ARCH" = arm ]; then
+		local _libgcc_file="$($CC -print-libgcc-file-name)"
+		local _libgcc_path="$(dirname $_libgcc_file)"
+		local _libgcc_name="$(basename $_libgcc_file)"
+		LDFLAGS+=" -L$_libgcc_path -l:$_libgcc_name"
+	fi
 }
 
 termux_step_make(){
