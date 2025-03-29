97390ea441ae864c7596b7b3413c9072e422dd35
Sovan <lunsokhasovan@proton.me>
2025-03-29 09:08:33 +0700

    Add depend for build

--- tur-on-device/libical-glib/build.sh
+++ tur-on-device/libical-glib/build.sh
@@ -7,6 +7,6 @@ TERMUX_PKG_SRCURL=https://github.com/libical/libical/releases/download/v$TERMUX_
 TERMUX_PKG_SHA256=e73de92f5a6ce84c1b00306446b290a2b08cdf0a80988eca0a2c9d5c3510b4c2
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, libical, libxml2"
-TERMUX_PKG_BUILD_DEPENDS="icu-devtools, gtk-doc"
+TERMUX_PKG_BUILD_DEPENDS="icu-devtools, gtk-doc, binutils"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DICAL_GLIB=true -DUSE_BUILTIN_TZDATA=true"
 TERMUX_CMAKE_BUILD="Unix Makefiles"
