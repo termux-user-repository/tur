21d4c16bfbceaab25c279af6176537be4819304c
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-04-10 22:00:30 +0800

    revbump: boost update

--- tur/libcmis/build.sh
+++ tur/libcmis/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="a C/C++ client library for the CMIS protocol"
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.1, MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=0.6.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/tdf/libcmis/releases/download/v${TERMUX_PKG_VERSION}/libcmis-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=1b5c2d7258ff93eb5f9958ff0e4dfd7332dc75a071bb717dde2217a26602a644
 TERMUX_PKG_DEPENDS="boost, curl, libxml2"
--- tur/libfreehand/build.sh
+++ tur/libfreehand/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="a library for import of Aldus/Macromedia/Adobe FreeHand 
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=0.1.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://dev-www.libreoffice.org/src/libfreehand/libfreehand-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=0e422d1564a6dbf22a9af598535425271e583514c0f7ba7d9091676420de34ac
 TERMUX_PKG_DEPENDS="boost, littlecms, libicu, librevenge, zlib"
