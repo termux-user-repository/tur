633563babf3606b66ef562c0885f89e873be0e00
Chongyun Lee <licy183@termux.dev>
2025-04-12 01:25:30 +0800

    switch source

--- tur/3dstool/build.sh
+++ tur/3dstool/build.sh
@@ -3,11 +3,16 @@ TERMUX_PKG_DESCRIPTION="An all-in-one tool for extracting/creating 3ds roms"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="1.2.6"
-TERMUX_PKG_SRCURL=git+https://github.com/john-peterson/3dstool/
-TERMUX_PKG_GIT_BRANCH=termux
-TERMUX_PKG_EXCLUDED_ARCHES="arm" # Some dependencies aren't available for arm
-TERMUX_PKG_DEPENDS=libiconv,openssl,libcurl,capstone
+TERMUX_PKG_SRCURL=https://github.com/dnasdw/3dstool/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256="7f6118bfe7b8e1ba87aa547a8cb892c29c9cc45ad817ee822121fa2142044859"
+TERMUX_PKG_DEPENDS="capstone, libc++, libcurl, libiconv, openssl"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_get_source() {
+	# Remove vendored source
+	rm -rf dep
+}
 
 termux_step_post_make_install() {
-	install -Dm755 ../src/bin/Release/3dstool $TERMUX_PREFIX/bin/3dstool
+	install -Dm755 bin/Release/3dstool $TERMUX_PREFIX/bin/3dstool
 }
