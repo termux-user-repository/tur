36e0d209a8eaaa8ff122cca02c7eb51674788bf6
fervi <fervi@Shinzan>
2025-04-12 22:39:50 +0200

    Added plib patches from Debian, fix simgear build i686

--- tur/plib/build.sh
+++ tur/plib/build.sh
@@ -5,7 +5,6 @@ TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=1.8.5
 TERMUX_PKG_SRCURL=http://deb.debian.org/debian/pool/main/p/plib/plib_${TERMUX_PKG_VERSION}.orig.tar.gz
 TERMUX_PKG_SHA256=485b22bf6fdc0da067e34ead5e26f002b76326f6371e2ae006415dea6a380a32
-TERMUX_PKG_DEPENDS="mesa"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_lib_MesaGL_glNewList=yes
