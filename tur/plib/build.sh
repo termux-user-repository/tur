dc74297a1acb4fd26ca3e3088177d26cc866326c
fervi <fervi@Shinzan>
2025-04-12 14:24:00 +0200

    Try to fix build on TUR

--- tur/plib/build.sh
+++ tur/plib/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=1.8.5
 TERMUX_PKG_SRCURL=http://deb.debian.org/debian/pool/main/p/plib/plib_${TERMUX_PKG_VERSION}.orig.tar.gz
 TERMUX_PKG_SHA256=485b22bf6fdc0da067e34ead5e26f002b76326f6371e2ae006415dea6a380a32
+TERMUX_PKG_DEPENDS="mesa"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
