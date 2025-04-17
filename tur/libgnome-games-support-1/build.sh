09703718bede834bb4823e2442b49bac9eb07ad4
lunsokhasovan <lunsokhasovan@proton.me>
2025-04-17 15:57:05 +0700

    Pick a wrong sha256

--- tur/libgnome-games-support-1/build.sh
+++ tur/libgnome-games-support-1/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@lunsokhasovan, @termux-user-repository"
 TERMUX_PKG_VERSION=1.8.2
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libgnome-games-support/${TERMUX_PKG_VERSION%.*}/libgnome-games-support-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=cb6c4859d16bffc941b1098f7f624c84e6a3339fce45629e45ba8b3f653d58f9
+TERMUX_PKG_SHA256=28434604a7b038731ac0231731388ff104f565bb2330cc24e78cda04cfd3ef7d
 TERMUX_PKG_DEPENDS="glib, gtk4, libgee"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, gettext"
 
