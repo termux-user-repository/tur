06a79b0cfbe0deb2e9c43dad22a7ddfc751dfc83
lunsokhasovan <lunsokhasovan@proton.me>
2025-04-17 15:46:05 +0700

    Add setup gir?

--- tur/libgnome-games-support-1/build.sh
+++ tur/libgnome-games-support-1/build.sh
@@ -6,4 +6,8 @@ TERMUX_PKG_VERSION=1.8.2
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libgnome-games-support/${TERMUX_PKG_VERSION%.*}/libgnome-games-support-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=cb6c4859d16bffc941b1098f7f624c84e6a3339fce45629e45ba8b3f653d58f9
 TERMUX_PKG_DEPENDS="glib, gtk4, libgee"
-TERMUX_PKG_BUILD_DEPENDS="gettext"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, gettext"
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
--- tur/libgnome-games-support/build.sh
+++ tur/libgnome-games-support/build.sh
@@ -6,5 +6,9 @@ TERMUX_PKG_VERSION=2.0.1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libgnome-games-support/${TERMUX_PKG_VERSION%.*}/libgnome-games-support-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=0186f25c4892c86c7eac43a307fc19db696df4f19aca7f54e83c221df9d9790a
 TERMUX_PKG_DEPENDS="glib, gtk4, libgee"
-TERMUX_PKG_BUILD_DEPENDS="gettext"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, gettext"
 TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
