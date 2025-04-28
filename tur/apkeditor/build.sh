8442eb0307bdc280024aea63f55aa3fb56697a6a
Veha Veha <vehav00@gmail.com>
2025-04-28 12:56:59 +0700

    apkeditor: a wrapper

--- tur/apkeditor/build.sh
+++ tur/apkeditor/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Android binary resource files editor"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@Veha0001"
 TERMUX_PKG_VERSION=1.4.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/REAndroid/APKEditor/releases/download/V${TERMUX_PKG_VERSION}/APKEditor-${TERMUX_PKG_VERSION}.jar
 TERMUX_PKG_SHA256=758f2f9153fff96c20260b177f025a3ca3cecc9777abdd43139a17e225724612
 TERMUX_PKG_DEPENDS="openjdk-21"
@@ -17,13 +17,12 @@ TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 termux_step_make_install() {
 	local RAWJAR="$TERMUX_PKG_CACHEDIR/APKEditor-${TERMUX_PKG_VERSION}.jar"
 	local INSTALL_PREFIX="$TERMUX_PREFIX/libexec/apkeditor/apkeditor.jar"
+	# Download and install the jar
 	termux_download $TERMUX_PKG_SRCURL $RAWJAR $TERMUX_PKG_SHA256
-	install -Dm600 $RAWJAR \
-		$INSTALL_PREFIX
-	cat <<- EOF > $TERMUX_PREFIX/bin/apkeditor
-	#!${TERMUX_PREFIX}/bin/sh
-	JAVA_OPTS="-Xmx512M"
-	exec java \$JAVA_OPTS -jar $INSTALL_PREFIX "\$@"
-	EOF
-	chmod 700 $TERMUX_PREFIX/bin/apkeditor
+	install -Dm600 $RAWJAR $INSTALL_PREFIX
+	# Process and install the wrapper script
+	sed -e "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" \
+		$TERMUX_PKG_BUILDER_DIR/wrapper.in \
+		> $TERMUX_PREFIX/bin/$TERMUX_PKG_NAME
+	chmod 700 $TERMUX_PREFIX/bin/$TERMUX_PKG_NAME
 }
