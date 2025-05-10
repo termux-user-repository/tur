7f70a5ed0ba2b8f889a185ca73647d31769047d7
Veha Veha <vehav00@gmail.com>
2025-05-11 06:22:42 +0700

    chore(tur/apkeditor): runable latest jar

--- tur/apkeditor/build.sh
+++ tur/apkeditor/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Android binary resource files editor"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@Veha0001"
 TERMUX_PKG_VERSION="1.4.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/REAndroid/APKEditor/releases/download/V${TERMUX_PKG_VERSION}/APKEditor-${TERMUX_PKG_VERSION}.jar
 TERMUX_PKG_SHA256=c242f5fc4591667a0084668320d0016a20e7c2abae102c1bd4d640e11d9f60ee
 TERMUX_PKG_DEPENDS="openjdk-21"
@@ -15,11 +16,8 @@ TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 
 termux_step_make_install() {
 	local RAWJAR="$TERMUX_PKG_CACHEDIR/APKEditor-${TERMUX_PKG_VERSION}.jar"
-	local INSTALL_PREFIX="$TERMUX_PREFIX/libexec/apkeditor/apkeditor.jar"
-	# Download and install the jar
 	termux_download $TERMUX_PKG_SRCURL $RAWJAR $TERMUX_PKG_SHA256
-	install -Dm600 $RAWJAR $INSTALL_PREFIX
-	# Process and install the wrapper script
+	install -Dm600 $RAWJAR $TERMUX_PREFIX/libexec/apkeditor/apkeditor.jar
 	sed -e "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" \
 		$TERMUX_PKG_BUILDER_DIR/wrapper.in \
 		> $TERMUX_PREFIX/bin/$TERMUX_PKG_NAME
