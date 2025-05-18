b20fad61545b8f3c392f3d9e3a5bf2c22c5b4b76
Chongyun Lee <licy183@termux.dev>
2025-05-18 11:52:29 +0800

    wine-stable, winetricks: move to tur

--- tur/winetricks/build.sh
+++ tur/winetricks/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Work around problems and install applications under Wine
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="20250102"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Winetricks/winetricks/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=24d339806e3309274ee70743d76ff7b965fef5a534c001916d387c924eebe42e
 TERMUX_PKG_AUTO_UPDATE=true
