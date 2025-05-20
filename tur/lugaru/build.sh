0d19dfe16bd4bd0ef5b0df0977f56b94d6cab7e4
fervi <fervi@Shinzan>
2025-05-20 09:27:26 +0200

    Newpkg: Lugaru

---
+++ tur/lugaru/build.sh
@@ -0,0 +1,18 @@
+TERMUX_PKG_HOMEPAGE=https://osslugaru.gitlab.io
+TERMUX_PKG_DESCRIPTION="Lugaru HD, free and open source ninja rabbit fighting game"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="1.2"
+TERMUX_PKG_SRCURL=https://github.com/osslugaru/lugaru/releases/download/${TERMUX_PKG_VERSION}/lugaru-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=f3ea477caf78911c69939fbdc163f9f6517c7ef2267e716a0e050be1a166ef97
+TERMUX_PKG_DEPENDS="sdl2, glu, openal-soft, lugaru-data"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_INSTALL_PREFIX=/data/data/com.termux/files/usr
+-DSYSTEM_INSTALL=ON
+-DCMAKE_INSTALL_BINDIR=bin
+-DCMAKE_INSTALL_DATADIR=share/games
+"
+
+termux_step_pre_configure() {
+	export LDFLAGS+=" -Wl,--no-as-needed,-lOpenSLES,--as-needed"
+}
