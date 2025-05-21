e845e5c963a46280978b985c380233fc7561d3e9
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-05-22 01:32:26 +0800

    lib-tls-client: bump to 1.9.1 (#1753)

--- tur/lib-tls-client/build.sh
+++ tur/lib-tls-client/build.sh
@@ -1,23 +1,18 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/bogdanfinn/tls-client/
-TERMUX_PKG_DESCRIPTION="Shared library for catopen/catgets/catclose of Bionic Libc"
+TERMUX_PKG_DESCRIPTION="A net/http.Client like HTTP Client with options to select specific client TLS Fingerprints to use for requests"
 # LICENSE: BSD-4-Clause
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="1.8.0"
+TERMUX_PKG_VERSION="1.9.1"
 TERMUX_PKG_SRCURL="https://github.com/bogdanfinn/tls-client/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=b0402333cde85d0772d4dbc9fa2665211219bfd0565cc024523a741dccb38567
+TERMUX_PKG_SHA256=9be6e9ece417ccb67e1e9345eae96e7cd1857ee7187fe39a4426d87bdc750d44
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
-termux_step_pre_configure() {
+termux_step_make() {
 	termux_setup_golang
 
-	go mod init || :
-	go mod tidy
-}
-
-termux_step_make() {
 	cd cffi_dist
 	go build -buildmode=c-shared -o ./dist/lib-tls-client.so
 }
