0625e81621177916f0df1379701e84feaedcb6dd
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-05-22 01:34:31 +0800

    cfitsio: bump to 4.6.2 (#1754)

--- tur/cfitsio/build.sh
+++ tur/cfitsio/build.sh
@@ -3,13 +3,19 @@ TERMUX_PKG_DESCRIPTION="a library of C and Fortran subroutines for reading and w
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="License.txt"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="4.5.0"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e4854fc3365c1462e493aa586bfaa2f3d0bb8c20b75a524955db64c27427ce09
+TERMUX_PKG_VERSION="4.6.2"
+TERMUX_PKG_SRCURL="https://github.com/HEASARC/cfitsio/archive/refs/tags/cfitsio-$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=83f710f9441e7c703eeb485c9a475f50801ae7fad32ac7e356a406d318b4c02f
 TERMUX_PKG_DEPENDS="curl, zlib"
 TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_METHOD=repology
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DM_LIB=
+-DUTILS=OFF
+-DTESTS=OFF
+"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -lm"
@@ -17,4 +23,18 @@ termux_step_pre_configure() {
 	if [ -e licenses/License.txt ]; then
 		cp licenses/License.txt License.txt
 	fi
+}
+
+termux_step_post_massage() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION_GUARD_FILES="
+lib/libcfitsio.so.10
+"
+	local f
+	for f in ${_SOVERSION_GUARD_FILES}; do
+		if [ ! -e "${f}" ]; then
+			termux_error_exit "SOVERSION guard check failed."
+		fi
+	done
 }
