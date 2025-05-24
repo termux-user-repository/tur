64ddc14858122690116459c49d37ad255a428140
Chongyun Lee <licy183@termux.dev>
2025-05-24 12:31:21 +0800

    bazel7: revbump

--- tur/bazel/bazel7.subpackage.sh
+++
@@ -1,5 +0,0 @@
-TERMUX_SUBPKG_DESCRIPTION="Correct, reproducible, and fast builds for everyone (Version 7)"
-TERMUX_SUBPKG_DEPEND_ON_PARENT=deps
-TERMUX_SUBPKG_INCLUDE="
-bin/bazel-$TERMUX_PKG_VERSION
-"
