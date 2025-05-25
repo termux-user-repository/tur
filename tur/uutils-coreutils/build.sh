216b76ccb46564a92bd57ab6f996a49ed82fffe0
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-05-25 12:14:44 +0800

    uutils-coreutils: bump to 0.1.0 (#1768)

--- tur/uutils-coreutils/build.sh
+++ tur/uutils-coreutils/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://uutils.github.io/
 TERMUX_PKG_DESCRIPTION="Cross-platform Rust rewrite of the GNU coreutils"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="0.0.30"
+TERMUX_PKG_VERSION="0.1.0"
 TERMUX_PKG_SRCURL=https://github.com/uutils/coreutils/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=732c0ac646be7cc59a51cdfdb2d0ff1a4d2501c28f900a2d447c77729fdfca22
+TERMUX_PKG_SHA256=55c528f2b53c1b30cb704550131a806e84721c87b3707b588a961a6c97f110d8
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
@@ -23,7 +23,7 @@ termux_step_make() {
 		SELINUX_ENABLED=0 \
 		MANPAGES=0 \
 		COMPLETIONS=0 \
-		SKIP_UTILS="pinky uptime users who hostid"
+		SKIP_UTILS="pinky uptime users who hostid chcon runcon"
 }
 
 termux_step_make_install() {
@@ -38,5 +38,5 @@ termux_step_make_install() {
 		SELINUX_ENABLED=0 \
 		MANPAGES=0 \
 		COMPLETIONS=0 \
-		SKIP_UTILS="pinky uptime users who hostid"
+		SKIP_UTILS="pinky uptime users who hostid chcon runcon"
 }
