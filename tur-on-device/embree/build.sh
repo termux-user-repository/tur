ba973f75f9089e6884a082f115dcdac0152b8f68
Robert Kirkman <rkirkman@termux.dev>
2025-05-26 03:05:12 -0500

    addpkg(tur-on-device/blender4): 4.5.3
    - Fixes https://github.com/termux-user-repository/tur/issues/1740
    
    - Possibly fixes https://github.com/termux-user-repository/tur/issues/1447 (on my device, only the default OpenGL mode is working, but configuring this Blender to Vulkan mode might work for someone else)
    
    - Contains some patches copied from **T-Dynamos' previous Blender 4 package** https://github.com/termux-user-repository/tur/pull/521
      - Not all of it, for example, https://projects.blender.org/blender/blender-addons.git was deprecated, and is no longer necessary for Blender 4.2+
    
    - To launch, has **higher GPU requirements than Blender 3.6**.
      - Requires a graphics driver that provides at least **Desktop-type** OpenGL 4.3 or higher
      - An example of a device where this works well is **Samsung Galaxy S9 SM-G960U** with Adreno 630 and Zink + Turnip driver from `pkg install mesa-vulkan-icd-freedreno`, which provides **Desktop-type** OpenGL 4.6
    
    ![image](https://github.com/user-attachments/assets/ae60413e-0343-4bad-a679-e582047f1248)
    
    - Uses a Gentoo-inspired technique to make this Blender be compiled and installed in such a way that every single file is in a versioned path, making this **not conflict with the preexisting `tur-on-device/blender` package**
    
    Dependencies removed:
    - `sdl2`: SDL is disabled by default in Blender 4 since September 2024: https://devtalk.blender.org/t/sdl-support-to-be-disabled-in-release-builds/36564
    
    Dependencies added to match Arch Linux Blender: https://gitlab.archlinux.org/archlinux/packaging/packages/blender/-/blob/8ac89fe82c194a7c1b40e7c508ea101d3a205fe8/PKGBUILD
    - `boost`
    - `draco`
    - `embree`
    - `imath`
    - `libharu`
    - `libtbb`
    - `libwebp`
    - `libx11`
    - `libxfixes`
    - `libxi`
    - `libxkbcommon`
    - `libyaml-cpp`
    - `materialx`
    - `mold`
    - `oidn`
    - `openpgl`
    - `openshadinglanguage`
    - `opensubdiv`
    - `openxr`
    - `usd`
    - `zlib`
    - `zstd`
    
    Dependencies omitted intentionally:
    
    > [!TIP]
    > I will enable any of these if someone asks for one of them to be enabled and has a way to help test it.
    
    - `xdg-utils`: `xdg-open` is implemented for non-folder files by `termux-tools`
      - `termux-tools` `xdg-open` is assumed to be preferred over `xdg-utils-xdg-open` from Termux `xdg-utils` until a counterexample (need to auto-open a folder) is found per-software
    - `libdecor`, etc: The use of Wayland in Termux is possible, but not prioritized at this time.
      - Complete implementations of Wayland are considered ABI-backwards-compatible with prebuilt X11 programs through Xwayland.
    - `pipewire`: The use of Pipewire in Termux is possible, but not prioritized at this time.
      - Complete implementations of Pipewire are considered ABI-backwards-compatible with prebuilt Pulseaudio programs through Pipewire-pulse.
    - `jack2`: The use of JACK in Termux is possible, but not prioritized at this time
    - `libjemalloc`: This package is disabled in Termux
    - CUDA, HIP, OneAPI: Devices which support one of these and also Termux simultaneously are thought to be very uncommon
      - Potential candidate supported device: **Google Pixel C**
    - `opencollada`: The Blender version of this is an active fork of OpenCOLLADA, while the official OpenCOLLADA is very slow-moving.
      - https://aur.archlinux.org/packages/opencollada-blender-git
    
    New packages added:
    
    - `embree`
    - `materialx`
    - `openshadinglanguage`
    - `pyside6`
    - `usd`
    - `blender4`
    
    Packages moved to termux/termux-packages:
    
    - `openimageio`
    - `openvdb`
    - `pybind11`

---
+++ tur-on-device/embree/build.sh
@@ -0,0 +1,26 @@
+TERMUX_PKG_HOMEPAGE=https://www.embree.org/
+TERMUX_PKG_DESCRIPTION="High-performance ray tracing library developed at Intel"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="4.4.0"
+TERMUX_PKG_SRCURL="https://github.com/RenderKit/embree/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=acb517b0ea0f4b442235d5331b69f96192c28da6aca5d5dde0cbe40799638d5c
+TERMUX_PKG_DEPENDS="libc++, libtbb"
+TERMUX_PKG_BUILD_DEPENDS="freeglut, libxmu"
+# Embree does not support 32-bit Linux
+TERMUX_PKG_EXCLUDED_ARCHES="arm, i686"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DEMBREE_TUTORIALS=OFF
+-DEMBREE_MAX_ISA=AVX512SKX
+-DEMBREE_BACKFACE_CULLING=OFF
+"
+
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		termux_error_exit "This package is extremely hard to cross-compile. If you know how, please help!"
+	fi
+}
+
+termux_step_post_make_install() {
+	mv "$TERMUX_PREFIX"/embree-vars.* "$TERMUX_PREFIX/bin/"
+}
---
+++ tur-on-device/materialx/build.sh
@@ -0,0 +1,65 @@
+TERMUX_PKG_HOMEPAGE=https://materialx.org/
+TERMUX_PKG_DESCRIPTION="Open standard for the exchange of rich material and look-development content in computer graphics"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="1.39.4"
+TERMUX_PKG_SRCURL="https://github.com/AcademySoftwareFoundation/MaterialX/releases/download/v$TERMUX_PKG_VERSION/MaterialX-$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=5335565de46195031763951d7bec29a3a2fa96656d9cf3972e1408b2578f7ebe
+TERMUX_PKG_DEPENDS="libc++, opencolorio, opengl, openimageio, python, zenity"
+TERMUX_PKG_MAKE_DEPENDS="dos2unix, pybind11"
+# viewer and graph editor GUIs depend on git submodules and an
+# old version of GLFW that is tedious to patch.
+# Enable if strongly desired.
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DMATERIALX_BUILD_SHARED_LIBS=ON
+-DMATERIALX_BUILD_PYTHON=ON
+-DMATERIALX_BUILD_VIEWER=OFF
+-DMATERIALX_BUILD_GRAPH_EDITOR=OFF
+-DMATERIALX_BUILD_OIIO=ON
+-DMATERIALX_BUILD_OCIO=ON
+"
+
+# The default installation paths of this software are very messy if not manually corrected.
+# Using packaging file structure fixes based on Arch Linux:
+# https://gitlab.archlinux.org/archlinux/packaging/packages/materialx/-/blob/bbc9215b7c941ff45cee19b2386ca7ce5e65dd96/PKGBUILD
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		termux_error_exit "This package is extremely hard to cross-compile. If you know how, please help!"
+	fi
+
+	sed -i "s|CMAKE_INSTALL_PREFIX|CMAKE_BINARY_DIR|g" python/CMakeLists.txt
+
+	sed -i "s|resources|$TERMUX_PREFIX/share/materialx/resources|g" source/MaterialXView/{Main.cpp,Viewer.cpp}
+	sed -i "s|\"libraries\"|\"$TERMUX_PREFIX/share/materialx/libraries\"|g" source/MaterialXView/Main.cpp
+	sed -i "s|resources|$TERMUX_PREFIX/share/materialx/resources|g" source/MaterialXGraphEditor/{Main.cpp,Graph.cpp}
+	sed -i "s|\"libraries\"|\"$TERMUX_PREFIX/share/materialx/libraries\"|g" source/MaterialXGraphEditor/{Main.cpp,Graph.cpp}
+	sed -i "s|\"libraries\"|\"$TERMUX_PREFIX/share/materialx/libraries\"|g" source/MaterialXGenShader/GenOptions.h
+
+	dos2unix python/Scripts/*
+
+	export LDFLAGS+=" -lpython$TERMUX_PYTHON_VERSION"
+}
+
+termux_step_post_make_install() {
+	rm "$TERMUX_PREFIX"/{LICENSE,CHANGELOG.md,README.md,THIRD-PARTY.md}
+	rm "$TERMUX_PREFIX/python/Scripts/README.md"
+	local file name
+	for file in "$TERMUX_PREFIX"/python/Scripts/*; do
+		name="${file%.py}"
+		chmod +x "$file"
+		mv "$file" "$name"
+	done
+
+	rm -rf "$TERMUX_PREFIX/share/$TERMUX_PKG_NAME"
+	mkdir -p "$TERMUX_PREFIX/share/$TERMUX_PKG_NAME"
+	mv -f "$TERMUX_PREFIX"/{resources,libraries} "$TERMUX_PREFIX/share/$TERMUX_PKG_NAME/"
+	cp "$TERMUX_PREFIX"/python/Scripts/* "$TERMUX_PREFIX/bin/"
+	rm -r "$TERMUX_PREFIX/python/Scripts/"
+	mkdir -p "$TERMUX_PREFIX/lib/python$TERMUX_PYTHON_VERSION/site-packages"
+	rm -rf "$TERMUX_PREFIX/lib/python$TERMUX_PYTHON_VERSION/site-packages"/MaterialX*
+	mv "$TERMUX_PREFIX"/python/* "$TERMUX_PREFIX/lib/python$TERMUX_PYTHON_VERSION/site-packages/"
+
+	sed -i 's|libraries|share/materialx/libraries|g' "$TERMUX_PREFIX/lib/cmake/MaterialX/MaterialXConfig.cmake"
+	sed -i 's|python|lib/python'$TERMUX_PYTHON_VERSION'/site-packages/MaterialX|g' "$TERMUX_PREFIX/lib/cmake/MaterialX/MaterialXConfig.cmake"
+	sed -i 's|resources|share/materialx/resources|g' "$TERMUX_PREFIX/lib/cmake/MaterialX/MaterialXConfig.cmake"
+}
