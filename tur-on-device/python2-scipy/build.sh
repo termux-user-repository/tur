b016841ae76c159322ceb20f081c65a3332e6222
Robert Kirkman <rkirkman@termux.dev>
2025-05-30 23:48:46 -0500

    fix(tur-on-device/python2-scipy): explicitly link to `libm.so`
    - Fixes https://github.com/termux-user-repository/tur/issues/1783
    
    - Removes interactive `bash` launch from `termux_step_post_massage()`
    
    - Works around this error happening intermittently during the build for only 32-bit x86:
    
    ```
     i686-linux-android-gfortran-11: fatal error: output filename may not be empty
        compilation terminated.
        Traceback (most recent call last):
          File "/data/data/com.termux/files/home/.termux-build/python2-scipy/tmp/_fake_bin/gfortran", line 30, in <module>
            main(sys.argv)
          File "/data/data/com.termux/files/home/.termux-build/python2-scipy/tmp/_fake_bin/gfortran", line 27, in main
            subprocess.check_call(args, env=os.environ, cwd=cwd, text=True)
          File "/data/data/com.termux/files/usr/lib/python3.12/subprocess.py", line 413, in check_call
            raise CalledProcessError(retcode, cmd)
        subprocess.CalledProcessError: Command '['/data/data/com.termux/files/usr/bin/i686-linux-android-gfortran-11', '-Wall', '-g', '-ffixed-form', '-fno-second-underscore', '-fallow-argument-mismatch', '-O3', '-funroll-loops', '-I/data/data/com.termux/files/usr/lib/python2.7/site-packages/numpy/core/include', '-c', '-c', 'scipy/special/specfun/specfun.f', '-o', '']' returned non-zero exit status 1.
        error: Command "/data/data/com.termux/files/home/.termux-build/python2-scipy/tmp/_fake_bin/gfortran -Wall -g -ffixed-form -fno-second-underscore -fallow-argument-mismatch -O3 -funroll-loops -I/data/data/com.termux/files/usr/lib/python2.7/site-packages/numpy/core/include -c -c scipy/special/specfun/specfun.f -o build/temp.linux-i686-2.7/scipy/special/specfun/specfun.o" failed with exit status 1
      Running setup.py install for scipy: finished with status 'error'
    ```

--- tur-on-device/python2-scipy/build.sh
+++ tur-on-device/python2-scipy/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Fundamental algorithms for scientific computing in Pytho
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="1.2.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://files.pythonhosted.org/packages/62/4f/7e95c5000c411164d5ca6f55ac54cda5d200a3b6719dafd215ee0bd61578/scipy-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=ecbe6413ca90b8e19f8475bfa303ac001e81b04ec600d17fa7f816271f7cca57
 TERMUX_PKG_DEPENDS="libc++, libopenblas, python2, python2-numpy"
@@ -22,6 +23,7 @@ termux_step_pre_configure() {
 	# Use a wrapper to ignore `-static-openmp`
 	mkdir -p $TERMUX_PKG_TMPDIR/_fake_bin
 	sed -e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
+		-e "s|@TERMUX_ARCH@|${TERMUX_ARCH}|g" \
 		-e "s|@COMPILER@|$(command -v ${CROSS_PREFIX}-gfortran-11)|g" \
 		"$TERMUX_PKG_BUILDER_DIR"/wrapper.in \
 		> $TERMUX_PKG_TMPDIR/_fake_bin/gfortran
@@ -31,7 +33,7 @@ termux_step_pre_configure() {
 	CPPFLAGS+=" -Wno-implicit-function-declaration -Wno-implicit-int -Wno-error=register -Wno-error=incompatible-function-pointer-types"
 	CFLAGS+=" -Wno-implicit-function-declaration -Wno-implicit-int -Wno-error=register -Wno-error=incompatible-function-pointer-types"
 	CXXFLAGS+=" -Wno-implicit-function-declaration -Wno-implicit-int -Wno-error=register -Wno-error=incompatible-function-pointer-types"
-	LDFLAGS+=" -Wl,--no-as-needed -lpython2.7"
+	LDFLAGS+=" -Wl,--no-as-needed -lpython2.7 -lm"
 
 	# https://github.com/scipy/scipy/issues/11611
 	export FFLAGS="-fallow-argument-mismatch"
@@ -51,7 +53,6 @@ termux_step_make_install() {
 
 termux_step_post_massage() {
 	cd $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX || exit 1
-	bash
 
 	local _rm_files="lib/python2.7/!(site-packages)"
 	shopt -s extglob
