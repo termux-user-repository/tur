9748ddc40053c1fada5e843dea7c5020c034abdb
Chongyun Lee <licy183@termux.dev>
2025-06-02 22:04:20 +0800

    libmongoc: fix build
    [skip ci]

--- tur/libmongoc/build.sh
+++ tur/libmongoc/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_VERSION=1.30.3
 TERMUX_PKG_SRCURL=https://github.com/mongodb/mongo-c-driver/releases/download/$TERMUX_PKG_VERSION/mongo-c-driver-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9f42eb507e8c591546dc9c584230a6f71127842cb597cc4ab219d1ebe251e7af
 TERMUX_PKG_DEPENDS="libbson, libicu, libmongocrypt, libsasl, libsnappy, openssl, zlib, zstd"
+TERMUX_PKG_BUILD_DEPENDS="libbson-static, libmongocrypt-static"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX
 -DCMAKE_INSTALL_LIBDIR=lib
