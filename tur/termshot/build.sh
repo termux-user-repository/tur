74317ddef882e5af727a48587c89bcd07c02922a
Veha Veha <vehav00@gmail.com>
2025-06-18 06:53:23 +0700

    termshot

--- tur/termshot/build.sh
+++ tur/termshot/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE="https://github.com/homeport/termshot"
-TERMUX_PKG_DESCRIPTION="A utility to create terminal screenshots with style"
+TERMUX_PKG_DESCRIPTION="Creates screenshots based on terminal command output"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@Veha0001"
-TERMUX_PKG_VERSION="1.4.0"
-TERMUX_PKG_SRCURL="https://github.com/homeport/termshot/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256="ddcbf5e4b3b2b9eb19b4c93c4d6e9cc4e5f2c9e7f344b0e076c2e610cb803581"
+TERMUX_PKG_VERSION=0.5.0
+TERMUX_PKG_SRCURL=git+https://github.com/homeport/termshot
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="imagemagick"
@@ -14,7 +13,7 @@ termux_step_pre_configure() {
 }
 
 termux_step_make() {
-	go build -trimpath -o termshot ./cmd/termshot
+	go build -ldflags="-s -w -X github.com/homeport/termshot/internal/cmd.version=$TERMUX_PKG_VERSION" -trimpath -o termshot ./cmd/termshot
 }
 
 termux_step_make_install() {
