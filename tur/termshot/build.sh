79eadc274430136ea7eda6574ba3cef1ab689be5
Veha Veha <vehav00@gmail.com>
2025-06-18 07:38:25 +0700

    termshot

--- tur/termshot/build.sh
+++ tur/termshot/build.sh
@@ -13,11 +13,7 @@ termux_step_pre_configure() {
 }
 
 termux_step_make() {
-	LDFALAGS="-s -w -X github.com/homeport/termshot/internal/cmd.version=$TERMUX_PKG_VERSION"
-	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
-		LDFALAGS+=" -extldflags=-static"
-	fi
-	go build -ldflags="$LDFALAGS" -trimpath -o termshot ./cmd/termshot
+	go build -ldflags="-X github.com/homeport/termshot/internal/cmd.version=$TERMUX_PKG_VERSION" -trimpath -o termshot ./cmd/termshot
 }
 
 termux_step_make_install() {
