98fec8e07c68b5c60e97dcb38664af4fbbdb82d3
IntinteDAO <lordfervi@protonmail.com>
2025-07-12 13:16:01 +0200

    Update PKG Maintainer

--- tur/lugaru/build.sh
+++ tur/lugaru/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://osslugaru.gitlab.io
 TERMUX_PKG_DESCRIPTION="Lugaru HD, free and open source ninja rabbit fighting game"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_MAINTAINER="@IntinteDAO"
 TERMUX_PKG_VERSION="1.2"
 TERMUX_PKG_SRCURL=https://github.com/osslugaru/lugaru/releases/download/${TERMUX_PKG_VERSION}/lugaru-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=f3ea477caf78911c69939fbdc163f9f6517c7ef2267e716a0e050be1a166ef97
