b614643dd924fbfd1491526e2a5db99a896b0a8a
Líng Yì <mail@ancientcatz.eu.org>
2025-10-03 00:44:51 +0700

    addpkg(tur/crush): 0.8.1 (#1892)
    [skip ci]

---
+++ tur/crush/build.sh
@@ -0,0 +1,36 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/charmbracelet/crush
+TERMUX_PKG_DESCRIPTION="The glamourous AI coding agent for your favourite terminal"
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="LICENSE.md"
+TERMUX_PKG_MAINTAINER="@ancientcatz"
+TERMUX_PKG_VERSION="0.8.1"
+TERMUX_PKG_SRCURL=https://github.com/charmbracelet/crush/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=45de4d00032381a178b33bc1b9adf0cc87bb6d31632d0d26abca84ea99926496
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=false
+TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
+
+termux_step_pre_configure() {
+	termux_setup_golang
+}
+
+termux_step_make() {
+	export GOEXPERIMENT='greenteagc'
+	go build -ldflags "-s -w -X github.com/charmbracelet/crush/internal/version.Version=${TERMUX_PKG_VERSION}"
+}
+
+termux_step_make_install() {
+	mkdir -p "${TERMUX_PREFIX}/share/man/man1"
+	mkdir -p "${TERMUX_PREFIX}/share/zsh/site-functions"
+	mkdir -p "${TERMUX_PREFIX}/share/fish/vendor_completions.d"
+	mkdir -p "${TERMUX_PREFIX}/share/bash-completion/completions"
+
+	# borrowed from packages/gh
+	unset GOOS GOARCH CGO_LDFLAGS
+	unset CC CXX CFLAGS CXXFLAGS LDFLAGS
+	go run .             man > "${TERMUX_PREFIX}/share/man/man1/${TERMUX_PKG_NAME}.1"
+	go run . completion  zsh > "${TERMUX_PREFIX}/share/zsh/site-functions/_${TERMUX_PKG_NAME}"
+	go run . completion bash > "${TERMUX_PREFIX}/share/bash-completion/completions/${TERMUX_PKG_NAME}"
+	go run . completion fish > "${TERMUX_PREFIX}/share/fish/vendor_completions.d/${TERMUX_PKG_NAME}.fish"
+	install -Dm700 crush "$TERMUX_PREFIX/bin/crush"
+}
