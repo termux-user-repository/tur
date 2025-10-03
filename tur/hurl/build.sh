9c16245e63229d196d7d9bb9676f6df198b1ce78
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-10-03 17:45:13 +0800

    hurl: bump to 7.0.0 (#1926)

--- tur/hurl/build.sh
+++ tur/hurl/build.sh
@@ -2,16 +2,22 @@ TERMUX_PKG_HOMEPAGE=https://github.com/Orange-OpenSource/hurl
 TERMUX_PKG_DESCRIPTION="Hurl, run and test HTTP requests with plain text"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@SunPodder"
-TERMUX_PKG_VERSION="6.1.1"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="7.0.0"
 TERMUX_PKG_SRCURL="https://github.com/Orange-OpenSource/hurl/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=26ef1ed685f4b94190914a0e03127f7b7f6a488abf65758c19092dc6b9034b2c
+TERMUX_PKG_SHA256=3f505848aabd9eec78360928b0fcbfea237a049b5c8d25b8d9e0aeb3f9d8dfed
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_BUILD_DEPENDS="openssl, libxml2"
 
 termux_step_make() {
 	termux_setup_rust
+
+	BINDGEN_EXTRA_CLANG_ARGS="--target=$CCTERMUX_HOST_PLATFORM"
+	BINDGEN_EXTRA_CLANG_ARGS+=" --sysroot=$NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot"
+	local env_name=BINDGEN_EXTRA_CLANG_ARGS_${CARGO_TARGET_NAME}
+	env_name=${env_name//-/_}
+	export "$env_name"="$BINDGEN_EXTRA_CLANG_ARGS"
+
 	cargo build --jobs $TERMUX_PKG_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
 }
 
