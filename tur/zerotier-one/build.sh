ca54443362b1b6d568327d7a1b3d3b8bb2ac1668
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-10-04 00:43:31 +0800

    zerotier-one: bump to 1.16.0

--- tur/zerotier-one/build.sh
+++ tur/zerotier-one/build.sh
@@ -1,12 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://www.zerotier.com
 TERMUX_PKG_DESCRIPTION="Creates virtual Ethernet networks of almost unlimited size."
-# LICENSE: Business Source License 1.1 (BSL-1.1)
+# LICENSE: MPL-2.0, ZeroTier SOURCE-AVAILABLE LICENSE
 TERMUX_PKG_LICENSE="non-free"
-TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
+TERMUX_PKG_LICENSE_FILE="LICENSE.txt, LICENSE-MPL.txt, nonfree/LICENSE.md"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.14.2"
+TERMUX_PKG_VERSION="1.16.0"
 TERMUX_PKG_SRCURL=https://github.com/zerotier/ZeroTierOne/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c2f64339fccf5148a7af089b896678d655fbfccac52ddce7714314a59d7bddbb
+TERMUX_PKG_SHA256=aa9de313d365bf0efb3871aaa56f2d323a08f46df47b627c4eff4f4203fa7fc5
 TERMUX_PKG_DEPENDS="miniupnpc, natpmpc, openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
