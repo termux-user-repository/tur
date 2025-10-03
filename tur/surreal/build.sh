cfd3740811c7226ddcc824f38d423d76254a23b3
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-10-03 18:37:05 +0800

    surreal: bump to 2.3.10

--- tur/surreal/build.sh
+++ tur/surreal/build.sh
@@ -4,9 +4,9 @@ TERMUX_PKG_DESCRIPTION="A scalable, distributed, collaborative, document-graph d
 TERMUX_PKG_LICENSE="non-free"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@SunPodder"
-TERMUX_PKG_VERSION="2.3.7"
+TERMUX_PKG_VERSION="2.3.10"
 TERMUX_PKG_SRCURL="https://github.com/surrealdb/surrealdb/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=3c7aa9865e339e8db7468c455c56fbed5ab4465bf9a86c2a3db7f85bab328e31
+TERMUX_PKG_SHA256=72294908d805ba73a8abc2fc68e07c1dababa17d6d00a6a4da0f93ab208eac2e
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="openssl, zlib"
@@ -37,6 +37,13 @@ termux_step_configure() {
 termux_step_make() {
 	export CXXFLAGS+=" -lz"
 	export RUSTFLAGS="--cfg surrealdb_unstable"
+
+	BINDGEN_EXTRA_CLANG_ARGS="--target=$CCTERMUX_HOST_PLATFORM"
+	BINDGEN_EXTRA_CLANG_ARGS+=" --sysroot=$NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot"
+	local env_name=BINDGEN_EXTRA_CLANG_ARGS_${CARGO_TARGET_NAME}
+	env_name=${env_name//-/_}
+	export "$env_name"="$BINDGEN_EXTRA_CLANG_ARGS"
+
 	cargo build --jobs $TERMUX_PKG_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
 }
 
