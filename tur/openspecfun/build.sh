773c6942204ba639be36edea1543bfa05501fdc7
Stas Sergeev <stsp2@yandex.ru>
2025-10-04 10:22:15 +0300

    openspecfun: rev-bump
    Version of 'openspecfun' has not changed.
    Either 'TERMUX_PKG_REVISION' or 'TERMUX_PKG_VERSION'
    need to be modified in the build.sh when changing a package build.
    Alternatively you can add '[no version check]'.
    To the commit message to skip this check.
    
    [skip ci]

--- tur/openspecfun/build.sh
+++ tur/openspecfun/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A collection of special mathematical functions"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=0.5.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/JuliaMath/openspecfun/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=6209413bc5291f4aad68903fc7851a18ddc94d3432ee0dd1b69e02dc370ece2a
 TERMUX_PKG_BUILD_IN_SRC=true
