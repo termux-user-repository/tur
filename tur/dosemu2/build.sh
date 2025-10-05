87ee56d27a0fa6a195518bd1db22e495b80330ce
Stas Sergeev <stsp2@yandex.ru>
2025-10-05 18:17:21 +0300

    dosemu2: update to latest (closes #1833)
    Had to change sha256 calc, as involving tar no longer generates
    similar hashes on my PC and on CI.

--- tur/dosemu2/build.sh
+++ tur/dosemu2/build.sh
@@ -3,18 +3,18 @@ TERMUX_PKG_DESCRIPTION="Run DOS programs under linux."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@stsp"
 TERMUX_PKG_VERSION="2.0pre9-git"
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=git+https://github.com/dosemu2/dosemu2
 TERMUX_PKG_GIT_BRANCH=devel
-_COMMIT=58cd68c2bbbf1976bbb80d39c4ddd8edd2dd93ea
-TERMUX_PKG_SHA256=e2fcd6e79091442f22fc61f24cac3c9e21dbe47df31ca3ae3f65b8dc457c3c72
-TERMUX_PKG_BUILD_DEPENDS="libandroid-posix-semaphore, libandroid-glob, slang, libao, fluidsynth, ladspa-sdk, libslirp, libbsd, readline, json-c, libseccomp, libsearpc, sdl2 | sdl2-compat, sdl2-ttf, fontconfig, fdpp, dj64dev"
+_COMMIT=e368b3cd4c83a7491b208ced18c8421df613632f
+TERMUX_PKG_SHA256=2b1378a97030a1a5c4ae15c0618a8255c5dd553497138439010d5417cde1fb89
+TERMUX_PKG_BUILD_DEPENDS="libandroid-posix-semaphore, libandroid-glob, slang, libao, fluidsynth, ladspa-sdk, libslirp, libbsd, readline, json-c, libseccomp, libsearpc, sdl3, sdl3-ttf, fontconfig, fdpp, dj64dev"
 TERMUX_PKG_DEPENDS="comcom64, instfd, libandroid-posix-semaphore, libandroid-glob, slang, libao, fluidsynth, ladspa-sdk, libslirp, libbsd, readline, json-c, libseccomp, libsearpc, fdpp, dj64dev"
 
 termux_step_post_get_source() {
 	git fetch --unshallow
 	git checkout $_COMMIT
-	local s=$(git archive --format=tar HEAD | sha256sum -b)
+	local s=$(git ls-files | xargs cat | sha256sum -b)
 	if [[ "${s}" != "${TERMUX_PKG_SHA256} "* ]]; then
 		termux_error_exit "Checksum mismatch for source files."
 	fi
