822a2eb4033170e3504a49efe72e618e78eb4434
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-10-05 19:07:53 +0800

    hashcat: bump to 7.1.2 (#1956)

--- tur-hacking/hashcat/build.sh
+++ tur-hacking/hashcat/build.sh
@@ -3,30 +3,35 @@ TERMUX_PKG_DESCRIPTION="World's fastest and most advanced password recovery util
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="docs/license.txt"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="6.2.6"
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION="7.1.2"
 TERMUX_PKG_SRCURL=https://github.com/hashcat/hashcat/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b25e1077bcf34908cc8f18c1a69a2ec98b047b2cbcf0f51144dcf3ba1e0b7b2a
-TERMUX_PKG_DEPENDS="opencl-vendor-driver, libiconv"
+TERMUX_PKG_SHA256=9546a6326d747530b44fcc079babad40304a87f32d3c9080016d58b39cfc8b96
+TERMUX_PKG_DEPENDS="libc++, libiconv, opencl-vendor-driver"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_configure() {
-	if [ "$TERMUX_ARCH" = 'aarch64' ]; then
-		CFLAGS+=' -march=armv8.1-a+crypto'
-		CXXFLAGS+=' -march=armv8.1-a+crypto'
-	fi
+	LDFLAGS+=" -liconv"
 }
 
 termux_step_make() {
 	rm -rf $TERMUX_PREFIX/opt/hashcat/
 	mkdir -p $TERMUX_PREFIX/opt/hashcat/
 
-	make -j $TERMUX_PKG_MAKE_PROCESSES
+	make \
+		CC_LINUX="$CC" \
+		CXX_LINUX="$CXX" \
+		AR_LINUX="$AR" \
+		-j $TERMUX_PKG_MAKE_PROCESSES \
+		host_linux modules_linux
 }
 
 termux_step_make_install() {
-	make install
+	make \
+		CC_LINUX="$CC" \
+		CXX_LINUX="$CXX" \
+		AR_LINUX="$AR" \
+		install_docs install_shared install_tools install_tunings install_kernels install_modules install_hashcat
 
 	cat <<- EOF > "$TERMUX_PREFIX"/bin/hashcat
 	LD_LIBRARY_PATH="$TERMUX_PREFIX/opt/vendor/lib:$TERMUX_PREFIX/lib" $TERMUX_PREFIX/opt/hashcat/bin/hashcat "\$@"
