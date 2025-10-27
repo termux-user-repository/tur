a5d21712a0f8a2b130800954ae8ad9a94f56a04e
Robert Kirkman <rkirkman@termux.dev>
2025-10-27 03:34:32 -0500

    bump(tur/openjdk-11): 11.0.29
    - Also add all applicable improvements that were introduced to the github.com/termux/termux-packages `openjdk-17`, `openjdk-21` and `openjdk-25` packages in between April 2024 and October 2025
      - Consult https://github.com/termux/termux-packages/commits/master/packages/openjdk-17/build.sh for a general overview
      - New `build.sh` follows a structure derived from a combination of the `openjdk-17` and `openjdk-25` packages
        - `termux_step_create_debscripts()` based on that of `openjdk-17`
        - `termux_step_host_build()` based on that of `openjdk-25`
    
    - LTO unfortunately not working although it does in later JDKs
      - Error: `ld: error: /home/builder/.termux-build/openjdk-11/src/build/linux-aarch64-normal-server-release /buildjdk/hotspot/variant-server/libjvm/objs/abstractCompiler.o: Invalid record`

--- tur/openjdk-11/build.sh
+++ tur/openjdk-11/build.sh
@@ -1,74 +1,122 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/openjdk/mobile
-TERMUX_PKG_DESCRIPTION="Java development kit and runtime (Version 11)"
+TERMUX_PKG_HOMEPAGE=https://openjdk.java.net
+TERMUX_PKG_DESCRIPTION="Java development kit and runtime"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=11.0.23+9
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://openjdk-sources.osci.io/openjdk11/openjdk-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=f51be67721f6cb8d14b0cd75cc9b53634e7d83e808148940d64e67c8b77accfc
-TERMUX_PKG_DEPENDS="libiconv, libjpeg-turbo, zlib, libandroid-spawn"
-TERMUX_PKG_BUILD_DEPENDS="cups, libandroid-spawn, libandroid-shmem, xorgproto"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="11.0.29"
+TERMUX_PKG_SRCURL=https://github.com/openjdk/jdk11u/archive/refs/tags/jdk-${TERMUX_PKG_VERSION}-ga.tar.gz
+TERMUX_PKG_SHA256=258bc09a2b1ce6d5965a1538ffaa2e2d25670ac3b75a43e7a08d210257e99bfc
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libandroid-shmem, libandroid-spawn, libiconv, libjpeg-turbo, zlib, littlecms, alsa-plugins"
+TERMUX_PKG_BUILD_DEPENDS="cups, fontconfig, libxrandr, libxt, xorgproto, alsa-lib"
 # openjdk-11-x is recommended because X11 separation is still very experimental.
 TERMUX_PKG_RECOMMENDS="ca-certificates-java, openjdk-11-x, resolv-conf"
 TERMUX_PKG_SUGGESTS="cups"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_HAS_DEBUG=false
+TERMUX_PKG_HOSTBUILD=true
+
+termux_pkg_auto_update() {
+	# based on `termux_github_api_get_tag.sh`
+	# fetch newest tags
+	local newest_tags newest_tag
+	newest_tags="$(curl -d "$(cat <<-EOF | tr '\n' ' '
+	{
+		"query": "query {
+			repository(owner: \"openjdk\", name: \"jdk11u\") {
+				refs(refPrefix: \"refs/tags/\", first: 20, orderBy: {
+					field: TAG_COMMIT_DATE, direction: DESC
+				})
+				{ edges { node { name } } }
+			}
+		}"
+	}
+	EOF
+	)" \
+		-H "Authorization: token ${GITHUB_TOKEN}" \
+		-H "Accept: application/vnd.github.v3+json" \
+		--silent \
+		--location \
+		--retry 10 \
+		--retry-delay 1 \
+		https://api.github.com/graphql \
+		| jq '.data.repository.refs.edges[].node.name')"
+	# filter only tags having "-ga" and extract only raw version.
+	read -r newest_tag < <(echo "$newest_tags" | grep -Po '11\.\d+\.\d+(?=-ga)' | sort -Vr)
+
+	[[ -z "${newest_tag}" ]] && termux_error_exit "Unable to get tag from ${TERMUX_PKG_SRCURL}"
+	termux_pkg_upgrade_version "${newest_tag}"
+}
+
+termux_step_host_build() {
+	# precompiled JDK release for GNU/Linux to use as host JDK for bootstrapping
+	# does not need to exactly match the TERMUX_PKG_VERSION
+	JDK_BINURL="https://download.java.net/openjdk/jdk11.0.0.2/ri/openjdk-11.0.0.2_linux-x64.tar.gz"
+	JDK_BINARCHIVE="${TERMUX_PKG_CACHEDIR}/openjdk-11.0.0.2_linux-x64.tar.gz"
+	JDK_BINSHA256=0ab38c0cf5c6809d29c35393faad0ec45703d55a92ec4abd81b55b14fafb3c2d
+	termux_download "$JDK_BINURL" "$JDK_BINARCHIVE" "$JDK_BINSHA256"
+	tar -xf "$JDK_BINARCHIVE" --strip-components=1 -C "$TERMUX_PKG_HOSTBUILD_DIR"
+}
 
 termux_step_pre_configure() {
 	unset JAVA_HOME
-
-	env -i PATH="$PATH" sudo apt update
-	env -i PATH="$PATH" sudo apt install openjdk-11-jdk openjdk-11-jre zip -y
 }
 
 termux_step_configure() {
-	unset JAVA_HOME
-
-	local _llvm_base_path="/usr/lib/llvm-18"
-
-	local jdk_ldflags="-L${TERMUX_PREFIX}/lib -Wl,-rpath=$TERMUX_PREFIX/lib/jvm/java-11-openjdk/lib -Wl,-rpath=${TERMUX_PREFIX}/lib -Wl,--enable-new-dtags"
+	local jdk_ldflags="-L${TERMUX_PREFIX}/lib \
+		-Wl,-rpath=$TERMUX_PREFIX/lib/jvm/java-11-openjdk/lib \
+		-Wl,-rpath=${TERMUX_PREFIX}/lib -Wl,--enable-new-dtags"
 	bash ./configure \
+		--with-boot-jdk="$TERMUX_PKG_HOSTBUILD_DIR" \
 		--disable-precompiled-headers \
 		--disable-warnings-as-errors \
 		--enable-option-checking=fatal \
-		--openjdk-target=$TERMUX_HOST_PLATFORM \
-		--with-cups-include="$TERMUX_PREFIX/include" \
+		--with-version-pre="" \
+		--with-version-opt="" \
+		--with-jvm-variants=server \
 		--with-debug-level=release \
+		--openjdk-target=$TERMUX_HOST_PLATFORM \
+		--with-toolchain-type=clang \
 		--with-extra-cflags="$CFLAGS $CPPFLAGS -DLE_STANDALONE -D__ANDROID__=1 -D__TERMUX__=1" \
 		--with-extra-cxxflags="$CXXFLAGS $CPPFLAGS -DLE_STANDALONE -D__ANDROID__=1 -D__TERMUX__=1" \
-		--with-extra-ldflags="${jdk_ldflags} -Wl,--as-needed -landroid-shmem -landroid-spawn" \
+		--with-extra-ldflags="${jdk_ldflags} -Wl,--as-needed -landroid-shmem -landroid-spawn -liconv" \
+		--with-cups-include="$TERMUX_PREFIX/include" \
 		--with-fontconfig-include="$TERMUX_PREFIX/include" \
 		--with-freetype-include="$TERMUX_PREFIX/include/freetype2" \
 		--with-freetype-lib="$TERMUX_PREFIX/lib" \
-		--with-giflib=system \
-		--with-jvm-variants=server \
-		--with-libjpeg=system \
-		--with-libpng=system \
-		--with-toolchain-type=clang \
+		--with-alsa="$TERMUX_PREFIX" \
+		--with-alsa-include="$TERMUX_PREFIX/include/alsa" \
+		--with-alsa-lib="$TERMUX_PREFIX/lib" \
 		--with-x="$TERMUX_PREFIX/include/X11" \
-		--with-zlib=system \
 		--x-includes="$TERMUX_PREFIX/include/X11" \
 		--x-libraries="$TERMUX_PREFIX/lib" \
+		--with-giflib=system \
+		--with-lcms=system \
+		--with-libjpeg=system \
+		--with-libpng=system \
+		--with-zlib=system \
+		--with-vendor-name="Termux" \
 		AR="$AR" \
 		NM="$NM" \
 		OBJCOPY="$OBJCOPY" \
 		OBJDUMP="$OBJDUMP" \
 		STRIP="$STRIP" \
 		CXXFILT="llvm-cxxfilt" \
-		BUILD_CC="$_llvm_base_path/bin/clang" \
-		BUILD_CXX="$_llvm_base_path/bin/clang++" \
-		BUILD_NM="$_llvm_base_path/bin/llvm-nm" \
-		BUILD_AR="$_llvm_base_path/bin/llvm-ar" \
-		BUILD_OBJCOPY="$_llvm_base_path/bin/llvm-objcopy" \
-		BUILD_STRIP="$_llvm_base_path/bin/llvm-strip"
+		BUILD_CC="$TERMUX_HOST_LLVM_BASE_DIR/bin/clang" \
+		BUILD_CXX="$TERMUX_HOST_LLVM_BASE_DIR/bin/clang++" \
+		BUILD_NM="$TERMUX_HOST_LLVM_BASE_DIR/bin/llvm-nm" \
+		BUILD_AR="$TERMUX_HOST_LLVM_BASE_DIR/bin/llvm-ar" \
+		BUILD_OBJCOPY="$TERMUX_HOST_LLVM_BASE_DIR/bin/llvm-objcopy" \
+		BUILD_STRIP="$TERMUX_HOST_LLVM_BASE_DIR/bin/llvm-strip" \
+		--with-jobs=$TERMUX_PKG_MAKE_PROCESSES
 }
 
 termux_step_make() {
 	cd build/linux-${TERMUX_ARCH/i686/x86}-normal-server-release
-	make JOBS=1 images
+	make images
 }
 
 termux_step_make_install() {
+	rm -rf $TERMUX_PREFIX/lib/jvm/java-11-openjdk
 	mkdir -p $TERMUX_PREFIX/lib/jvm/java-11-openjdk
 	cp -r build/linux-${TERMUX_ARCH/i686/x86}-normal-server-release/images/jdk/* \
 		$TERMUX_PREFIX/lib/jvm/java-11-openjdk/
@@ -90,44 +138,103 @@ termux_step_post_make_install() {
 	for manpage in *.1; do
 		gzip "$manpage"
 	done
+
+	# Make sure that our alternatives file is up to date.
+	binaries="$(find $TERMUX_PREFIX/lib/jvm/java-11-openjdk/bin -executable -type f | xargs -I{} basename "{}" | xargs echo)"
+	manpages="$(find $TERMUX_PREFIX/lib/jvm/java-11-openjdk/man/man1 -name "*.1.gz" | xargs -I{} basename "{}" | xargs echo)"
+
+	local failure=false
+	for binary in $binaries; do
+		grep -q "lib/jvm/java-11-openjdk/bin/${binary}$" "$TERMUX_PKG_BUILDER_DIR"/openjdk-11.alternatives || {
+			echo "ERROR: Missing entry for binary: $binary in openjdk-11.alternatives"
+			failure=true
+		}
+	done
+	for manpage in $manpages; do
+		grep -q "lib/jvm/java-11-openjdk/man/man1/${manpage}$" "$TERMUX_PKG_BUILDER_DIR"/openjdk-11.alternatives || {
+			echo "ERROR: Missing entry for manpage: $manpage in openjdk-11.alternatives"
+			failure=true
+		}
+	done
+	if [[ "$failure" = true ]]; then
+		termux_error_exit "openjdk-11.alternatives is not up to date, please update it."
+	fi
 }
 
 termux_step_create_debscripts() {
-	local binaries="$(find $TERMUX_PREFIX/lib/jvm/java-11-openjdk/bin -executable -type f | xargs -I{} basename "{}" | xargs echo)"
-	local manpages="$(find $TERMUX_PREFIX/lib/jvm/java-11-openjdk/man/man1 -name "*.1.gz" | xargs -I{} basename "{}" | xargs echo)"
-	cat <<-EOF >./postinst
-		#!$TERMUX_PREFIX/bin/sh
-		if [ "$TERMUX_PACKAGE_FORMAT" = "pacman" ] || [ "\$1" = "configure" ] || [ "\$1" = "abort-upgrade" ]; then
-			if [ -x "$TERMUX_PREFIX/bin/update-alternatives" ]; then
-				update-alternatives --install $TERMUX_PREFIX/etc/profile.d/java.sh java-profile	$TERMUX_PREFIX/lib/jvm/java-11-openjdk/etc/profile.d/java.sh 11
-				for tool in $binaries; do
-					update-alternatives --install \
-						$TERMUX_PREFIX/bin/\$tool \$tool \
-						$TERMUX_PREFIX/lib/jvm/java-11-openjdk/bin/\$tool 11
-				done
-
-				for manpage in $manpages; do
-					update-alternatives --install \
-						$TERMUX_PREFIX/share/man/man1/\$manpage \$manpage \
-						$TERMUX_PREFIX/lib/jvm/java-11-openjdk/man/man1/\$manpage 11
-				done
-			fi
-		fi
-	EOF
-
-	cat <<-EOF >./prerm
-		#!$TERMUX_PREFIX/bin/sh
-		if [ "$TERMUX_PACKAGE_FORMAT" = "pacman" ] || [ "\$1" != "upgrade" ]; then
-			if [ -x "$TERMUX_PREFIX/bin/update-alternatives" ]; then
-				update-alternatives --remove java-profile $TERMUX_PREFIX/etc/profile.d/java.sh
-				for tool in $binaries; do
-					update-alternatives --remove \$tool $TERMUX_PREFIX/bin/\$tool
-				done
-
-				for manpage in $manpages; do
-					update-alternatives --remove \$manpage $TERMUX_PREFIX/share/man/man1/\$manpage
-				done
-			fi
-		fi
-	EOF
+	# For older versions of openjdk-11, openjdk-17 and openjdk-21, we used to provide different alternatives for each binary and manpage.
+	# This script removes those alternatives if the user is upgrading from an older version.
+	#
+	# Using slaves for all binaries and manpages makes it much easier to switch between different versions of openjdk.
+	local old_alternatives=(
+		java-profile
+		jaotc
+		jar
+		jarsigner
+		javac
+		javadoc
+		javap
+		jcmd
+		jconsole
+		jdb
+		jdeprscan
+		jdeps
+		jfr
+		jhsdb
+		jimage
+		jinfo
+		jjs
+		jlink
+		jmap
+		jmod
+		jps
+		jrunscript
+		jshell
+		jstack
+		jstat
+		jstatd
+		keytool
+		pack200
+		rmic
+		rmid
+		rmiregistry
+		serialver
+		unpack200
+		jar.1.gz
+		jarsigner.1.gz
+		java.1.gz
+		javac.1.gz
+		javadoc.1.gz
+		javap.1.gz
+		jcmd.1.gz
+		jconsole.1.gz
+		jdb.1.gz
+		jdeps.1.gz
+		jinfo.1.gz
+		jjs.1.gz
+		jmap.1.gz
+		jps.1.gz
+		jrunscript.1.gz
+		jstack.1.gz
+		jstat.1.gz
+		jstatd.1.gz
+		keytool.1.gz
+		pack200.1.gz
+		rmic.1.gz
+		rmid.1.gz
+		rmiregistry.1.gz
+		serialver.1.gz
+		unpack200.1.gz
+	)
+	# For older versions
+	echo 'if [ "$#" = "3" ] && dpkg --compare-versions "$2" le "11.0.24"; then' > ./preinst
+	echo '  echo "Removing older alternatives for openjdk-21, openjdk-17 and openjdk-11"' >> ./preinst
+	echo '  echo "This may take a while if mandoc package is installed, please wait..."' >> ./preinst
+	echo '  echo "Newer versions of openjdk-21, openjdk-17 and openjdk-11 change how alternatives are handled."' >> ./preinst
+	echo '  echo "Instead of having different alternatives for each manpage and binary, now you can switch java versions much easily using \"update-alternatives --config java\""' >> ./preinst
+	echo '  echo "This should switch all java binaries, manpages, and bash profile for java in a single command instead of switching everything manually"' >> ./preinst
+	for alternative in "${old_alternatives[@]}"; do
+		echo "  update-alternatives --remove-all ${alternative} || :" >> ./preinst
+	done
+	echo 'fi' >> ./preinst
 }
