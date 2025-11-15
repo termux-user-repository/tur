68599df1e2a809c48b32dfea09ed723cbc3057ab
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-11-15 23:56:44 +0800

    filebrowser: bump to 2.45.3 (#2051)

--- tur/filebrowser/build.sh
+++ tur/filebrowser/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://filebrowser.org/
 TERMUX_PKG_DESCRIPTION="Web file browser based on Go"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="2.45.1"
+TERMUX_PKG_VERSION="2.45.3"
 TERMUX_PKG_SRCURL="https://github.com/filebrowser/filebrowser/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=8c1076ee39ecd01f4e887f5d3d8593f4ae3d75f0cfcdded56cbc3d773e2f07a0
+TERMUX_PKG_SHA256=433a79deb0bca31cabfb17e5517728a32dd6692070e520f9beb8a10659e3e0c6
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_HOSTBUILD=true
@@ -30,12 +30,12 @@ termux_step_configure() {
 }
 
 termux_step_make() {
+	(cd frontend && pnpm install --frozen-lockfile && pnpm run build)
 	local _commit_hash=$(
 		curl https://api.github.com/repos/filebrowser/filebrowser/git/ref/tags/v$TERMUX_PKG_VERSION \
 		| jq -r .object.sha
 	)
 	local _module_name=$(env GO111MODULE=on go list -m)
-	make build-frontend
 	go build -ldflags="-X $_module_name/version.Version=$TERMUX_PKG_VERSION -X $_module_name/version.CommitSHA=$_commit_hash"
 }
 
