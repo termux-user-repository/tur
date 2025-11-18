641800b8783c89ef3bab122ba5f9a8b044d7ac51
Robert Kirkman <rkirkman@termux.dev>
2025-11-18 17:01:22 -0600

    addpkg(tur/rumdl): 0.0.179
    - Fixes https://github.com/termux-user-repository/tur/issues/2054

---
+++ tur/rumdl/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/rvben/rumdl
+TERMUX_PKG_DESCRIPTION="Markdown Linter and Formatter written in Rust"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="0.0.179"
+TERMUX_PKG_SRCURL="https://github.com/rvben/rumdl/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=80ad7bc887bd2feedb906fc6121739563aaf8402f86a7d360bebfd4c2f778363
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	termux_setup_rust
+}
+
+termux_step_make() {
+	cargo build --jobs "$TERMUX_PKG_MAKE_PROCESSES" --target "$CARGO_TARGET_NAME" --release
+}
+
+termux_step_make_install() {
+	install -Dm755 -t "$TERMUX_PREFIX/bin" "target/$CARGO_TARGET_NAME/release/$TERMUX_PKG_NAME"
+}
