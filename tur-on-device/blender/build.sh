bd19af8e0e5b76f4c16b34cfae12e5eb8e8eb3bb
Robert Kirkman <rkirkman@termux.dev>
2025-11-18 06:32:13 -0600

    fix(tur-on-device/blender): switch to `ffmpeg7`
    - Unlike the case of package `audacity`, this is believed to work
    
    - It is too hard to make Blender 3.6 work with FFmpeg 8, but this works

--- tur-on-device/blender/build.sh
+++ tur-on-device/blender/build.sh
@@ -5,11 +5,11 @@ TERMUX_PKG_DESCRIPTION="A fully integrated 3D graphics creation suite (legacy ve
 TERMUX_PKG_LICENSE="GPL-3.0-or-later"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=1:3.6.23
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=git+https://projects.blender.org/blender/blender
 # Blender does not support 32-bit
 TERMUX_PKG_EXCLUDED_ARCHES="arm, i686"
-TERMUX_PKG_DEPENDS="alembic, boost, brotli, clang, desktop-file-utils, draco, ffmpeg, fftw, freetype, glew, hicolor-icon-theme, imath, libandroid-execinfo, libandroid-posix-semaphore, libblosc, libc++, libepoxy, libharu, libllvm, libpng, libpugixml, libraw, libsndfile, libspnav, libtbb, libtiff, libwebp, libx11, libxfixes, libxi, libxkbcommon, libyaml-cpp, oidn, openal-soft, opencolorio, openexr, openimageio, openjpeg, openpgl, openshadinglanguage, opensubdiv, openvdb, openxr, potrace, ptex, python, python-numpy, python-pip, shaderc, shared-mime-info, usd, zlib, zstd"
+TERMUX_PKG_DEPENDS="alembic, boost, brotli, clang, desktop-file-utils, draco, ffmpeg7, fftw, freetype, glew, hicolor-icon-theme, imath, libandroid-execinfo, libandroid-posix-semaphore, libblosc, libc++, libepoxy, libharu, libllvm, libpng, libpugixml, libraw, libsndfile, libspnav, libtbb, libtiff, libwebp, libx11, libxfixes, libxi, libxkbcommon, libyaml-cpp, oidn, openal-soft, opencolorio, openexr, openimageio, openjpeg, openpgl, openshadinglanguage, opensubdiv, openvdb, openxr, potrace, ptex, python, python-numpy, python-pip, shaderc, shared-mime-info, usd, zlib, zstd"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers, git-lfs, mold, sse2neon"
 TERMUX_PKG_PYTHON_COMMON_DEPS="requests"
 # do not enable WITH_CYCLES_NATIVE_ONLY - results in crashing when opening the Edit->Preferences->System menu on some devices
@@ -27,6 +27,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DWITH_JACK=OFF
 -DWITH_LINKER_MOLD=ON
 -DWITH_MATERIALX=ON
+-DCMAKE_PREFIX_PATH=$TERMUX_PREFIX/opt/ffmpeg7
 "
 TERMUX_PKG_RM_AFTER_INSTALL="
 lib/python*
@@ -46,6 +47,8 @@ termux_step_pre_configure() {
 	LDFLAGS+=" -Wl,--undefined-version"
 	# error: undefined symbol: llvm::Triple::getEnvironmentVersionString() const
 	LDFLAGS+=" -lLLVM -lclang-cpp"
+
+	LDFLAGS+=" -Wl,-rpath=$TERMUX_PREFIX/opt/ffmpeg7/lib"
 
 	# Fetch addons
 	local _ARCH
