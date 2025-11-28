cd3f82cd8805d365a03ec08f529c682cb508c0ba
Robert Kirkman <rkirkman@termux.dev>
2025-11-28 02:32:17 -0600

    revbump(tur/itstool): for `libxml2` 2.15

--- tur/itstool/build.sh
+++ tur/itstool/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Translate XML with PO files using W3C Internationalizati
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=2.0.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://files.itstool.org/itstool/itstool-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=6b9a7cd29a12bb95598f5750e8763cee78836a1a207f85b74d8b3275b27e87ca
 TERMUX_PKG_DEPENDS="libxml2-python"
