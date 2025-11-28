c54f737445e504e267897743d416d2722527b406
Robert Kirkman <rkirkman@termux.dev>
2025-11-28 02:38:46 -0600

    revbump(tur/libmongoc): for `libicu` 78

--- tur/libmongoc/build.sh
+++ tur/libmongoc/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="Apache-2.0, custom"
 TERMUX_PKG_LICENSE_FILE="COPYING, THIRD_PARTY_NOTICES, src/libmongoc/THIRD_PARTY_NOTICES"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=1.30.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/mongodb/mongo-c-driver/releases/download/$TERMUX_PKG_VERSION/mongo-c-driver-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9f42eb507e8c591546dc9c584230a6f71127842cb597cc4ab219d1ebe251e7af
 TERMUX_PKG_DEPENDS="libbson, libicu, libmongocrypt, libsasl, libsnappy, openssl, zlib, zstd"
