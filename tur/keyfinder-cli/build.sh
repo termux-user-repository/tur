c014d1ea484ada87d9c992c0594282b480af3504
flos <flosnvjx@users.noreply.github.com>
2025-12-05 16:21:04 +0800

    new package: keyfinder-cli

---
+++ tur/keyfinder-cli/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/evanpurkhiser/keyfinder-cli"
+TERMUX_PKG_DESCRIPTION="Utility to estimate the key in digital music recordings"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@flosnvjx"
+TERMUX_PKG_VERSION=1.1.5
+TERMUX_PKG_SRCURL="https://github.com/evanpurkhiser/keyfinder-cli/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=SKIP_CHECKSUM
+TERMUX_PKG_DEPENDS="libkeyfinder,ffmpeg,fftw"
