280f886ea115136e6b62c406a98b8417095fda15
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-12-06 14:32:04 +0800

    afrog: bump to 3.2.6 (#2094)

--- tur-hacking/afrog/build.sh
+++ tur-hacking/afrog/build.sh
@@ -2,11 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://github.com/zan8in/afrog
 TERMUX_PKG_DESCRIPTION="A tool for finding vulnerabilities"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@UtermuxBlog"
-TERMUX_PKG_VERSION="3.2.5"
+TERMUX_PKG_VERSION="3.2.6"
 TERMUX_PKG_SRCURL=https://github.com/zan8in/afrog/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=68521dac484636bb8d348db62ff35bf220ebb1d7676e0ae43b8f10104f3a3bec
+TERMUX_PKG_SHA256=512e50267c09f9d929f241620fd3587759a9162697e489a023437dc4bb56b623
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_post_get_source() {
+	curl -L \
+		"https://github.com/zan8in/afrog/commit/e01cc41151b94a987b8b3d0327a4dae355dbd4ce.patch" \
+		-o "$TERMUX_PKG_SRCDIR/web.patch"
+	cd "$TERMUX_PKG_SRCDIR"
+	git apply "$TERMUX_PKG_SRCDIR/web.patch"
+}
 
 termux_step_pre_configure() {
 	termux_setup_golang
