fdfcd5ed9c54dee8c615a9c426200b625befa6c0
Jia Yuan Lo <jylo06g@gmail.com>
2025-12-08 14:08:00 +0800

    fix(tur/rustc-nightly): sync patch from main repo (#2103)

--- tur/rustc-nightly/build.sh
+++ tur/rustc-nightly/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Rust compiler and utilities (nightly version)"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="1.94.0-2025.12.07-nightly"
+TERMUX_PKG_REVISION=1
 _RUST_VERSION=$(echo $TERMUX_PKG_VERSION | cut -d- -f1)
 _DATE="$(echo $TERMUX_PKG_VERSION | cut -d- -f2 | sed 's|\.|-|g')"
 _LLVM_MAJOR_VERSION=$(. $TERMUX_SCRIPTDIR/packages/libllvm/build.sh; echo $LLVM_MAJOR_VERSION)
