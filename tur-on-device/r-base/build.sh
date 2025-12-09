63d8a38266e532b0b068b421d5bf4b280beca0e7
Robert Kirkman <rkirkman@termux.dev>
2025-12-09 14:51:09 -0600

    bump(tur-on-device/r-base): 4.5.2
    - Fixes https://github.com/termux-user-repository/tur/issues/2101

--- tur-on-device/r-base/build.sh
+++ tur-on-device/r-base/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.r-project.org/
 TERMUX_PKG_DESCRIPTION="A free software environment for statistical computing and graphics"
 TERMUX_PKG_LICENSE="GPL-2.0-or-later, LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.4.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=4.5.2
 TERMUX_PKG_SRCURL=https://cran.r-project.org/src/base/R-${TERMUX_PKG_VERSION::1}/R-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=ace4125f9b976d2c53bcc5fca30c75e30d4edc401584859cbadb080e72b5f030
+TERMUX_PKG_SHA256=0d71ff7106ec69cd7c67e1e95ed1a3cee355880931f2eb78c530014a9e379f20
 TERMUX_PKG_DEPENDS="libandroid-glob, libiconv, libbz2, libcurl, liblzma, pcre2, readline, zlib"
 TERMUX_PKG_BUILD_DEPENDS="binutils, gcc-15, openjdk-21, which"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -14,6 +13,8 @@ ac_cv_have_decl_wctrans=yes
 --enable-R-shlib
 --with-readline=yes
 "
+# prevents 'duping jobs pipe: Bad file descriptor.  Stop.''
+TERMUX_PKG_MAKE_PROCESSES=1
 
 termux_step_pre_configure() {
 	if [ "${TERMUX_ON_DEVICE_BUILD}" = false ]; then
