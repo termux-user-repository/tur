fb34039c581c27e8990abf885a9970581da0f6e7
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-12-19 05:43:49 +0000

    keyfinder: move to main
    [skip ci]

--- tur/libkeyfinder/build.sh
+++
@@ -1,8 +0,0 @@
-TERMUX_PKG_HOMEPAGE="https://github.com/mixxxdj/libkeyfinder"
-TERMUX_PKG_DESCRIPTION="Library to estimate the key in digital music recordings"
-TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_MAINTAINER="@flosnvjx"
-TERMUX_PKG_VERSION=2.2.8
-TERMUX_PKG_SRCURL="https://github.com/mixxxdj/libkeyfinder/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=SKIP_CHECKSUM
-TERMUX_PKG_DEPENDS="fftw"
