e8ee7f64b798968eeba3cf65216ef97f1265800f
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-12-21 22:31:42 +0800

    mdds: fix auto-update
    [skip ci]

--- tur/mdds/build.sh
+++ tur/mdds/build.sh
@@ -6,15 +6,3 @@ TERMUX_PKG_VERSION="3.2.0"
 TERMUX_PKG_SRCURL=https://gitlab.com/api/v4/projects/mdds%2Fmdds/packages/generic/source/${TERMUX_PKG_VERSION}/mdds-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=16defab556c53f94b463460ea2a1faab96e58bad2a95d7981300d2956534abbb
 TERMUX_PKG_AUTO_UPDATE=true
-
-termux_pkg_auto_update() {
-	local latest_tag
-	latest_tag="$(
-		termux_gitlab_api_get_tag \
-			"${TERMUX_PKG_HOMEPAGE}" "${TERMUX_PKG_UPDATE_TAG_TYPE}" "${TERMUX_GITLAB_API_HOST}"
-	)"
-	if [[ -z "${latest_tag}" ]]; then
-		termux_error_exit "ERROR: Unable to get tag from ${TERMUX_PKG_HOMEPAGE}"
-	fi
-	termux_pkg_upgrade_version "${latest_tag}"
-}
