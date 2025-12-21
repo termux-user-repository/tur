b793f7dfc7f43770027968350539edbbc5cabf52
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-12-21 22:30:19 +0800

    smartdns: bump to 47.1

--- tur/smartdns/build.sh
+++ tur/smartdns/build.sh
@@ -2,25 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://github.com/pymumu/smartdns
 TERMUX_PKG_DESCRIPTION="A local DNS server to obtain the fastest website IP for the best Internet experience, support DoT, DoH"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=47
+TERMUX_PKG_VERSION=47.1
 TERMUX_PKG_SRCURL=https://github.com/pymumu/smartdns/archive/refs/tags/Release$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=90414fc46de16bdec69a8511d78616287db17c5cc20034c210978896dfa7530a
+TERMUX_PKG_SHA256=0a143ee81ccb7a31b7b7b0c29d6a6ee41a54331da75477719925592af124ec97
 TERMUX_PKG_DEPENDS="libandroid-glob, libc++, openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+"
-
-termux_pkg_auto_update() {
-	# Get the newest tag:
-	local tag
-	tag="$(termux_github_api_get_tag "${TERMUX_PKG_SRCURL}" "${TERMUX_PKG_UPDATE_TAG_TYPE}")"
-	# check if this is not a release:
-	if grep -qP "^Release${TERMUX_PKG_UPDATE_VERSION_REGEXP}\$" <<<"$tag"; then
-		termux_pkg_upgrade_version "$tag"
-	else
-		echo "WARNING: Skipping auto-update: Not a release($tag)"
-	fi
-}
+TERMUX_PKG_UPDATE_VERSION_SED_REGEXP='s/Release//'
 
 termux_step_configure() {
 	LDFLAGS+=" -landroid-glob"
