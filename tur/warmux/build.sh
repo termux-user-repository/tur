dcf9338a083af24156359a5dc0bfe2d091aba57e
Fervi (IntinteDAO) <lordfervi@protonmail.com>
2026-01-05 23:03:04 +0100

    Warmux: Small fixes

--- tur/warmux/build.sh
+++ tur/warmux/build.sh
@@ -1,11 +1,12 @@
 TERMUX_PKG_HOMEPAGE=http://www.warmux.org
 TERMUX_PKG_DESCRIPTION="Worms-like game"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_MAINTAINER="@IntinteDAO"
 TERMUX_PKG_VERSION=11.04.1
 TERMUX_PKG_SRCURL=https://www.warmux.org/warmux-11.04.1.tar.bz2
 TERMUX_PKG_SHA256=789c4f353e4c5ce0a2aba2e82861d3fd0e5218bc76d8da1a332f2c7b1b27e4ee
 TERMUX_PKG_DEPENDS="libcurl, libpng, libxml2, sdl, sdl-gfx, sdl-image, sdl-mixer, sdl-net, sdl-ttf"
+TERMUX_PKG_GROUPS="games"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-sdl-prefix=$TERMUX_PREFIX
 --with-libcurl=$TERMUX_PREFIX
