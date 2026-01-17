c57937d8eea68e551d16ddd14b1c315c9d935307
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2026-01-17 14:42:55 +0800

    chromium-beta-host-tools: bump to 145.0.7632.5 (#2175)

--- tur/chromium-beta-host-tools/build.sh
+++ tur/chromium-beta-host-tools/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.chromium.org/Home
 TERMUX_PKG_DESCRIPTION="Chromium web browser (Host tools)"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@licy183"
-TERMUX_PKG_VERSION=144.0.7559.59
+TERMUX_PKG_VERSION=145.0.7632.5
 TERMUX_PKG_SRCURL=https://commondatastorage.googleapis.com/chromium-browser-official/chromium-$TERMUX_PKG_VERSION-lite.tar.xz
-TERMUX_PKG_SHA256=8d6de5bbf447b9d0f3c99843230ffebe5954cbcb38d9f8c2ddbbeb5fbcfa9179
+TERMUX_PKG_SHA256=455eb8c447164e54840b1a1324be0d49a904783b903597635a8785a7f63c1b4a
 TERMUX_PKG_DEPENDS="atk, cups, dbus, fontconfig, gtk3, krb5, libc++, libevdev, libxkbcommon, libminizip, libnss, libx11, mesa, openssl, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_DEPENDS="libffi-static"
 # TODO: Split chromium-common and chromium-headless
@@ -130,6 +130,38 @@ EOF
 	# Install nodejs
 	if [ ! -f "third_party/node/linux/node-linux-x64/bin/node" ]; then
 		./third_party/node/update_node_binaries
+	fi
+
+	# Download npm deps
+	if [ ! -d "third_party/node/node_modules" ]; then
+		local _npm_object_name=$(python -c "Var = str
+Str = str
+exec(open('$TERMUX_PKG_SRCDIR/DEPS').read())
+print(deps['src/third_party/node/node_modules']['objects'][0]['object_name'])
+")
+		local _npm_sha256sum=$(python -c "Var = str
+Str = str
+exec(open('$TERMUX_PKG_SRCDIR/DEPS').read())
+print(deps['src/third_party/node/node_modules']['objects'][0]['sha256sum'])
+")
+		local _npm_file="$TERMUX_PKG_SRCDIR/third_party/node/node_modules.tar.gz"
+		termux_download \
+			"https://commondatastorage.googleapis.com/chromium-nodejs/$_npm_object_name" \
+			"${_npm_file}" \
+			"${_npm_sha256sum}"
+		mkdir -p $TERMUX_PKG_SRCDIR/third_party/node/node_modules-tmp
+		tar -xf "$_npm_file" --strip-components=1 -C "$TERMUX_PKG_SRCDIR/third_party/node/node_modules-tmp"
+		mv "$TERMUX_PKG_SRCDIR/third_party/node/node_modules-tmp" "$TERMUX_PKG_SRCDIR/third_party/node/node_modules"
+	fi
+
+	# Sync rollup-related native deps in devtools
+	if [ ! -d "third_party/devtools-frontend/src/node_modules/@rollup/rollup-linux-x64-gnu" ]; then
+		if [ ! -f "third_party/devtools-frontend/src/third_party/rollup_libs/rollup.linux-x64-gnu.node" ]; then
+			termux_error_exit "rollup.linux-x64-gnu.node not found"
+		fi
+		pushd third_party/devtools-frontend/src
+		python3 scripts/deps/sync_rollup_libs.py
+		popd # third_party/devtools-frontend/src
 	fi
 
 	local CARGO_TARGET_NAME="${TERMUX_ARCH}-linux-android"
