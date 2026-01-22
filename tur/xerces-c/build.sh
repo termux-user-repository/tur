662bcf2bbc59ebb953410e5139816ce4a4664f4b
Fervi (IntinteDAO) <lordfervi@protonmail.com>
2026-01-22 14:47:17 +0100

    Fix xerces-c build on ARM

--- tur/xerces-c/build.sh
+++ tur/xerces-c/build.sh
@@ -5,3 +5,10 @@ TERMUX_PKG_MAINTAINER="@IntinteDAO"
 TERMUX_PKG_VERSION=3.3.0
 TERMUX_PKG_SRCURL=https://archive.apache.org/dist/xerces/c/3/sources/xerces-c-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9555f1d06f82987fbb4658862705515740414fd34b4db6ad2ed76a2dc08d3bde
+TERMUX_PKG_DEPENDS="iconv, libicu"
+
+termux_step_pre_configure() {
+	if [ "$TERMUX_ARCH" = "arm" ]; then
+		LDFLAGS+=" $($CC -print-libgcc-file-name)"
+	fi
+}
