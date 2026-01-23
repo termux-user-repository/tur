00b85a1018d8045f9bcc14f670598e52cf4d15d5
Fervi (IntinteDAO) <lordfervi@protonmail.com>
2026-01-23 13:41:48 +0100

    addpkg(tur/tuxfootball): 0.3.1

---
+++ tur/tuxfootball/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=http://tuxfootball.sourceforge.net/
+TERMUX_PKG_DESCRIPTION="A 2D football game reminiscent of Sensible Soccer and Kick Off"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@IntinteDAO"
+TERMUX_PKG_VERSION=0.3.1
+TERMUX_PKG_SRCURL=http://sourceforge.net/projects/tuxfootball/files/0.3/tuxfootball-${TERMUX_PKG_VERSION}.tar.gz/download
+TERMUX_PKG_SHA256=44056c15572c2a3f0e3794719961915af15fef5f05596d2ef3f9e247f8a1f3e5
+TERMUX_PKG_DEPENDS="gettext, libiconv, sdl, sdl-image, sdl-mixer"
+TERMUX_PKG_GROUPS="games"
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -liconv"
+}
