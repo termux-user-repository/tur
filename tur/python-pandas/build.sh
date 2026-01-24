381c564b74b00f3d7b54b57eb2ae440dbf872657
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2026-01-25 01:24:15 +0800

    python-pandas: bump to 3.0.0

--- tur/python-pandas/build.sh
+++ tur/python-pandas/build.sh
@@ -3,31 +3,53 @@ TERMUX_PKG_HOMEPAGE=https://pandas.pydata.org/
 TERMUX_PKG_DESCRIPTION="Powerful Python data analysis toolkit"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="2.3.3"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="3.0.0"
 TERMUX_PKG_SRCURL=git+https://github.com/pandas-dev/pandas
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, python, python-numpy, python-pip"
 _NUMPY_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python-numpy/build.sh; echo $TERMUX_PKG_VERSION)
-TERMUX_PKG_PYTHON_COMMON_BUILD_DEPS="'Cython==3.0.5', 'numpy==$_NUMPY_VERSION', wheel, versioneer"
-TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_PYTHON_COMMON_BUILD_DEPS="wheel, Cython, meson-python, build, versioneer"
+TERMUX_PKG_PYTHON_CROSS_BUILD_DEPS="'numpy==$_NUMPY_VERSION'"
+TERMUX_PKG_ON_DEVICE_BUILD_NOT_SUPPORTED=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 
+TERMUX_MESON_WHEEL_CROSSFILE="$TERMUX_PKG_TMPDIR/wheel-cross-file.txt"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--cross-file $TERMUX_MESON_WHEEL_CROSSFILE
+"
+
+termux_step_post_get_source() {
+	local _commit="$(git rev-parse HEAD)"
+	cat << EOF > _version_meson.py
+__version__="$TERMUX_PKG_VERSION"
+__git_version__="$_commit"
+EOF
+}
+
 termux_step_pre_configure() {
-	CFLAGS="-I$TERMUX_PYTHON_HOME/site-packages/numpy/_core/include $CFLAGS"
-	CPPFLAGS="-I$TERMUX_PYTHON_HOME/site-packages/numpy/_core/include $CPPFLAGS"
-	CXXFLAGS="-I$TERMUX_PYTHON_HOME/site-packages/numpy/_core/include $CXXFLAGS"
 	LDFLAGS+=" -lm"
 }
 
 termux_step_configure() {
-	:
+	termux_setup_meson
+
+	cp -f $TERMUX_MESON_CROSSFILE $TERMUX_MESON_WHEEL_CROSSFILE
+	sed -i 's|^\(\[binaries\]\)$|\1\npython = '\'$(command -v python)\''|g' \
+		$TERMUX_MESON_WHEEL_CROSSFILE
+	sed -i 's|^\(\[properties\]\)$|\1\nnumpy-include-dir = '\'$TERMUX_PYTHON_HOME/site-packages/numpy/_core/include\''|g' \
+		$TERMUX_MESON_WHEEL_CROSSFILE
+
+	(unset PYTHONPATH && termux_step_configure_meson)
 }
 
 termux_step_make() {
-	python setup.py bdist_wheel -vvv
+	pushd $TERMUX_PKG_SRCDIR
+	PYTHONPATH= python -m build -w -n -x --config-setting builddir=$TERMUX_PKG_BUILDDIR .
+	popd
 }
 
 termux_step_make_install() {
-	pip install --force-reinstall ./dist/*.whl --no-deps --prefix=$PREFIX -vvv
+	local _pyv="${TERMUX_PYTHON_VERSION/./}"
+	local _whl="pandas-${TERMUX_PKG_VERSION}-cp$_pyv-cp$_pyv-linux_$TERMUX_ARCH.whl"
+	pip install --force-reinstall --no-deps --prefix=$TERMUX_PREFIX $TERMUX_PKG_SRCDIR/dist/$_whl
 }
