2047bc40edeea430452795216d1124bfabbdaba9
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2026-01-25 00:56:00 +0800

    iamb: bump to 0.0.11 (#2203)

--- tur/iamb/build.sh
+++ tur/iamb/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://iamb.chat/
 TERMUX_PKG_DESCRIPTION="A terminal-based client for Matrix for the Vim addict"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="0.0.10"
+TERMUX_PKG_VERSION="0.0.11"
 TERMUX_PKG_SRCURL="https://github.com/ulyssa/iamb/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=f628cfbd9eba9e8881902b970e9432fec815044ec9bea901a8562ea3ef8f4615
+TERMUX_PKG_SHA256=a5cf4f248e0893b5657c5ad1234207c09968018c5462d4063c096f0db459dd7c
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
@@ -15,13 +15,21 @@ termux_step_pre_configure() {
 		RUSTFLAGS+=" -C link-arg=$($CC -print-libgcc-file-name)"
 	fi
 
-	: "${CARGO_HOME:=$HOME/.cargo}"
-	export CARGO_HOME
+	# https://github.com/termux-user-repository/tur/pull/591#discussion_r2701702095
+	cargo vendor
+	find ./vendor \
+		-mindepth 1 -maxdepth 1 -type d \
+		! -wholename ./vendor/arboard \
+		! -wholename ./vendor/x11rb-protocol \
+		-exec rm -rf '{}' \;
 
-	rm -rf $CARGO_HOME/registry/src/index.crates.io-*/arboard-*
-	cargo fetch --target "${CARGO_TARGET_NAME}"
+	find vendor/{arboard,x11rb-protocol} -type f -print0 | \
+		xargs -0 sed -i \
+		-e 's|android|disabling_this_because_it_is_for_building_an_apk|g' \
+		-e "s|/tmp|$TERMUX_PREFIX/tmp|g"
 
-	for d in $CARGO_HOME/registry/src/index.crates.io-*/arboard-*/; do
-		patch --silent -p1 -d ${d} < "$TERMUX_PKG_BUILDER_DIR/0001-arboard-dummy-platform.diff"
-	done
+	echo "" >> Cargo.toml
+	echo '[patch.crates-io]' >> Cargo.toml
+	echo "arboard = { path = \"./vendor/arboard\" }" >> Cargo.toml
+	echo "x11rb-protocol = { path = \"./vendor/x11rb-protocol\" }" >> Cargo.toml
 }
