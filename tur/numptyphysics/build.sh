16a4a77df648a667e59563c4d81be3448ca3e053
Fervi (IntinteDAO) <lordfervi@protonmail.com>
2026-01-26 23:05:35 +0100

    Add sdl2 / sdl2-compat

--- tur/numptyphysics/build.sh
+++ tur/numptyphysics/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@IntinteDAO"
 TERMUX_PKG_VERSION=0.3.10
 TERMUX_PKG_SRCURL=https://gitlab.com/thp/numptyphysics/-/archive/0.3.10/numptyphysics-0.3.10.tar.bz2
 TERMUX_PKG_SHA256=f863ba1bae51934bef31984e40ce1b3dbd82672c0bcd705b50185dd40c4f3618
-TERMUX_PKG_DEPENDS="libtinyxml2, fontconfig, sdl2-image, sdl2-ttf"
+TERMUX_PKG_DEPENDS="libtinyxml2, fontconfig, sdl2 | sdl2-compat, sdl2-image, sdl2-ttf"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_GROUPS="games"
 
