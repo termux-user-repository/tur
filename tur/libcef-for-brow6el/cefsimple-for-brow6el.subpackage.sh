4ee3f28687dd58c567c962eec11ad4324adb5c6a
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2026-01-27 22:36:02 +0800

    new package: libcef-for-brow6el

---
+++ tur/libcef-for-brow6el/cefsimple-for-brow6el.subpackage.sh
@@ -0,0 +1,4 @@
+TERMUX_SUBPKG_DESCRIPTION="Sample application for CEF (From libcef-for-brow6el)"
+TERMUX_SUBPKG_INCLUDE="
+opt/libcef-for-brow6el/Release/cefsimple
+"
---
+++ tur/libcef-for-brow6el/libcef-for-brow6el-dev.subpackage.sh
@@ -0,0 +1,4 @@
+TERMUX_SUBPKG_DESCRIPTION="Dev files for CEF (Used by brow6el)"
+TERMUX_SUBPKG_INCLUDE="
+opt/libcef-for-brow6el-dev
+"
