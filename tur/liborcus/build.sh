2a9d7d813aaad82de6f2322f8addc8fedd37c90e
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2026-02-10 22:51:39 +0800

    liborcus: bump to 0.21.0

--- tur/liborcus/build.sh
+++ tur/liborcus/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE="https://gitlab.com/orcus/orcus/"
 TERMUX_PKG_DESCRIPTION="File import filter library for spreadsheet documents."
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="0.20.1"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="0.21.0"
 TERMUX_PKG_SRCURL="https://gitlab.com/orcus/orcus/-/archive/${TERMUX_PKG_VERSION}/orcus-${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=ff2d2dd1fb0625baeeafe8ac1bfffd467b27a880995b5d6c25a21fa2e6d4d0e5
+TERMUX_PKG_SHA256=3b4224cc28d280b0c45a2302aa024f9ad2a79781323ebf14c268819723cb00f1
 TERMUX_PKG_DEPENDS="boost, libc++, libixion, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers, mdds"
 TERMUX_PKG_AUTO_UPDATE=true
@@ -32,10 +31,10 @@ termux_step_post_massage() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
 	local _SOVERSION_GUARD_FILES="
-lib/liborcus-0.20.so
-lib/liborcus-parser-0.20.so
-lib/liborcus-mso-0.20.so
-lib/liborcus-spreadsheet-model-0.20.so
+lib/liborcus-0.21.so
+lib/liborcus-parser-0.21.so
+lib/liborcus-mso-0.21.so
+lib/liborcus-spreadsheet-model-0.21.so
 "
 	local f
 	for f in ${_SOVERSION_GUARD_FILES}; do
