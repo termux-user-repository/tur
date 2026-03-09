d2455c9a7651c6aff47c6963ddc9132eb1ac6eb7
Robert Kirkman <rkirkman@termux.dev>
2026-03-09 06:44:53 -0500

    fix(tur-on-device/openshadinglangauge): mark as depending on `python`
    - Fixes `FileNotFoundError: [Errno 2] No such file or directory: 'python3.12'` because `openshadinglanguage` contains `/data/data/com.termux/files/usr/lib/python3.12/site-packages/oslquery/__init__.py`

--- tur-on-device/openshadinglanguage/build.sh
+++ tur-on-device/openshadinglanguage/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="Advanced shading language for production GI renderers"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="1.15.1.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/AcademySoftwareFoundation/OpenShadingLanguage/releases/download/v$TERMUX_PKG_VERSION/OSL-$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=4083ca1b0afcefc7d62d52a557693a6c5550eb2d1057b3afb15a1a101a3389fa
-TERMUX_PKG_DEPENDS="freetype, imath, libc++, libpng, libtiff, ncurses, openimageio, qt6-qtbase, zlib"
+TERMUX_PKG_DEPENDS="freetype, imath, libc++, libpng, libtiff, ncurses, openimageio, python, qt6-qtbase, zlib"
 # OSL does not support 32-bit
 TERMUX_PKG_EXCLUDED_ARCHES="arm, i686"
 TERMUX_PKG_RM_AFTER_INSTALL="
