353bb229ce027355951da3b968853447abd947dc
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2026-05-25 11:04:21 +0000

    rustc-nightly: bump to 1.97.0-2026.05.19-nightly

---
+++ tur/rustc-nightly/rust-nightly-std-wasm32v1-none.subpackage.sh
@@ -0,0 +1,6 @@
+TERMUX_SUBPKG_DESCRIPTION="Rust std for target wasm32v1-none (nightly version)"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=false
+TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
+TERMUX_SUBPKG_INCLUDE="
+opt/rust-nightly/lib/rustlib/wasm32v1-none
+"
