666a159ec174deedaf7e1d58535bf3231b285149
Chongyun Lee <licy183@termux.dev>
2026-05-26 23:46:49 +0800

    wine-staging: bump to 11.9

--- tur/wine-staging/build.sh
+++ tur/wine-staging/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="A compatibility layer for running Windows programs"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_LICENSE_FILE="LICENSE, LICENSE.OLD, COPYING.LIB"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="11.8"
+TERMUX_PKG_VERSION="11.9"
 _REAL_VERSION="${TERMUX_PKG_VERSION/\~/-}"
 _VERSION_FOLDER="$(test "${_REAL_VERSION:3:1}" = 0 && echo ${_REAL_VERSION:0:4} || echo ${_REAL_VERSION:0:3}x)"
 TERMUX_PKG_SRCURL=(
@@ -11,8 +11,8 @@ TERMUX_PKG_SRCURL=(
 	https://github.com/wine-staging/wine-staging/archive/v$_REAL_VERSION.tar.gz
 )
 TERMUX_PKG_SHA256=(
-	53aa85995d4b97f0116a1c56b8a6a1417730ef59a277819d2d3d31364ea556b0
-	843a2670067befab0d27cfd268251f0e40047f55b78f326ce9ae5c717e0eeb05
+	e39cc18db287358a2436f8af498bedc7e444d55eb99ef545b60e778bb4ea0f6f
+	fd953f5d78473a58f77140912b57bcdd60a78a7982cac919bf62988be8d514fa
 )
 TERMUX_PKG_DEPENDS="fontconfig, freetype, krb5, libandroid-spawn, libc++, libgmp, libgnutls, libxcb, libxcomposite, libxcursor, libxfixes, libxrender, opengl, pulseaudio, sdl2 | sdl2-compat, vulkan-loader, xorg-xrandr"
 TERMUX_PKG_ANTI_BUILD_DEPENDS="sdl2-compat, vulkan-loader"
