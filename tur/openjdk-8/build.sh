274113babdaaa8e8d81931aecd5ffc4d3b89e871
Hernandez <171220766+SrErikCoderx@users.noreply.github.com>
2026-06-18 17:49:08 -0500

    tur/openjdk-8: build from source via android-openjdk_8-build pipeline
    Rewrite build.sh to source-build OpenJDK 8 inside TUR CI instead of
    downloading pre-built .deb binaries. Runs the existing
    ci_build_arch_*.sh pipeline with TUR_BUILD=1 to skip the final
    dpkg-deb --build step, then copies debdata/ content to TERMUX_PREFIX.

--- tur/openjdk-8/build.sh
+++ tur/openjdk-8/build.sh
@@ -3,46 +3,32 @@ TERMUX_PKG_DESCRIPTION="Java 8 JDK and JRE for Android/Termux (OpenJDK 8)"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@SrErikCoderx"
 TERMUX_PKG_VERSION="8.0.502"
-TERMUX_PKG_REVISION=4
-TERMUX_PKG_SKIP_SRC_EXTRACT=true
-TERMUX_PKG_HAS_DEBUG=false
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/SrErikCoderx/android-openjdk_8-build/archive/refs/tags/v8u502-build4.tar.gz
+TERMUX_PKG_SHA256=0da4a7cb3f26cce49c72547f52cafd7bb08afcda72f1d2d3811c6da0974570a9
+TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STRIP=true
 TERMUX_PKG_NO_ELF_CLEANER=true
 TERMUX_PKG_DEPENDS="freetype"
 TERMUX_PKG_RECOMMENDS="alsa-plugins, libandroid-shmem, libandroid-spawn, libiconv, libjpeg-turbo, littlecms, zlib"
 
-TERMUX_PKG_UNDEF_SYMBOLS_FILES="*/libsaproc.so"
-_OPENJDK8_TAG=v8u502-build4
+termux_step_make() {
+	cd "$TERMUX_PKG_SRCDIR"
+	export TUR_BUILD=1
+	sed -i '/dpkg-deb --build/s/^/# /' debpack.sh
+	case "$TERMUX_ARCH" in
+		arm)   bash ci_build_arch_aarch32.sh ;;
+		i686)  bash ci_build_arch_x86.sh ;;
+		*)     bash ci_build_arch_${TERMUX_ARCH}.sh ;;
+	esac
+}
 
 termux_step_make_install() {
-	local deb_arch="$TERMUX_ARCH"
-	local deb_url="https://github.com/SrErikCoderx/android-openjdk_8-build/releases/download/${_OPENJDK8_TAG}/openjdk-8_${TERMUX_PKG_VERSION}_${deb_arch}.deb"
-	local deb_file="$TERMUX_PKG_CACHEDIR/openjdk-8_${TERMUX_PKG_VERSION}_${deb_arch}.deb"
-	local extract_dir="$TERMUX_PKG_TMPDIR/openjdk8-extract"
-
-	termux_download "$deb_url" "$deb_file" SKIP_CHECKSUM
-
-	rm -rf "$extract_dir"
-	mkdir -p "$extract_dir"
-
-	dpkg-deb -x "$deb_file" "$extract_dir"
-	cp -a "$extract_dir/data/data/com.termux/files/usr/." "$TERMUX_PREFIX/"
+	cd "$TERMUX_PKG_SRCDIR"
+	cp -a debdata/data/data/com.termux/files/usr/. "$TERMUX_PREFIX/"
 }
 
 termux_step_create_debscripts() {
-	local deb_arch="$TERMUX_ARCH"
-	local deb_url="https://github.com/SrErikCoderx/android-openjdk_8-build/releases/download/${_OPENJDK8_TAG}/openjdk-8_${TERMUX_PKG_VERSION}_${deb_arch}.deb"
-	local deb_file="$TERMUX_PKG_CACHEDIR/openjdk-8_${TERMUX_PKG_VERSION}_${deb_arch}.deb"
-	local ctrl_dir="$TERMUX_PKG_TMPDIR/openjdk8-ctrl"
-
-	rm -rf "$ctrl_dir"
-	mkdir -p "$ctrl_dir"
-	dpkg-deb -e "$deb_file" "$ctrl_dir"
-
-	if [ -f "$ctrl_dir/postinst" ]; then
-		cp "$ctrl_dir/postinst" ./postinst
-	fi
-	if [ -f "$ctrl_dir/prerm" ]; then
-		cp "$ctrl_dir/prerm" ./prerm
-	fi
+	cp "$TERMUX_PKG_SRCDIR/debdata/DEBIAN/postinst" ./postinst
+	cp "$TERMUX_PKG_SRCDIR/debdata/DEBIAN/prerm"    ./prerm
 }
