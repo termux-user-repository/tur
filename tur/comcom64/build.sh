723e0b0d54965e347f9c0676fd7b4f4adaaa30e0
Stas Sergeev <stsp2@yandex.ru>
2026-06-19 19:17:10 +0300

    comcom64: update thunk-gen path
    Its pkgconfig file was moved to libdir.

--- tur/comcom64/build.sh
+++ tur/comcom64/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="64bit command.com"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@stsp"
 TERMUX_PKG_VERSION=0.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/dosemu2/comcom64/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b1b23d65fd14aa78d11b59937a474f9aad21e52999faa7ae80a481184fa35af5
 TERMUX_PKG_BUILD_DEPENDS="dj64dev, djstub-cross, thunk-gen-cross"
@@ -13,7 +13,7 @@ termux_step_pre_configure() {
 	local _PREFIX_FOR_DJSTUB=${TERMUX_PREFIX}/opt/djstub/cross
 	local _PREFIX_FOR_THUNK_GEN=${TERMUX_PREFIX}/opt/thunk-gen/cross
 	export PATH="$PATH:$_PREFIX_FOR_DJSTUB/bin"
-	export PKG_CONFIG_PATH="${_PREFIX_FOR_THUNK_GEN}/share/pkgconfig"
+	export PKG_CONFIG_PATH="${_PREFIX_FOR_THUNK_GEN}/lib/$TERMUX_HOST_PLATFORM/pkgconfig"
 }
 
 termux_step_make_install() {
