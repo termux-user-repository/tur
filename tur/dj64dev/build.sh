717c7c1789a1bd0fcbe3d5a23649d4a9a2638dff
Stas Sergeev <stsp2@yandex.ru>
2026-06-19 19:19:12 +0300

    dj64: update thunk-gen path
    Its pkgconfig file was moved to libdir.

--- tur/dj64dev/build.sh
+++ tur/dj64dev/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="development suite that allows to cross-build 64-bit prog
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@stsp"
 TERMUX_PKG_VERSION=0.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/stsp/dj64dev/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1ec03bef2f1b028faf727e4be1c30102d46331a366fdaaffc6f2aab4ebcadf4f
 TERMUX_PKG_BUILD_DEPENDS="ctags-cross, thunk-gen-cross, libelf, ncurses-utils, pkg-config"
@@ -16,7 +16,7 @@ termux_step_pre_configure() {
 	local _PREFIX_FOR_CTAGS=${TERMUX_PREFIX}/opt/ctags/cross
 	local _PREFIX_FOR_THUNK_GEN=${TERMUX_PREFIX}/opt/thunk-gen/cross
 	export PATH="$PATH:${_PREFIX_FOR_CTAGS}/bin"
-	export PKG_CONFIG_PATH="${_PREFIX_FOR_THUNK_GEN}/share/pkgconfig"
+	export PKG_CONFIG_PATH="${_PREFIX_FOR_THUNK_GEN}/lib/$TERMUX_HOST_PLATFORM/pkgconfig"
 	cd $TERMUX_PKG_SRCDIR
 	autoreconf -v -i -I m4
 }
