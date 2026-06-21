9a4e066cab319725772837e35ccd04798f6a634c
Natarizki <natakeren890@gmail.com>
2026-06-21 21:21:08 +0700

    updating build.sh

--- tur/glyph/build.sh
+++ tur/glyph/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="Natarizki <natakeren890@gmail.com>"
 TERMUX_PKG_VERSION="1.0.0"
 TERMUX_PKG_SRCURL=https://github.com/Natarizki/glyph/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=85134fce77e16fecc204852070fd8004a066bf04fcab1d38e77ec7327f62f8a0
-TERMUX_PKG_DEPENDS=""
+# TERMUX_PKG_DEPENDS=""
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
