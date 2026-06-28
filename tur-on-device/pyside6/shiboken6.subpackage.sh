f2cc30598b2c16f5f997ac51453b088556860c66
Robert Kirkman <rkirkman@termux.dev>
2026-06-27 23:17:21 -0500

    fix(tur-on-device/shiboken6): mark as `TERMUX_SUBPKG_DEPEND_ON_PARENT=false`
    - Fixes https://github.com/termux-user-repository/tur/issues/2607
    
    - Necessary in order for the dependency on `shiboken6` of `pyside6` to take effect

--- tur-on-device/pyside6/shiboken6.subpackage.sh
+++ tur-on-device/pyside6/shiboken6.subpackage.sh
@@ -1,5 +1,6 @@
 TERMUX_SUBPKG_DESCRIPTION="Generates bindings for C++ using CPython source code"
 TERMUX_PKG_DEPENDS="clang, qt6-qtbase, libxml2, libxslt"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=false
 TERMUX_SUBPKG_INCLUDE="
 */*hiboken*
 */*/*hiboken*
