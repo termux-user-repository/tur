19460faf31e0018826137528f55ad4a3eb8d058c
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2026-07-03 23:45:06 +0800

    memtest-vulkan: rename

--- tur/memtest-vulkan/build.sh
+++ tur/memtest-vulkan/build.sh
@@ -1,11 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/GpuZelenograd/memtest_vulkan
 TERMUX_PKG_DESCRIPTION="Vulkan compute tool for testing video memory stability"
-TERMUX_PKG_LICENSE="Zlib"
+TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="わたあめえ <wataamee777@gmail.com>"
 TERMUX_PKG_VERSION=0.5.0
 TERMUX_PKG_SRCURL=https://github.com/GpuZelenograd/memtest_vulkan/archive/refs/tags/v{TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ec7100c94d92e0d5f5357386f144dc33ec428890f5dc0d7925e9b1f09cb58e5e
-
 TERMUX_PKG_DEPENDS="vulkan-loader-generic"
 TERMUX_PKG_BUILD_IN_SRC=true
 
