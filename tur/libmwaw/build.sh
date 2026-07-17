2161248df343a3cd967cd43618db21d40ca4164a
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2026-07-17 22:37:39 +0530

    revbump(tur/libmwaw): skip Doxygen PNG install
    Co-authored-by: Robert Kirkman <rkirkman@termux.dev>

--- tur/libmwaw/build.sh
+++ tur/libmwaw/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Import library for some old mac text documents."
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION=0.3.22
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/libmwaw/files/libmwaw/libmwaw-${TERMUX_PKG_VERSION}/libmwaw-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=a1a39ffcea3ff2a7a7aae0c23877ddf4918b554bf82b0de5d7ce8e7f61ea8e32
 TERMUX_PKG_DEPENDS="doxygen, librevenge"
@@ -13,6 +14,8 @@ termux_step_pre_configure() {
 	local _libgcc_path="$(dirname $_libgcc_file)"
 	local _libgcc_name="$(basename $_libgcc_file)"
 	LDFLAGS+=" -L$_libgcc_path -l:$_libgcc_name"
+
+	autoreconf -fi
 }
 
 termux_step_post_massage() {
