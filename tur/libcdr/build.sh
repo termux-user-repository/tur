73635590bb5136ffdd10ed9aa6c0a7cd68ea6d90
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2026-07-17 22:26:27 +0530

    bump(tur/libcdr): 0.1.8

--- tur/libcdr/build.sh
+++ tur/libcdr/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://wiki.documentfoundation.org/DLP/Libraries/libcdr
 TERMUX_PKG_DESCRIPTION="CorelDraw file format importer library for LibreOffice"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="0.1.7"
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION="0.1.8"
 TERMUX_PKG_SRCURL="https://dev-www.libreoffice.org/src/libcdr/libcdr-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=5666249d613466b9aa1e987ea4109c04365866e9277d80f6cd9663e86b8ecdd4
+TERMUX_PKG_SHA256=ced677c8300b29c91d3004bb1dddf0b99761bf5544991c26c2ee8f427e87193c
 TERMUX_PKG_DEPENDS="libicu, librevenge, libxml2, littlecms, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost, libwpg, cppunit"
 
@@ -14,6 +13,8 @@ termux_step_pre_configure() {
 	local _libgcc_path="$(dirname $_libgcc_file)"
 	local _libgcc_name="$(basename $_libgcc_file)"
 	LDFLAGS+=" -L$_libgcc_path -l:$_libgcc_name"
+
+	autoreconf -fi
 }
 
 termux_step_post_configure() {
