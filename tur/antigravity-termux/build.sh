0c906e9a19c83ca56b1ae0d52be2790c50574bfb
root <root@localhost.localdomain>
2026-07-27 06:20:37 +0000

    tur/antigravity-termux: fix build failure by generating missing LICENSE file

--- tur/antigravity-termux/build.sh
+++ tur/antigravity-termux/build.sh
@@ -9,8 +9,28 @@ TERMUX_PKG_DEPENDS="proot-distro, curl, tar, coreutils"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
+termux_step_post_get_source() {
+	cat << 'EOF' > LICENSE
+MIT License
+
+Copyright (c) 2026 Javed Ahmed
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO REGARDLESS OF THE FAILURE OF THIS
+SOFTWARE.
+EOF
+}
+
 termux_step_make_install() {
-    mkdir -p $TERMUX_PREFIX/bin
-    cp $TERMUX_PKG_SRCDIR/install-agy.sh $TERMUX_PREFIX/bin/antigravity-termux-setup
-    chmod +x $TERMUX_PREFIX/bin/antigravity-termux-setup
+	install -Dm755 "$TERMUX_PKG_SRCDIR/install-agy.sh" "$TERMUX_PREFIX/bin/antigravity-termux-setup"
 }
