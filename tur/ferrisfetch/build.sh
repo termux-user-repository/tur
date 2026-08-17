243e2f19dc3c97aa7d0bd9c24a5f42405c7bfcb3
Kushagra Kumar <kkushagra86@gmail.com>
2026-08-17 23:57:03 +0530

    chore: update repository references to kk376

--- tur/ferrisfetch/build.sh
+++ tur/ferrisfetch/build.sh
@@ -1,9 +1,9 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/Kk376/ferrisfetch
+TERMUX_PKG_HOMEPAGE=https://github.com/kk376/ferrisfetch
 TERMUX_PKG_DESCRIPTION="A fast, lightweight Linux and Android system information fetch CLI written in Rust"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Kushagra Kumar <kk376@users.noreply.github.com>"
 TERMUX_PKG_VERSION="0.1.0"
-TERMUX_PKG_SRCURL="https://github.com/Kk376/ferrisfetch/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SRCURL="https://github.com/kk376/ferrisfetch/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=6e2fbf052988cb945c436087f42dcf9ebd8885d3cd19638b58221e7d41eca9df
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
