5602eaa27ce5d2ddcb7162b3a2e79217ef0dc8c6
Stas Sergeev <stsp2@yandex.ru>
2026-08-18 17:21:15 +0300

    fdpp: update to 1.11

--- tur/fdpp/build.sh
+++ tur/fdpp/build.sh
@@ -2,26 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://github.com/dosemu2/fdpp
 TERMUX_PKG_DESCRIPTION="64-bit DOS core"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@stsp"
-TERMUX_PKG_VERSION=1.10
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/dosemu2/fdpp/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fd6e4332ab3756acff10511d7a97af8c6eb231861341f8b4194b22011a8dc749
-TERMUX_PKG_BUILD_DEPENDS="nasm-segelf-cross, libelf, thunk-gen-cross"
+TERMUX_PKG_VERSION="1.11"
+TERMUX_PKG_SRCURL="https://github.com/dosemu2/fdpp/archive/refs/tags/${TERMUX_PKG_VERSION[0]}.tar.gz"
+TERMUX_PKG_SHA256="66a66103847ed33167c9d869c9272d6a96613489b381060e61f84e385e8a3db5"
+TERMUX_PKG_SETUP_PYTHON=true
 TERMUX_PKG_DEPENDS="libelf"
+TERMUX_PKG_BUILD_DEPENDS="nasm, thunk-gen, thunk-gen-cross"
 
-termux_step_configure() {
-	local _PREFIX_FOR_SEGELF=${TERMUX_PREFIX}/opt/nasm-segelf/cross
-	local _PREFIX_FOR_THUNK_GEN=${TERMUX_PREFIX}/opt/thunk-gen/cross
-	export PATH="$PATH:$_PREFIX_FOR_SEGELF/bin"
-	export PKG_CONFIG_PATH="${_PREFIX_FOR_THUNK_GEN}/lib/$TERMUX_HOST_PLATFORM/pkgconfig"
-	cd $TERMUX_PKG_BUILDDIR
-	$TERMUX_PKG_SRCDIR/configure
-}
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		local _PREFIX_FOR_THUNK_GEN="${TERMUX_PREFIX}/opt/thunk-gen/cross"
+		export PKG_CONFIG_PATH="${_PREFIX_FOR_THUNK_GEN}/share/pkgconfig"
+	fi
 
-termux_step_make() {
-	make -j $TERMUX_PKG_MAKE_PROCESSES -C $TERMUX_PKG_BUILDDIR prefix=$PREFIX CC=$CC CXX=$CXX
-}
-
-termux_step_make_install() {
-	make -C $TERMUX_PKG_BUILDDIR install prefix=$PREFIX
+	TERMUX_PKG_EXTRA_MAKE_ARGS="CROSS_LD=$LD"
 }
