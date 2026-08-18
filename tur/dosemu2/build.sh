c5c7b7b02721cc399aeca996db8a297126802491
Stas Sergeev <stsp2@yandex.ru>
2026-08-18 20:10:29 +0300

    dosemu2: update to latest git

--- tur/dosemu2/build.sh
+++ tur/dosemu2/build.sh
@@ -3,13 +3,13 @@ TERMUX_PKG_DESCRIPTION="Run DOS programs under linux."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@stsp"
 TERMUX_PKG_VERSION="2.0pre9-git"
-TERMUX_PKG_REVISION=8
+TERMUX_PKG_REVISION=9
 TERMUX_PKG_SRCURL=git+https://github.com/dosemu2/dosemu2
 TERMUX_PKG_GIT_BRANCH=devel
-_COMMIT=36d7c7757a9d63c7456c2f682fb7388ea8462bbe
-TERMUX_PKG_SHA256=3ccdcd722baf48cd4075beda0b331fdc04e422d85c423a5b4e4199e64f4b6645
-TERMUX_PKG_BUILD_DEPENDS="libandroid-posix-semaphore, libandroid-glob, slang, libao, fluidsynth, ladspa-sdk, libslirp, libbsd, readline, json-c, libseccomp, libsearpc, sdl3, sdl3-ttf, fontconfig, fdpp, dj64dev"
-TERMUX_PKG_DEPENDS="comcom64, instfd, libandroid-posix-semaphore, libandroid-glob, slang, libao, fluidsynth, ladspa-sdk, libslirp, libbsd, readline, json-c, libseccomp, libsearpc, sdl3, sdl3-ttf, fontconfig, fdpp, dj64dev"
+_COMMIT=82770aba398485117c56523a1a5c261f6e37ca64
+TERMUX_PKG_SHA256=6ae390a22f2e50600937b21172568a11df35c04440002ffbb0554b76bbdbfe58
+TERMUX_PKG_BUILD_DEPENDS="libandroid-posix-semaphore, libandroid-glob, slang, libao, fluidsynth, ladspa-sdk, libslirp, readline, json-c, libseccomp, libsearpc, sdl3, sdl3-ttf, fontconfig, fdpp, dj64dev"
+TERMUX_PKG_DEPENDS="comcom64, instfd, libandroid-posix-semaphore, libandroid-glob, slang, libao, fluidsynth, ladspa-sdk, libslirp, readline, json-c, libseccomp, libsearpc, sdl3, sdl3-ttf, fontconfig, fdpp, dj64dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-landlock
 --disable-solib
@@ -17,7 +17,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_post_get_source() {
 	git fetch --unshallow
-	git checkout $_COMMIT
+	git checkout "$_COMMIT"
 	local s=$(git ls-files | xargs cat | sha256sum -b)
 	if [[ "${s}" != "${TERMUX_PKG_SHA256} "* ]]; then
 		termux_error_exit "Checksum mismatch for source files."
@@ -25,7 +25,7 @@ termux_step_post_get_source() {
 }
 
 termux_step_pre_configure() {
-	cd $TERMUX_PKG_SRCDIR
+	cd "$TERMUX_PKG_SRCDIR"
 	./autogen.sh
 	# switch off X plugin and use SDL instead
 	sed -i -E 's/^X$//' plugin_list
