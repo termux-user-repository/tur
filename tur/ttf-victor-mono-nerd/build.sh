3f6b6e41464ecab8565e7c1bbe258fb4c4bf206e
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2026-08-21 03:27:22 +0800

    ttf-victor-mono-nerd: rebuild

--- tur/ttf-victor-mono-nerd/build.sh
+++ tur/ttf-victor-mono-nerd/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="OFL-1.1"
 TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="Gouranga Das Samrat <gouranga.das.khulna@gmail.com>"
 TERMUX_PKG_VERSION=3.5.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/ryanoasis/nerd-fonts/releases/download/v${TERMUX_PKG_VERSION}/VictorMono.zip
 TERMUX_PKG_SHA256=6f81fe83b7d4fcb27941d13e860c43e36621e124a330f5aeff2adcd8dede57b4
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
