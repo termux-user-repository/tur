d228aee4c17bce5409158861af7faf95a4616e47
Wataamee777 <wataamee777@gmail.com>
2026-08-22 14:03:24 +0900

    add:some packages

---
+++ tur/linux-ibm/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE="https://archive.ubuntu.com/ubuntu/pool/main/l/linux-ibm/"
+TERMUX_PKG_DESCRIPTION="Auto generated package for linux-ibm"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="Ubuntu Kernel Team <kernel-team@lists.ubuntu.com>"
+TERMUX_PKG_VERSION="5.4.0"
+TERMUX_PKG_SRCURL="https://archive.ubuntu.com/ubuntu/pool/main/l/linux-ibm/"
+
+# Autotools または標準 Make (Termuxのデフォルト動作を利用)
