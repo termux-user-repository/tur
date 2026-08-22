d1ad6983b19ebfb9e2ee8b610ca168e4a0d0e190
Wataamee777 <wataamee777@gmail.com>
2026-08-22 14:03:24 +0900

    add:some packages

---
+++ tur/gcc-defaults/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE="https://archive.ubuntu.com/ubuntu/pool/main/g/gcc-defaults/"
+TERMUX_PKG_DESCRIPTION="Auto generated package for gcc-defaults"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="Debian GCC Maintainers <debian-gcc@lists.debian.org>"
+TERMUX_PKG_VERSION="1.124ubuntu6"
+TERMUX_PKG_SRCURL="https://archive.ubuntu.com/ubuntu/pool/main/g/gcc-defaults/"
+
+# Autotools または標準 Make (Termuxのデフォルト動作を利用)
