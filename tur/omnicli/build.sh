a1edb4c3ed1f65ba8fdd9b8e0f3012d5f4b3eef9
Jules <manash07ji@gmail.com>
2026-08-24 03:35:25 +0000

    fix: remove empty TERMUX_PKG_DEPENDS string causing CI failure

--- tur/omnicli/build.sh
+++ tur/omnicli/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_SRCURL="https://github.com/Manash07Bhoi/OmniCLI/archive/refs/tags/v${
 TERMUX_PKG_SHA256=9732f3b96f51246f878d00af15c2245f8df6cdc62535dfb067b82e6e63614a6c
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS=""
+
 
 termux_step_make() {
 	termux_setup_rust
