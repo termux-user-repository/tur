98aedf509e938658c71e4f27caabb117388d6920
Stas Sergeev <stsp2@yandex.ru>
2026-08-27 22:00:14 +0300

    dosemu2: add the potential fix for crash on android
    @DuilioPerez reported another problem, which I have problems
    reproducing. But I think it should be fixed in this revision.
    There is no other way to find out, but to try out...

--- tur/dosemu2/build.sh
+++ tur/dosemu2/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="Run DOS programs under linux."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@stsp"
 TERMUX_PKG_VERSION="2.0pre9-git"
-TERMUX_PKG_REVISION=11
+TERMUX_PKG_REVISION=12
 TERMUX_PKG_SRCURL=git+https://github.com/dosemu2/dosemu2
 TERMUX_PKG_GIT_BRANCH=devel
-_COMMIT=5c4f9a44507993f55fd209ef3dd728689555b850
-TERMUX_PKG_SHA256=925d05c4d4a1722d0a080d4e836c7148da1bd91918064dd4058a6774536701b4
+_COMMIT=7a2b1c72dd3a851f105d3e40db2cda419804b1fa
+TERMUX_PKG_SHA256=99709741eca94e0c0870602cee9fbb91914d4a9f368e2ffe44dab0d4988fb7b6
 TERMUX_PKG_DEPENDS="comcom64, instfd, libandroid-posix-semaphore, libandroid-glob, slang, libao, fluidsynth, ladspa-sdk, libslirp, readline, json-c, libseccomp, libsearpc, sdl3, sdl3-ttf, fontconfig, fdpp, dj64dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-landlock
