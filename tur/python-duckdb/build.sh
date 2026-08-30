9a297298c4d1064063596f39b48f582d3470f9e3
Pablo <pablobouzada10@gmail.com>
2026-08-31 00:31:49 +0200

    fix(duckdb): move setuptools_scm to pip install and clean build depends

--- tur/python-duckdb/build.sh
+++ tur/python-duckdb/build.sh
@@ -8,7 +8,7 @@ TERMUX_PKG_SRCURL="https://github.com/duckdb/duckdb/archive/refs/tags/v${TERMUX_
 TERMUX_PKG_SHA256="72f33ee57ca7595b23957671a2cc7f7fe2be0ecc2d68f63abedcfcaa3a5c1238"
 
 TERMUX_PKG_DEPENDS="python, libc++"
-TERMUX_PKG_BUILD_DEPENDS="cmake, ninja, python-pip, python-setuptools-scm, pybind11"
+TERMUX_PKG_BUILD_DEPENDS="cmake, ninja, pybind11"
 
 TERMUX_PKG_BUILD_IN_SRC=true
 
@@ -16,6 +16,7 @@ termux_step_pre_configure() {
 	termux_setup_ninja
 	termux_setup_cmake
 	termux_setup_python_crossenv
+	pip3 install setuptools_scm
 }
 
 termux_step_make_install() {
