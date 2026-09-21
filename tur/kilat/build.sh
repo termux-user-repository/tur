04bb30c8800d73c904220a7a0f1cdd11d559df93
Shannyiee <najihko1@gmail.com>
2026-09-21 17:10:53 +0700

    tur/kilat: update maintainer format to Name <email>

--- tur/kilat/build.sh
+++ tur/kilat/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/ihsannyy/kilat
 TERMUX_PKG_DESCRIPTION="Ultra-lightweight JavaScript and TypeScript runtime for Termux"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_MAINTAINER="@ihsannyy"
+TERMUX_PKG_MAINTAINER="Ihsann <najihko1@gmail.com>"
 TERMUX_PKG_VERSION="5.0.0"
 TERMUX_PKG_SRCURL="https://github.com/ihsannyy/kilat/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=00f68d67c9abae14c55842d6fb4a14a6af500c0271a2bdb3550adf213d751df4
