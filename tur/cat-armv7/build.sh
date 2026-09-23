7c06ab17d04dc389020c79158a08bbbb3e1d7d62
ivan1231545 <ivan.rygyi2000@gmail.com>
2026-09-23 16:18:28 +0300

    Update tur/cat-armv7/build.sh
    Co-authored-by: Chongyun Lee <45286352+licy183@users.noreply.github.com>

--- tur/cat-armv7/build.sh
+++ tur/cat-armv7/build.sh
@@ -1,5 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/ivan1231545/cat-armv7
-TERMUX_PKG_DESCRIPTION="ARM assembly cat utility for Termux"                                                TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_DESCRIPTION="ARM assembly cat utility for Termux"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@ivan1231545"
 TERMUX_PKG_VERSION=1.0.0
 TERMUX_PKG_SRCURL=https://github.com/ivan1231545/cat-armv7/archive/refs/tags/main.tar.gz
 TERMUX_PKG_SHA256=49013f4a6957a21d8b4bd0b92a176c97617d538067b695010413d1d95a3ec958
