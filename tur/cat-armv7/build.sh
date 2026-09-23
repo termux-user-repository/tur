f38373818d0b344fd68c203b4a15f42ca0d203a6
Ivan I. <ivan.rygyi@gmail.com>
2026-09-23 16:14:26 +0300

    fix: update srcurl and sha256 for cat-armv7

--- tur/cat-armv7/build.sh
+++ tur/cat-armv7/build.sh
@@ -1,8 +1,8 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/твой-ник/cat-armv7
+TERMUX_PKG_HOMEPAGE=https://github.com/ivan1231545/cat-armv7
 TERMUX_PKG_DESCRIPTION="ARM assembly cat utility for Termux"                                                TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=1.0.0
-TERMUX_PKG_SRCURL=https://github.com/ivan1231545/cat-armv7/archive/refs/tags/v1.0.0.tar.gz
-TERMUX_PKG_SHA256=73a94523fe34a046eed4aff72b5f9c130f515dad963c53ead99c8a39b90fb2af
+TERMUX_PKG_SRCURL=https://github.com/ivan1231545/cat-armv7/archive/refs/tags/main.tar.gz
+TERMUX_PKG_SHA256=49013f4a6957a21d8b4bd0b92a176c97617d538067b695010413d1d95a3ec958
 
 termux_step_make_install() {
 	# Копируем исходный файл .s в директорию сборки
@@ -12,4 +12,4 @@ termux_step_make_install() {
 	clang --target=armv7a-linux-gnueabihf -nostdlib -static \
 		-o $TERMUX_PREFIX/bin/cat_armv7 $TERMUX_BUILD_DIR/cat_armv7.s
 }
-https://github.com/ivan1231545/cat-armv7/archive/refs/tags/v1.0.0.tar.gz
+https://github.com/ivan1231545/cat-armv7/archive/refs/tags/main.tar.gz
