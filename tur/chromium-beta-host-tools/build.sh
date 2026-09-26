3928b1b44f3c1f444126b8ca36f2a5bd1729c2c0
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2026-09-26 17:36:52 +0800

    chromium-beta-host-tools: bump to 150.0.7871.186

--- tur/chromium-beta-host-tools/build.sh
+++ tur/chromium-beta-host-tools/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.chromium.org/Home
 TERMUX_PKG_DESCRIPTION="Chromium web browser (Host tools)"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@licy183"
-TERMUX_PKG_VERSION=149.0.7827.53
+TERMUX_PKG_VERSION=150.0.7871.186
 TERMUX_PKG_SRCURL=https://commondatastorage.googleapis.com/chromium-browser-official/chromium-$TERMUX_PKG_VERSION-lite.tar.xz
-TERMUX_PKG_SHA256=dabb5f0af076a53f2eb436703affcb51a5e07e08d078b2f39a0430b1a5166c34
+TERMUX_PKG_SHA256=4ff4b64971c8097631a4f17d2f7fb050e8ede62b1b3c0f1ef0a1fffcac11bbb7
 TERMUX_PKG_DEPENDS="atk, cups, dbus, fontconfig, gtk3, krb5, libc++, libevdev, libxkbcommon, libminizip, libnss, libx11, mesa, openssl, pango, pipewire, pulseaudio, zlib"
 TERMUX_PKG_BUILD_DEPENDS="libffi-static"
 # TODO: Split chromium-common and chromium-headless
@@ -59,13 +59,6 @@ termux_pkg_auto_update() {
 }
 
 termux_step_post_get_source() {
-	# Apply patches related to c++23
-	local f
-	for f in $(find "$TERMUX_PKG_BUILDER_DIR/cxx-patches" -maxdepth 1 -type f -name *.patch | sort); do
-		echo "Applying patch: $(basename $f)"
-		patch -p1 --silent < "$f"
-	done
-
 	# Apply patches related to chromium
 	local f
 	for f in $(find "$TERMUX_PKG_BUILDER_DIR/cr-patches" -maxdepth 1 -type f -name *.patch | sort); do
@@ -319,9 +312,6 @@ exclude_unwind_tables = false
 use_jumbo_build = true
 # Compile pdfium as a static library
 pdf_is_complete_lib = true
-# NDK r29 can't compile chromium with cxx23, see
-# https://github.com/termux/termux-packages/issues/28459#issuecomment-3991943697
-use_cxx23 = false
 " > $_common_args_file
 
 	if [ "$TERMUX_ARCH" = "arm" ]; then
